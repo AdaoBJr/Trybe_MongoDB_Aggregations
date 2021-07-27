9ad1cc78ceafd9ea532f8665252f9b91573ec35d
denis-rossati <denis.rossati@hotmail.com>
2021-07-27 20:59:35 -0300

    requisito 3 - desenvolvimento / requisito 2 - code climate

---
+++ desafio3.sql
@@ -0,0 +1,6 @@
+SELECT
+jobs.JOB_TITLE AS "Cargo",
+jobs.MAX_SALARY - jobs.MIN_SALARY AS "Diferença entre salários máximo e mínimo"
+FROM hr.employees employees
+INNER JOIN hr.jobs jobs
+WHERE employees.JOB_ID = jobs.JOB_ID;
