40c680d594f55a06059d476120d6ae4056a331ca
Thiago Marchini <thiago@msn.com>
2021-07-27 20:43:12 -0300

    Correcoes CodeClimate

--- desafio5.sql
+++ desafio5.sql
@@ -1,9 +1,10 @@
-SELECT jobs.JOB_TITLE AS 'Cargo',
+SELECT
+jobs.JOB_TITLE AS 'Cargo',
 (jobs.MAX_SALARY - jobs.MIN_SALARY) AS 'Variação Salarial',
-(jobs.MIN_SALARY/12) AS 'Média Mínima Mensal',
+(jobs.MIN_SALARY/12) AS 'Média mínima Mensal',
 (jobs.MAX_SALARY/12) AS 'Média máxima mensal'
 FROM hr.jobs AS jobs
 INNER JOIN hr.employees AS employees
 ON jobs.JOB_ID = employees.JOB_ID
-GROUP BY (jobs.MAX_SALARY - jobs.MIN_SALARY)
-ORDER BY (jobs.MAX_SALARY - jobs.MIN_SALARY) ASC, JOB_TITLE ASC;
+GROUP BY `Cargo`, `Variação Salarial`, `Média mínima Mensal`, `Média máxima mensal`
+ORDER BY `Variação Salarial` ASC, `Cargo` ASC;
