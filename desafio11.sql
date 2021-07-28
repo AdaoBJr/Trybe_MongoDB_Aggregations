41de9826bde763a318b9ae39647a6c28c0a4dbac
Ederson Rodrigues Araujo <edersonrodara@gmail.com>
2021-07-28 20:08:17 -0300

    fix

--- desafio11.sql
+++ desafio11.sql
@@ -1,1 +1,11 @@
-SELECT t1.ContactName AS Nome, t1.Country AS País, COUNT(t1.Country) FROM w3schools.customers t1, w3schools.customers t2 WHERE t1.Country = t2.Country AND t1.ContactName <> t2.ContactName GROUP BY t1.ContactName, t1.Country ORDER BY Nome; 
+SELECT
+  t1.ContactName AS Nome,
+  t1.Country AS País,
+  COUNT(t1.Country) AS 'Número de compatriotas'
+FROM w3schools.customers t1,
+     w3schools.customers t2
+WHERE t1.Country = t2.Country
+AND t1.ContactName <> t2.ContactName
+GROUP BY t1.ContactName,
+         t1.Country
+ORDER BY Nome;
