33fe4d1a8e0e244cb7915c79ae86d385db0d5155
Alessandra Rezende <alessandra.rosarezende@gmail.com>
2021-07-27 22:01:03 -0300

    Requisito7

---
+++ desafio7.sql
@@ -0,0 +1,7 @@
+SELECT CONCAT(e.FIRST_NAME, ' ', e.LAST_NAME) 'Nome completo', jh.START_DATE 'Data de início', e.SALARY 'Salário'
+FROM hr.employees e
+INNER JOIN hr.job_history jh ON e.EMPLOYEE_ID = jh.EMPLOYEE_ID
+INNER JOIN hr.departments d ON d.DEPARTMENT_ID = jh.DEPARTMENT_ID
+INNER JOIN hr.jobs j ON j.JOB_ID = jh.JOB_ID
+WHERE MONTH(jh.START_DATE) IN (01, 02, 03)
+ORDER BY CONCAT(e.FIRST_NAME, ' ', e.LAST_NAME), jh.START_DATE;
