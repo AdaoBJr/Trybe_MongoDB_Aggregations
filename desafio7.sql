1044ce999d17538459a69c839427bc319077a790
dennis <dennismarcelo15@gmail.com>
2021-07-28 17:40:54 -0300

    desafio 7

---
+++ desafio7.sql
@@ -0,0 +1,11 @@
+SELECT 
+    concat(FIRST_NAME, ' ',LAST_NAME ) AS 'Nome completo',
+    START_DATE AS 'Data de início',
+    SALARY AS 'Salário'
+FROM
+    job_history jh
+INNER JOIN employees e
+INNER JOIN jobs j
+on e.EMPLOYEE_ID = jh.EMPLOYEE_ID AND j.JOB_ID = jh.JOB_ID
+WHERE(MONTH(START_DATE) BETWEEN 1 AND 3)
+ORDER BY `Nome completo`, `Data de início`;
