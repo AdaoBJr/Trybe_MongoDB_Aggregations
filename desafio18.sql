cb889aff1577ccc773ee741faf5ec5269fde5cf7
denis-rossati <denis.rossati@hotmail.com>
2021-07-28 17:12:08 -0300

    requisito 18 - desenvolvimento

---
+++ desafio18.sql
@@ -0,0 +1,8 @@
+SELECT 
+CONCAT(employees.FIRST_NAME, " ", employees.LAST_NAME) AS "Nome completo",
+CONCAT(DAY(job_history.START_DATE), "/", MONTH(job_history.START_DATE), "/", YEAR(job_history.START_DATE)) AS "Data de início",
+CONCAT(DAY(job_history.END_DATE), "/", MONTH(job_history.END_DATE), "/", YEAR(job_history.END_DATE)) AS "Data de rescisão",
+YEAR(job_history.END_DATE) - YEAR(job_history.START_DATE) AS "Anos trabalhados"
+FROM hr.job_history AS job_history
+INNER JOIN hr.employees AS employees
+ON job_history.EMPLOYEE_ID = employees.EMPLOYEE_ID;
