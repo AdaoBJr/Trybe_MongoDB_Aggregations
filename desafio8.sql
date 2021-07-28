b3f57c4434006a8d26697671273719cc93619ce6
Adão Benites <adaobjr@gmail.com>
2021-07-28 18:02:14 -0400

    feat: :sparkles: Req-08

---
+++ desafio8.sql
@@ -0,0 +1,13 @@
+SELECT
+  c.ContactName AS 'Nome de contato',
+  sh.ShipperName AS 'Empresa que fez o envio',
+  od.OrderDate AS 'Data do pedido'
+
+FROM
+    w3schools.orders AS od
+    INNER JOIN w3schools.customers AS c ON od.CustomerID = c.CustomerID
+    INNER JOIN w3schools.shippers AS sh ON od.ShipperID = sh.ShipperID
+
+WHERE
+    sh.ShipperName IN ('Speedy Express' , 'United Package')
+ORDER BY 1, 2, 3;
