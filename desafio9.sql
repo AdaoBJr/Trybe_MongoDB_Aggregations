50ec3bd79ff78c50131a08635f08eddd93192908
Henrique 'Rikos' Zózimo <hzozimo@gmail.com>
2021-07-28 16:57:33 -0300

    desafios8 e 9 rererefeitos

--- desafio9.sql
+++ desafio9.sql
@@ -1,9 +1,9 @@
 SELECT 
-    CONCAT(e.FirstName, ' ', e.LastName) AS 'Nome Completo',
+    CONCAT(e.FirstName, ' ', e.LastName) AS 'Nome completo',
     COUNT(*) AS 'Total de pedidos'
 FROM
     w3schools.orders AS o
         INNER JOIN
     w3schools.employees AS e ON o.EmployeeID = e.EmployeeID
 GROUP BY o.EmployeeID
-ORDER BY COUNT(o.OrderID)
+ORDER BY COUNT(*)
