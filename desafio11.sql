db188347fb7fdf532179af8200d92e0709b5a828
Fabio Marturano <fabiocmjor@gmail.com>
2021-07-28 16:33:07 -0300

    Ajuste req 11

--- desafio11.sql
+++ desafio11.sql
@@ -3,4 +3,5 @@ ContactName as 'Nome',
 Country as 'País',
 (select count(*) - 1 from customers where Country = c.Country) as 'Número de compatriotas'
 from customers c
+where (select count(*) - 1 from customers where Country = c.Country) > 0
 order by ContactName;
