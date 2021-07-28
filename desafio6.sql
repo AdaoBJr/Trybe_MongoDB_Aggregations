93537038291bf81cc5b3f84e0df402f3c12e2c7b
tonpasso <wellpasso@gmail.com>
2021-07-28 13:35:51 -0300

    requisitos 6 e 7

---
+++ desafio6.sql
@@ -0,0 +1,13 @@
+SELECT 
+CONCAT(e.first_name, ' ', e.last_name) AS 'Nome Completo',
+j.JOB_TITLE AS 'Cargo',
+h.START_DATE AS 'Data de início do cargo',
+d.DEPARTMENT_NAME AS 'Departamento'
+FROM employees AS e
+INNER JOIN job_history AS h
+ON e.EMPLOYEE_ID = h.EMPLOYEE_ID
+INNER JOIN jobs AS j 
+ON j.JOB_ID = h.JOB_ID
+INNER JOIN departments AS d 
+ON d.DEPARTMENT_ID = h.DEPARTMENT_ID
+ORDER BY `Nome Competo` DESC, `Cargo`;
---
+++ desafio7.sql
@@ -0,0 +1,9 @@
+SELECT  
+ucase(CONCAT(e.first_name, ' ', e.last_name)) AS 'Nome Completo',
+h.START_DATE AS 'Data de início',
+e.SALARY AS 'Salário'
+FROM employees AS e
+INNER JOIN job_history AS h
+ON e.EMPLOYEE_ID = h.EMPLOYEE_ID
+WHERE month(start_date) IN(1, 2, 3)
+ORDER BY `Nome Completo`, `Data de início`;
