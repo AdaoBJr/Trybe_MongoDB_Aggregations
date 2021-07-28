b8cb14566c0073eca0a1fdb684aff45ccc7425a1
Lucas Ribeiro <lucasrb04@gmail.com>
2021-07-28 18:55:49 -0300

    correção

--- desafio2.sql
+++ desafio2.sql
@@ -4,7 +4,7 @@ SELECT
         WHEN MAX_SALARY BETWEEN 5000 AND 10000 THEN 'Baixo'
         WHEN MAX_SALARY BETWEEN 10001 AND 20000 THEN 'Médio'
         WHEN MAX_SALARY BETWEEN 20001 AND 30000 THEN 'Alto'
-        WHEN MAX_SALARY > 30000 THEN 'Muito alto'
+        WHEN MAX_SALARY > 30000 THEN 'Altíssimo'
         ELSE 'Usuário sem acesso'
     END AS 'Nível'
 FROM hr.jobs
