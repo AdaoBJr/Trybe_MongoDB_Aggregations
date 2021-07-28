e96b6c6d3ea15fbe6d074ec6eb0a96a20ac89c66
Mariana Mohr <marianamohr@gmail.com>
2021-07-28 16:52:55 -0300

    Requisito 4 - feito

---
+++ desafio4.sql
@@ -0,0 +1,28 @@
+SELECT 
+    j.JOB_title AS Cargo,
+    ROUND(AVG(e.SALARY), 2) AS 'Média salarial',
+    CASE
+        WHEN
+            AVG(e.SALARY) >= 2000
+                AND AVG(e.SALARY) <= 5800
+        THEN
+            'Júnior'
+        WHEN
+            AVG(e.SALARY) >= 5801
+                AND AVG(e.SALARY) <= 7500
+        THEN
+            'Pleno'
+        WHEN
+            AVG(e.SALARY) >= 7501
+                AND AVG(e.SALARY) <= 10500
+        THEN
+            'Sênior'
+        WHEN AVG(e.SALARY) > 10500 THEN 'CEO'
+        ELSE 'Usuário sem acesso'
+    END AS 'Senioridade'
+FROM
+    hr.jobs AS j
+        INNER JOIN
+    hr.employees AS e ON e.JOB_ID = j.JOB_ID
+GROUP BY j.JOB_ID
+ORDER BY AVG(e.SALARY) , j.JOB_TITLE;
