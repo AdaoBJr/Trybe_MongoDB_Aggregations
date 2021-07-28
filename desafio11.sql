63ac5f4a43f0ac844e3cfdb228c1f22d3b57f82d
Thiago Marchini <thiago@msn.com>
2021-07-28 10:10:44 -0300

    Adicionados desafios 11 e 12

---
+++ desafio11.sql
@@ -0,0 +1,10 @@
+SELECT
+b.ContactName AS 'Nome',
+a.Country AS 'País',
+COUNT(*) - 1 AS 'Número de Compatriotas'
+FROM w3schools.customers AS a
+INNER JOIN w3schools.customers AS b
+ON a.Country = b.Country
+GROUP BY 2, 1
+HAVING COUNT(*) - 1 <> 0
+ORDER BY 1 ASC;
