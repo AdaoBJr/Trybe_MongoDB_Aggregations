0e3cbbba7fc52d05da2bd9108136da913b8bf89b
Thiago Kachinsky <tjk091@gmail.com>
2021-07-28 16:16:50 -0300

    Requisito 8

--- desafio8.sql
+++ desafio8.sql
@@ -1,8 +1,11 @@
-SELECT c.ContactName AS 'Nome de contato', s.ShipperName AS 'Empresa que fez o envio  FROM customers', o.OrderDate AS 'Data do pedido'
+SELECT 
+c.ContactName AS 'Nome de contato', 
+s.ShipperName AS 'Empresa que fez o envio  FROM customers', 
+o.OrderDate AS 'Data do pedido'
 FROM w3schools.orders AS o
 INNER JOIN w3schools.customers AS c
 ON o.CustomerID = c.CustomerID
 INNER JOIN w3schools.shippers AS s
 ON s.ShipperID = o.ShipperID
-WHERE s.ShipperID <= 2
+WHERE o.ShipperID <= 2
 ORDER BY c.ContactName, s.ShipperName, o.OrderDate;
