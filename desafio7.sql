342adce6d8cf667295cd499fa64fc5c74c5ccf76
lucas <lucasportellaagu@gmail.com>
2021-07-28 15:50:39 -0300

    fix formatter

--- desafio7.sql
+++ desafio7.sql
@@ -1,6 +1,6 @@
 SELECT 
     UCASE(CONCAT(emp.FIRST_NAME, ' ', emp.LAST_NAME)) AS `Nome completo`,
-    hist.START_DATE AS Data de início do cargo,
+    hist.START_DATE AS `Data de início`,
     emp.SALARY AS 'Salário'
 FROM
     hr.employees AS emp
@@ -10,4 +10,4 @@ FROM
     hr.jobs AS jobs ON hist.JOB_ID = jobs.JOB_ID
 WHERE
     MONTH(hist.START_DATE) IN (1 , 2, 3)
-ORDER BY Nome completo , Data de início do cargo;
+ORDER BY `Nome completo` , `Data de início`;
