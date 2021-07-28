1327eb976f5a74551209ac50d5ff6a34539fb8c9
Renato GSouza <renatomacedo1995@gmail.com>
2021-07-28 20:15:11 -0300

    req11

--- desafio11.sql
+++ desafio11.sql
@@ -1,12 +1,10 @@
 SELECT 
 C1.ContactName 'Nome',
 C1.Country 'País',
-(SELECT 
-(COUNT(Country) - 1)
-FROM
-customers C2
-WHERE
-C1.Country = C2.Country) 'Número de compatriotas'
+(COUNT(C1.Country) - 1) 'Número de compatriotas'
 FROM
 customers C1
-ORDER BY `Nome`;
+,customers C2 WHERE C1.Country = C2.Country
+GROUP BY `Nome`
+HAVING `Número de compatriotas` > 0    
+ORDER BY `Número de compatriotas`;
