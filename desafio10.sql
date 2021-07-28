95d970f821f837430a04cb248f2d9dc07124575f
Alessandra Rezende <alessandra.rosarezende@gmail.com>
2021-07-27 22:33:52 -0300

    Requisito10

---
+++ desafio10.sql
@@ -0,0 +1,6 @@
+SELECT p.ProductName 'Produto', MIN(o.Quantity) 'Mínima', MAX(o.Quantity) 'Máxima', ROUND(AVG(o.Quantity), 2) 'Média'
+FROM w3schools.products p
+INNER JOIN w3schools.order_details o ON p.ProductID = o.ProductID
+GROUP BY o.ProductID
+HAVING AVG(o.Quantity) > 20.00
+ORDER BY AVG(o.Quantity), p.ProductName;
