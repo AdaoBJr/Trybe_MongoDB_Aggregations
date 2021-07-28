23979d6b8dd49add214cd8222fb1bf23cf85dc42
Daniel <darkdanielr@gmail.com>
2021-07-28 15:39:18 -0300

    desafio3

--- desafio3.sql
+++ desafio3.sql
@@ -1,12 +1,5 @@
 SELECT
-  JOB_TITLE AS 'Cargo',
-  CASE
-    WHEN MAX_SALARY <= 10000 THEN 'Baixo'
-    WHEN MAX_SALARY > 10001 AND
-      MAX_SALARY <= 20000 THEN 'Médio'
-    WHEN MAX_SALARY > 20001 AND
-      MAX_SALARY <= 30000 THEN 'Alto'
-    WHEN MAX_SALARY > 30000 THEN 'Altíssimo'
-  END AS 'Diferença entre salários máximo e mínimo'
-FROM hr.JOBS
-ORDER BY Cargo;
+  JOB_TITLE AS 'Cargo', 
+  MAX_SALARY - MIN_SALARY AS 'Diferença entre salários máximo e mínimo'
+FROM hr.jobs
+ORDER BY `Diferença entre salários máximo e mínimo`, `Cargo` ASC;
