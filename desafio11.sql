d231c970c8dce458bbc68ee623c9825c12ca8aa3
Lucas Martins da Silva <lucasmartins.dsilva@gmail.com>
2021-07-28 20:11:22 -0300

    corrigindo indentações nas linhas 22, 23 e 29

--- desafio11.sql
+++ desafio11.sql
@@ -20,11 +20,11 @@ Monte uma query que exiba 03 colunas:
 Os resultados devem estar ordenados pelo nome de contato da pessoa cliente em ordem alfabética.*/
 -- Créditos ao Raphael Gumieri e ao Denis Rossati
 SELECT  c1.ContactName 'Nome'
-       ,c1.Country 'País'
-       ,COUNT(c1.Country) 'Número de compatriotas'
+        ,c1.Country 'País'
+        ,COUNT(c1.Country) 'Número de compatriotas'
 FROM w3schools.customers c1, w3schools.customers c2
 WHERE c1.Country = c2.Country 
 AND c1.CustomerID <> c2.CustomerID 
 GROUP BY  c1.ContactName
-         ,c2.Country
+          ,c2.Country
 ORDER BY c1.ContactName;
