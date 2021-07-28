87eea88db41b539058ee6ccc13851b2e98d88cac
Fabio Marturano <fabiocmjor@gmail.com>
2021-07-28 15:13:10 -0300

    Ajuste req 7

--- desafio7.sql
+++ desafio7.sql
@@ -1,8 +1,8 @@
 select
-UCASE(concat(e.FIRST_NAME, ' ', e.LAST_NAME)) as 'Nome completo',
-h.START_DATE as 'Data de início',
-e.SALARY as 'Salário'
+    UCASE(concat(e.FIRST_NAME, ' ', e.LAST_NAME)) as 'Nome completo',
+    h.START_DATE as 'Data de início',
+    e.SALARY as 'Salário'
 from job_history h
 inner join employees e on h.EMPLOYEE_ID = e.EMPLOYEE_ID
 where month(h.START_DATE) in (01, 02, 03)
-order by concat(e.FIRST_NAME, ' ', e.LAST_NAME), h.START_DATE desc
+order by concat(e.FIRST_NAME, ' ', e.LAST_NAME), h.START_DATE
