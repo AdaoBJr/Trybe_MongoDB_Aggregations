1eeb899b5d112e87908212374d8f33e94c433de5
Letycia Barreto <letyciasbarreto@gmail.com>
2021-07-28 10:10:04 -0300

    FIX

--- desafio5.sql
+++ desafio5.sql
@@ -1,8 +1,8 @@
 SELECT 
     JOB_TITLE AS 'Cargo',
-	(Max_salary - min_salary) AS 'Variação Salarial',
-   ROUND( (min_salary / 12), 2) as 'Média mínima mensal',
-   ROUND( (max_salary / 12), 2) as 'Média máxima mensal'
+	  (MAX_SALARY - MIN_SALARY) AS 'Variação Salarial',
+    ROUND( (min_salary / 12), 2) as 'Média mínima mensal',
+    ROUND( (max_salary / 12), 2) as 'Média máxima mensal'
 FROM
     hr.jobs
 GROUP BY cargo
