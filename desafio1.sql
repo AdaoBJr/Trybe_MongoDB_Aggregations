ff09af4d9e1baeea3763c1e2701abfdb18d5599f
renanpbraga <renanpbraga@gmail.com>
2021-07-28 15:02:24 -0300

    adiciona req1

---
+++ desafio1.sql
@@ -0,0 +1,2 @@
+-- Exiba os países e indicando se cada um deles se encontra ou não na região formada pela Europa
+SELECT country_name AS 'País', IF(region_id = 1, 'incluído', 'não incluído') AS 'Status Inclusão' FROM hr.countries ORDER BY country_name;
