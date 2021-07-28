284bb4d1105c2c8a5d767c09ec44cdc7b39bdcf6
Jonathan Souza <souzajonathan570@gmail.com>
2021-07-28 16:19:51 -0300

    correcao de code climate

--- desafio1.sql
+++ desafio1.sql
@@ -1,7 +1,8 @@
-SELECT COUNTRY_NAME AS País,
-CASE
-	WHEN REGION_ID = '1' THEN "incluído"
+SELECT
+  COUNTRY_NAME AS País,
+  CASE
+    WHEN REGION_ID = 1 THEN "incluído"
     ELSE "não incluído"
-END AS "Status Inclusão"
+  END AS "Status Inclusão"
 FROM hr.countries
 ORDER BY País;
