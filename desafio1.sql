701abfade24f22b8d96a7cc77846889b7912a261
tonpasso <wellpasso@gmail.com>
2021-07-28 09:18:57 -0300

    requisito 1 corrigido

--- desafio1.sql
+++ desafio1.sql
@@ -1,4 +1,4 @@
 SELECT COUNTRY_NAME AS 'País', 
-REGION_ID, IF(REGION_ID = 1, 'incluído', 'não incluído') AS 'Status Inclusão'
+IF(REGION_ID = 1, 'incluído', 'não incluído') AS 'Status Inclusão'
 FROM countries
 ORDER BY COUNTRY_NAME;
