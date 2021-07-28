01800bc67782774dd39c83d25f6e54566b7edf4d
Daniel <darkdanielr@gmail.com>
2021-07-28 18:09:15 -0300

    desafio6

---
+++ desafio6.sql
@@ -0,0 +1,4 @@
+select CONCAT(FIRST_NAME,' ', LAST_NAME) AS 'Nome completo', j.JOB_TITLE as 'Cargo', HIRE_DATE as 'Data de início do cargo',  dep.DEPARTMENT_NAME as 'Departamento' from hr.employees as emplo
+inner join hr.departments AS dep ON emplo.DEPARTMENT_ID = dep.DEPARTMENT_ID
+inner join hr.jobs as j on emplo.JOB_ID = j.JOB_ID
+order by `Nome Completo`, `Cargo`;
