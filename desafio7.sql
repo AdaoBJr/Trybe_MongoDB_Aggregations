32cb9902bf7e931b7759d7333e73e8b68f5f5475
denis-rossati <denis.rossati@hotmail.com>
2021-07-28 12:48:10 -0300

    requisito 7 - correção

--- desafio7.sql
+++ desafio7.sql
@@ -5,5 +5,8 @@ employees.SALARY AS "Salário"
 FROM hr.job_history AS job_history
 INNER JOIN hr.employees AS employees
 ON employees.EMPLOYEE_ID = job_history.EMPLOYEE_ID
+WHERE MONTH(job_history.START_DATE) = 01
+OR MONTH(job_history.START_DATE) = 02
+OR MONTH(job_history.START_DATE) = 03
 ORDER BY `Nome completo` ASC,
 `Data de início` ASC;
