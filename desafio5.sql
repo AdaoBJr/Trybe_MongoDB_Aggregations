9af927ed7290ddafca1fd9ee280e6be2f47f14ff
Thiago Leite <leite.tjs@gmail.com>
2021-07-28 19:37:33 -0300

    fix 5 again, again

--- desafio5.sql
+++ desafio5.sql
@@ -3,4 +3,4 @@ ROUND((max_salary - min_salary), 2) AS 'Variação Salarial',
 ROUND((min_salary / 12), 2) AS 'Média mínima mensal',
 ROUND((max_salary  / 12), 2) AS 'Média máxima mensal'
 FROM hr.jobs
-ORDER BY `Variação salarial`, 'Cargo';
+ORDER BY `Variação salarial`, `Cargo`;
