4a862ca030b9052f77b383b4adcfe034bdc6867c
Renan0211 <renan.antunes.rocha@gmail.com>
2021-07-28 20:28:35 -0300

    resolve o desafio 11 (?)

---
+++ desafio11.sql
@@ -0,0 +1,10 @@
+SELECT 
+    c1.ContactName AS Nome, c1.Country AS País, COUNT(*) AS 'Número de compatriotas'
+FROM
+    w3schools.customers AS c1,
+    w3schools.customers AS c2
+    WHERE c1.Country = c2.Country
+    GROUP BY Nome, País
+    HAVING `Número de compatriotas` > 1
+ ORDER BY Nome;
+ 
