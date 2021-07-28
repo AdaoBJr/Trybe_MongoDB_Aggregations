0861a3600803dcb3916311789aab7b83bc7d3726
Abimaelr <abimaelr.albu@gmail.com>
2021-07-28 17:42:14 -0300

    Feature 3

--- desafio3.sql
+++ desafio3.sql
@@ -1,4 +1,4 @@
 select JOB_TITLE as Cargo,
 (MAX_SALARY - MIN_SALARY) as 'Diferença entre salários máximo e mínimo'
 from hr.jobs
-ORDER BY (MAX_SALARY - MIN_SALARY);
+ORDER BY (MAX_SALARY - MIN_SALARY), JOB_TITLE;
