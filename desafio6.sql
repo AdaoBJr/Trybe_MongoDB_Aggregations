c9cf1c33b2b653ba3ab0931b93b265ff0957ecb6
Fabio Marturano <fabiocmjor@gmail.com>
2021-07-28 14:51:10 -0300

    Ajuste Req 6

--- desafio6.sql
+++ desafio6.sql
@@ -4,7 +4,7 @@ j.JOB_TITLE as 'Cargo',
 h.START_DATE as 'Data de início do cargo',
 d.DEPARTMENT_NAME as 'Departamento'
 from employees e
-inner join jobs j on e.JOB_ID = j.JOB_ID
 inner join job_history h on e.EMPLOYEE_ID = h.EMPLOYEE_ID
+inner join jobs j on h.JOB_ID = j.JOB_ID
 inner join departments d on h.DEPARTMENT_ID = d.DEPARTMENT_ID
 order by concat(e.FIRST_NAME, ' ', e.LAST_NAME) desc, d.DEPARTMENT_NAME desc
