5b30aa09eee15d4cb500ed03d19615265c1263c4
aythan azevedo <aythan16@gmail.com>
2021-07-28 16:22:13 -0300

    aythan inicial commit

--- hr.sql
+++ hr.sql
@@ -1,6 +1,6 @@
 -- MySQL dump 10.13  Distrib 5.6.33, for debian-linux-gnu (x86_64)
 --
--- Host: localhost    Database: w3resour_hr
+-- Host: localhost    Database: w3resour_hr 
 -- ------------------------------------------------------
 -- Server version 5.6.33-0ubuntu0.14.04.1
 DROP database if exists hr;
