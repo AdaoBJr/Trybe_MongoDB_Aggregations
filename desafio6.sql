72af2a8cb7209090123cea6102c8b6187b71f988
tonpasso <wellpasso@gmail.com>
2021-07-28 14:30:10 -0300

    mais um ajuste requisitos 6 e 7

--- desafio6.sql
+++ desafio6.sql
@@ -1,5 +1,5 @@
 SELECT 
-CONCAT(e.first_name, ' ', e.last_name) AS 'Nome Completo',
+CONCAT(e.first_name, ' ', e.last_name) AS 'Nome completo',
 j.JOB_TITLE AS 'Cargo',
 h.START_DATE AS 'Data de início do cargo',
 d.DEPARTMENT_NAME AS 'Departamento'
@@ -10,4 +10,4 @@ INNER JOIN jobs AS j
 ON h.JOB_ID = j.JOB_ID
 INNER JOIN departments AS d 
 ON d.DEPARTMENT_ID = h.DEPARTMENT_ID
-ORDER BY `Nome Completo` DESC, `Cargo`;
+ORDER BY `Nome completo` DESC, `Cargo`;
--- desafio7.sql
+++ desafio7.sql
@@ -1,9 +1,9 @@
 SELECT  
-ucase(CONCAT(e.first_name, ' ', e.last_name)) AS 'Nome Completo',
+ucase(CONCAT(e.first_name, ' ', e.last_name)) AS 'Nome completo',
 h.START_DATE AS 'Data de início',
 e.SALARY AS 'Salário'
 FROM employees AS e
 INNER JOIN job_history AS h
 ON h.EMPLOYEE_ID = e.EMPLOYEE_ID
 WHERE MONTH(h.START_DATE) IN(1, 2, 3)
-ORDER BY `Nome Completo` ASC, `Data de início` ASC; 
+ORDER BY `Nome completo` ASC, `Data de início` ASC; 
