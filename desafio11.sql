c98a655b750639120d9a436e0dfe8b990a3cced9
Hugo Simoes <hugolfsimoes@gmail.com>
2021-07-28 19:05:27 -0300

    feito tarefa 11

---
+++ desafio11.sql
@@ -0,0 +1,5 @@
+SELECT c1.ContactName AS Nome, c1.Country AS País, COUNT(*) AS `Número de compatriotas` 
+FROM w3schools.customers as c1, w3schools.customers AS c2
+WHERE c1.Country = c2.Country AND c1.CustomerName <> c2.CustomerName
+GROUP BY Nome
+ORDER BY Nome;
