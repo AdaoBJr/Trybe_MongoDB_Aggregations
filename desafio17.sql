584d95dbced7d10fc394da325f6971f7b1baf975
denis-rossati <denis.rossati@hotmail.com>
2021-07-28 16:56:31 -0300

    requisito 17 - correção

--- desafio17.sql
+++ desafio17.sql
@@ -5,7 +5,6 @@ CREATE TRIGGER insert_date_on_order_date
 BEFORE INSERT ON w3schools.orders
 FOR EACH ROW
 BEGIN
-	SET NEW.OrderDate = DATE(NOW());
+SET NEW.OrderDate = DATE(NOW());
 END $$
-
 DELIMITER ;
