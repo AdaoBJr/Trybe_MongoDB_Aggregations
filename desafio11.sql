007dec6dc8e77e7500696d1cdca0347799715ca7
joao966 <joaooliveira966@gmail.com>
2021-07-28 18:50:34 -0300

    FIX: request-11

--- desafio11.sql
+++ desafio11.sql
@@ -1,1 +1,1 @@
-select contactName as 'Nome', country as 'País', (select count(*) from w3schools.customers as clientes_2 where clientes_1.Country = clientes_2.Country and clientes_1.ContactName <> clientes_2.ContactName ) from w3schools.customers as clientes_1 order by `Nome`;
+select contactName as 'Nome', country as 'País', (select count(*) from w3schools.customers as clientes_2 where clientes_1.Country = clientes_2.Country and clientes_1.ContactName <> clientes_2.ContactName ) as 'Número de compatriotas' from w3schools.customers as clientes_1  order by `Nome`;
