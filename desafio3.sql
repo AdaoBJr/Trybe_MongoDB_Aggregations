cc939967bdc8f560feca88bc5291797cf329bebc
Fabio Marturano <fabiocmjor@gmail.com>
2021-07-28 11:21:46 -0300

    Ajuste Req 3

--- desafio3.sql
+++ desafio3.sql
@@ -3,4 +3,4 @@ JOB_TITLE as 'Cargo',
 (MAX_SALARY - MIN_SALARY) as 'Diferença entre salários máximo e mínimo'
 FROM
 jobs
-ORDER BY (MAX_SALARY - MIN_SALARY);
+ORDER BY (MAX_SALARY - MIN_SALARY), JOB_TITLE;
