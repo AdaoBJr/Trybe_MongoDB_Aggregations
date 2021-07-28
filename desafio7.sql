5b729712810e9304c9d198558d1d966357aec3c1
tonpasso <wellpasso@gmail.com>
2021-07-28 13:45:24 -0300

    correcoes

--- desafio7.sql
+++ desafio7.sql
@@ -5,5 +5,5 @@ e.SALARY AS 'Salário'
 FROM employees AS e
 INNER JOIN job_history AS h
 ON e.EMPLOYEE_ID = h.EMPLOYEE_ID
-WHERE month(start_date) IN(1, 2, 3)
-ORDER BY `Nome Completo`, `Data de início`;
+WHERE month(h.start_date) IN(1, 2, 3)
+ORDER BY `Nome Completo`, `Data de início`; 
