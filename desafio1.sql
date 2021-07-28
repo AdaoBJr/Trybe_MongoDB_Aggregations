b6822e8e065b44a91a0393447c9ba7c5609e8d24
Renato GSouza <renatomacedo1995@gmail.com>
2021-07-28 16:07:52 -0300

    req1

--- desafio1.sql
+++ desafio1.sql
@@ -1,1 +1,1 @@
-SELECT COUNTRY_NAME 'País',(SELECT IF (REGION_ID = 1, 'incluido', 'não incluido')) 'Status Inclusão'  FROM countries;
+SELECT COUNTRY_NAME 'País',(SELECT IF (REGION_ID = 1, 'incluido', 'não incluido')) 'Status Inclusão'  FROM countries ORDER BY 'País';
