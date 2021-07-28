25dbdfd6f9a29d7fa668aac6ff2455603f194657
Daniel <dsouzaborini@gmail.com>
2021-07-28 17:21:48 -0300

    add req 4

---
+++ desafio4.sql
@@ -0,0 +1,16 @@
+SELECT
+  JOB_TITLE 'Cargo',
+  ROUND(AVG(e.SALARY), 2) 'Média salarial',
+  CASE
+    WHEN AVG(e.SALARY) BETWEEN 2000 AND 5800 THEN 'Júnior'
+    WHEN AVG(e.SALARY) BETWEEN 5801 AND 7500 THEN 'Pleno'
+    WHEN AVG(e.SALARY) BETWEEN 7501 AND 10500 THEN 'Sênior'
+    WHEN AVG(e.SALARY) > 10500 THEN 'CEO'
+    ELSE 'não incluído'
+  END AS 'Senioridade'
+FROM 
+  hr.jobs j
+   INNER JOIN
+  hr.employees e ON j.JOB_ID = e.JOB_ID
+GROUP BY j.JOB_ID
+ORDER BY AVG(e.SALARY), JOB_TITLE;
