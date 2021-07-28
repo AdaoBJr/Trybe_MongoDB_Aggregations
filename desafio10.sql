cfb8c132f24d509e1493aa1aac9637c4ace4186f
Paulo Xavier <pxavierf@gmail.com>
2021-07-28 15:32:14 -0300

    desafio10 fixing

--- desafio10.sql
+++ desafio10.sql
@@ -1,6 +1,6 @@
 SELECT p.ProductName AS 'Produto',
 MIN(od.Quantity) AS 'Mínima',
-MAX(od.Quantity) AS 'Máxina',
+MAX(od.Quantity) AS 'Máxima',
 ROUND(AVG(od.Quantity), 2) AS 'Média'
 FROM w3schools.products AS p
 INNER JOIN w3schools.order_details AS od
