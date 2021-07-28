f1435bf7fbcbb2639c96f783d632dd4051a248a7
Maiara Borsatti <maiara.borsatti@gmail.com>
2021-07-28 16:46:14 -0300

    desafio 6

--- desafio6.sql
+++ desafio6.sql
@@ -4,11 +4,11 @@ SELECT
   h.START_DATE AS 'Data de início do cargo',
   d.DEPARTMENT_NAME AS Departamento
 FROM hr.job_history AS h
-LEFT JOIN hr.employees AS e
+INNER JOIN hr.employees AS e
   ON e.EMPLOYEE_ID = h.EMPLOYEE_ID
-RIGHT JOIN hr.jobs AS cargo
+INNER JOIN hr.jobs AS cargo
   ON cargo.JOB_ID = h.JOB_ID
-RIGHT JOIN hr.departments AS d
+INNER JOIN hr.departments AS d
   ON d.DEPARTMENT_ID = h.DEPARTMENT_ID
 WHERE CONCAT(FIRST_NAME, ' ', LAST_NAME) IS NOT NULL
 ORDER BY CONCAT(FIRST_NAME, ' ', LAST_NAME) DESC, cargo.JOB_TITLE;
