035c1cf0a823d47f9984f4b68afdd4750f218213
Maiara Borsatti <maiara.borsatti@gmail.com>
2021-07-28 15:08:22 -0300

    desafio 4

---
+++ desafio4.sql
@@ -0,0 +1,14 @@
+SELECT
+  hr.jobs.JOB_TITLE AS 'Cargo',
+  ROUND(AVG(SALARY), 2) AS 'Média salarial',
+  CASE
+    WHEN AVG(SALARY) BETWEEN 2000 AND 5800 THEN 'Júnior'
+    WHEN AVG(SALARY) BETWEEN 5801 AND 7500 THEN 'Pleno'
+    WHEN AVG(SALARY) BETWEEN 7501 AND 10500 THEN 'Sênior'
+    ELSE 'CEO'
+  END AS 'Senioridade'
+FROM hr.employees
+INNER JOIN hr.jobs
+  ON hr.employees.JOB_ID = hr.jobs.JOB_ID
+GROUP BY hr.jobs.JOB_TITLE
+ORDER BY AVG(SALARY), hr.jobs.JOB_TITLE;
