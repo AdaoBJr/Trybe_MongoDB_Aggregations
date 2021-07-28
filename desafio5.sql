3e0b738242978e800fb317c12efc17620cdc2853
Camila Arruda <camilaam1802@gmail.com>
2021-07-28 18:21:10 -0300

    Requisito 5 corrigido

--- desafio5.sql
+++ desafio5.sql
@@ -1,7 +1,7 @@
 SELECT job_title AS Cargo,
 (max_salary - min_salary) AS 'Variação Salarial',
 round(AVG(min_salary/12),2) AS 'Média mínima mensal',
-round(AVG(max_salary/12),2) AS 'Média mínima mensal'
+round(AVG(max_salary/12),2) AS 'Média máxima mensal'
 FROM jobs
 GROUP BY job_title
 ORDER BY (max_salary - min_salary) ASC, job_title ASC;
