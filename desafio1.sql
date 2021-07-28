d5c121c36f23fa07d9ebbd46cc30979c5aa07e31
Dângelo-Silva-Miranda <dangelo.silva.miranda@gmail.com>
2021-07-28 18:13:48 -0300

    fix: :art: corrige erro de comentário

--- desafio1.sql
+++ desafio1.sql
@@ -1,4 +1,4 @@
---1 - Exiba os países e indicando se cada um deles se encontra ou não na região formada pela Europa
+/* 1 - Exiba os países e indicando se cada um deles se encontra ou não na região formada pela Europa */
 SELECT 
   c.COUNTRY_NAME `País`, 
   IF(r.REGION_NAME = 'Europe', 'incluído', 'não incluído') `Status Inclusão`
