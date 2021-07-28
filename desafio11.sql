2e8795ede74720f73eb13728d7921a8413323519
Alessandra Rezende <alessandra.rosarezende@gmail.com>
2021-07-28 08:45:22 -0300

    Requisito11

--- desafio11.sql
+++ desafio11.sql
@@ -1,5 +1,5 @@
-SELECT c1.ContactName 'Nome', c2.Country 'País', COUNT(*) 'Número de compatriotas'
+SELECT c1.ContactName 'Nome', c1.Country 'País', COUNT(*) 'Número de compatriotas'
 FROM w3schools.customers c1, w3schools.customers c2
-WHERE c1.Country = c2.Country AND c1.CustomerID <> c2.CustomerID
+WHERE c1.Country = c2.Country AND c1.ContactName <> c2.ContactName
 GROUP BY c1.ContactName
 ORDER BY c1.ContactName;
