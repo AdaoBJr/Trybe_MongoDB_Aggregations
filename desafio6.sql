2ba3953e2a79472c64e9ebb93acc8bf61c364f94
Alessandra Rezende <alessandra.rosarezende@gmail.com>
2021-07-27 21:56:15 -0300

    Requisito6

---
+++ desafio6.sql
@@ -0,0 +1,6 @@
+SELECT CONCAT(e.FIRST_NAME, ' ', e.LAST_NAME) 'Nome completo', j.JOB_TITLE, jh.START_DATE 'Data de início do cargo', d.DEPARTMENT_NAME
+FROM hr.employees e
+INNER JOIN hr.job_history jh ON e.EMPLOYEE_ID = jh.EMPLOYEE_ID
+INNER JOIN hr.departments d ON d.DEPARTMENT_ID = jh.DEPARTMENT_ID
+INNER JOIN hr.jobs j ON j.JOB_ID = jh.JOB_ID
+ORDER BY CONCAT(e.FIRST_NAME, ' ', e.LAST_NAME) DESC, j.JOB_TITLE;
