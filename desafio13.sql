c104a2ff81db1aec82e9eb8de5ff3524cfce3ba9
Felippe <felippecorrea2010@gmail.com>
2021-07-28 20:41:23 -0300

    DONE: desafio13.sql

---
+++ desafio13.sql
@@ -0,0 +1,1 @@
+SELECT p.ProductName AS 'Produto', p.Price AS 'Preço'  FROM w3schools.products AS p INNER JOIN w3schools.order_details AS o ON p.ProductID = o.ProductID WHERE o.quantity > 80 GROUP BY p.ProductName, p.Price ORDER BY ProductName;
