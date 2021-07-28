9b82cb0978969edf2c5c889babdccafc7e4e2628
Roger Franco <rogerfrancogalt@gmail.com>
2021-07-28 19:12:05 -0300

    Desafio 4

---
+++ desafio4.sql
@@ -0,0 +1,14 @@
+SELECT
+  JOB_TITLE AS Cargo,
+  ROUND(AVG(e.SALARY), 2) AS 'Média salarial',
+  CASE
+    WHEN AVG(e.SALARY) BETWEEN 2000 AND 5800 THEN 'Júnior'
+    WHEN AVG(e.SALARY) BETWEEN 5801 AND 7500 THEN 'Pleno'
+    WHEN AVG(e.SALARY) BETWEEN 7501 AND 10500 THEN 'Sênior'
+    WHEN AVG(e.SALARY) > 10500 THEN 'CEO'
+  END AS Senioridade
+FROM hr.employees AS e
+JOIN hr.jobs AS j
+  ON e.JOB_ID = j.JOB_ID
+GROUP BY Cargo
+ORDER BY AVG(e.SALARY), Cargo;
