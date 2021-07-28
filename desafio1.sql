e4a26a75656b965fb4d9028cb8634439f8643888
brunamfguimaraes <brunamfguimaraes@gmail.com>
2021-07-28 17:54:14 -0300

    Corrigindo o requisito 1

--- desafio1.sql
+++ desafio1.sql
@@ -4,7 +4,7 @@ SELECT
         'incluído',
         'não incluído') AS 'Status Inclusão'
 FROM
-    countries AS 'País'
+    countries AS País
         INNER JOIN
     regions AS Status_Inclusao ON Pais.REGION_ID = Status_Inclusao.REGION_ID
 ORDER BY COUNTRY_NAME ASC;
