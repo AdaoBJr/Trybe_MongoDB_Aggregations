5befbb0a53ade1f0ab69166acd7a928c000a857d
riquee <strikedv@hotmail.com>
2021-07-28 14:13:36 -0300

    agora vai...

--- desafio6.sql
+++ desafio6.sql
@@ -1,10 +1,10 @@
 SELECT 
-CONCAT(t2.first_name, ' ', t2.last_name) 'Nome completo',
-t3.job_title 'Cargo',
-t1.start_date 'Data de início do cargo',
-t4.department_name 'Departamento'
-FROM hr.job_history 't1'
-INNER JOIN hr.employees 't2' ON t1.employee_id = t2.employee_id
-INNER JOIN hr.jobs 't3' ON t1.job_id = t3.job_id
-INNER JOIN hr.departments 't4' ON t1.department_id = t4.department_id
+CONCAT(t2.first_name, ' ', t2.last_name) AS 'Nome completo',
+t3.job_title AS Cargo,
+t1.start_date AS 'Data de início do cargo',
+t4.department_name AS Departamento
+FROM hr.job_history AS t1
+INNER JOIN hr.employees AS t2 ON t1.employee_id = t2.employee_id
+INNER JOIN hr.jobs AS t3 ON t1.job_id = t3.job_id
+INNER JOIN hr.departments AS t4 ON t1.department_id = t4.department_id
 ORDER BY 1 DESC , 2 ASC;
--- desafio7.sql
+++ desafio7.sql
@@ -1,8 +1,8 @@
 SELECT 
-UCASE(CONCAT(t1.first_name, ' ', t1.last_name)) 'Nome completo',
-t2.start_date 'Data de início',
-t1.salary 'Salário'
-FROM hr.employees 't1'
-INNER JOIN hr.job_history 't2' ON t1.employee_id = t2.employee_id
+UCASE(CONCAT(t1.first_name, ' ', t1.last_name)) AS 'Nome completo',
+t2.start_date AS 'Data de início',
+t1.salary AS Salário
+FROM hr.employees AS t1
+INNER JOIN hr.job_history AS t2 ON t1.employee_id = t2.employee_id
 WHERE MONTH(start_date) IN (1 , 2, 3)
 ORDER BY 1 ASC , 2 ASC;
