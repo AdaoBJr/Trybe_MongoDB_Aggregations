79c414b1d7a4fdea599e87dd404c71a5f8132dd4
riquee <strikedv@hotmail.com>
2021-07-28 09:58:56 -0300

    req12 finalizado

---
+++ desafio12.sql
@@ -0,0 +1,13 @@
+SELECT 
+	CONCAT(E1.FIRST_NAME, ' ', E1.LAST_NAME) 'Nome completo funcionário 1',
+	E1.SALARY 'Salário funcionário 1',
+	E1.PHONE_NUMBER 'Telefone funcionário 1',
+	CONCAT(E2.FIRST_NAME, ' ', E2.LAST_NAME) 'Nome completo funcionário 2',
+	E2.SALARY 'Salário funcionário 2',
+	E2.PHONE_NUMBER 'Telefone funcionário 2'
+FROM hr.employees E1, hr.employees E2
+WHERE 
+	E1.JOB_ID = E2.JOB_ID
+HAVING  
+	`Nome completo funcionário 1` <> `Nome completo funcionário 2`
+ORDER BY `Nome completo funcionário 1`, `Nome completo funcionário 2`;
