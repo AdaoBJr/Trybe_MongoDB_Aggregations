0d051c39be1a8f57ea682cd391dfd3e9780097d1
Pedro Amaral <amaralph1995@gmail.com>
2021-07-28 17:53:31 -0300

    Corrige erro

--- desafio1.sql
+++ desafio1.sql
@@ -1,3 +1,3 @@
-SELECT c.country_name AS 'País', IF(r.region_name = 'Europe', 'incluído', 'não incluido')
+SELECT c.country_name AS 'País', IF(r.region_name = 'Europe', 'incluído', 'não incluido') AS 'Status Inclusão'
 FROM hr.countries AS c
 INNER JOIN hr.regions AS r ON r.region_id = c.region_id;
