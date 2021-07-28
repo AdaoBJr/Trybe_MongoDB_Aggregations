37c0ece7743fb68219770f532305a1a9beb33334
riquee <strikedv@hotmail.com>
2021-07-28 10:00:25 -0300

    req16 finalizado

---
+++ desafio16.sql
@@ -0,0 +1,16 @@
+USE hr;
+
+DELIMITER $$
+CREATE FUNCTION buscar_quantidade_de_empregos_por_funcionario(in_email VARCHAR(20))
+RETURNS INT READS SQL DATA
+BEGIN
+	DECLARE result INT;
+    SELECT COUNT(*)
+	FROM employees E
+		JOIN job_history JH ON JH.EMPLOYEE_ID = E.EMPLOYEE_ID
+	WHERE 
+		EMAIL = in_email
+	INTO in_email;
+    RETURN in_email;
+END $$
+DELIMITER ;
