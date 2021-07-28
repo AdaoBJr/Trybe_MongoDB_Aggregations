c90af550f40cf0fb43d5c55b9f08f22973228146
EderPaiva <edergomesdepaiva12@gmail.com>
2021-07-28 14:00:15 -0300

    parte do desafio 7 completo bora pro esquenta

---
+++ desafio7.sql
@@ -0,0 +1,10 @@
+select 
+concat(employees.FIRST_NAME,' ',employees.LAST_NAME) as 'Nome completo', 
+historico.START_DATE as 'Data de início do cargo'
+from hr.job_history as historico  
+inner join hr.employees as employees
+on employees.EMPLOYEE_ID = historico.EMPLOYEE_ID
+where month(historico.START_DATE) between 1 and 3
+order by `Nome Completo` DESC
+
+
