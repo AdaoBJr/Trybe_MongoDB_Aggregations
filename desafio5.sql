a91cb38c30582cb84da004762f9d983bf52939d8
Johnny-batata <you@example.com>
2021-07-28 19:29:15 -0300

    sending again requirement 5 but now without climate issues

--- desafio5.sql
+++ desafio5.sql
@@ -9,19 +9,3 @@ FROM
     hr.employees e ON j.JOB_ID = e.JOB_ID
 GROUP BY JOB_TITLE , MAX_SALARY , MIN_SALARY
 ORDER BY j.MAX_SALARY - j.MIN_SALARY , JOB_TITLE;
-
-SELECT * FROM hr.jobs;
-SELECT * FROM hr.employees;
-SELECT * FROM hr.departments;
-
-SELECT 
-    j.JOB_TITLE AS 'Cargo',
-    (j.MAX_SALARY - j.MIN_SALARY) AS 'Variação Salarial',
-    ROUND(MAX(e.SALARY)), 2) AS 'Média mínima mensal',
-
-FROM
-    hr.jobs j
-        INNER JOIN
-    hr.employees e ON j.JOB_ID = e.JOB_ID
-GROUP BY JOB_TITLE , MAX_SALARY , MIN_SALARY
-ORDER BY j.MAX_SALARY - j.MIN_SALARY , JOB_TITLE;
