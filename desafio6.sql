dd4b7b862ef4507943ac191c52b2d6d130d34b9a
Renato GSouza <renatomacedo1995@gmail.com>
2021-07-28 18:45:57 -0300

    req6 AGORA VAI

--- desafio6.sql
+++ desafio6.sql
@@ -1,10 +1,10 @@
 SELECT 
 CONCAT(e.FIRST_NAME, ' ', e.LAST_NAME) 'Nome completo',
 j.JOB_TITLE 'Cargo', jh.START_DATE 'Data de início do cargo',
-(SELECT d.DEPARTMENT_NAME FROM departments d where e.DEPARTMENT_ID = d.DEPARTMENT_ID) 'Departamento'
+(SELECT d.DEPARTMENT_NAME FROM departments d where jh.DEPARTMENT_ID = d.DEPARTMENT_ID) 'Departamento'
 FROM
 employees e
-INNER JOIN
-jobs j ON j.JOB_ID = e.JOB_ID
 INNER JOIN job_history jh ON jh.EMPLOYEE_ID = e.EMPLOYEE_ID
+INNER JOIN
+jobs j ON j.JOB_ID = jh.JOB_ID    
 ORDER BY `Nome Completo` DESC, `Cargo`;
