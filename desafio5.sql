496b79d42b8d1506e6eb1579bc9e880d4a7cf210
Alessandra Rezende <alessandra.rosarezende@gmail.com>
2021-07-27 21:28:09 -0300

    Requisito5

--- desafio5.sql
+++ desafio5.sql
@@ -1,8 +1,4 @@
-SELECT 
-	JOB_TITLE 'Cargo', 
-  (MAX_SALARY - MIN_SALARY) 'Variação Salarial',
-  ROUND(MIN_SALARY/12, 2) 'Média mínima mensal',
-  ROUND(MAX_SALARY/12, 2) 'Média máxima mensal'
+SELECT JOB_TITLE 'Cargo', (MAX_SALARY - MIN_SALARY) 'Variação Salarial', ROUND(MIN_SALARY/12, 2) 'Média mínima mensal', ROUND(MAX_SALARY/12, 2) 'Média máxima mensal'
 FROM hr.jobs
 GROUP BY JOB_ID
 ORDER BY (MAX_SALARY - MIN_SALARY), JOB_TITLE;
