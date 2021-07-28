c4d69047d4d56b687357dd5b1dc914e0346f4e47
Huggo Parcelly <h.parcelly@gmail.com>
2021-07-28 14:58:28 -0300

    ordena os paises

--- desafio1.sql
+++ desafio1.sql
@@ -2,4 +2,5 @@ SELECT C.COUNTRY_NAME 'País',
 IF(R.REGION_NAME = 'Europe', 'incluso', 'não incluso') 'Status Inclusão' 
 FROM hr.countries C
 INNER JOIN hr.regions R
-ON R.REGION_ID = C.REGION_ID;
+ON R.REGION_ID = C.REGION_ID
+ORDER BY `País`;
