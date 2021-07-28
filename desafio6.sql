374226d4e20e9a8d2c4f0d4e680c2927470f5ac6
Fernanda Porto Oliveira <fernanda.oliveira.mec@gmail.com>
2021-07-28 16:49:59 -0300

    Desafio 6 com inner join

--- desafio6.sql
+++ desafio6.sql
@@ -4,14 +4,14 @@ h.START_DATE AS 'Data de início do cargo' ,
 d.DEPARTMENT_NAME AS Departamento
 
 FROM job_history AS h
-LEFT JOIN employees AS e
+INNER JOIN employees AS e
 ON e.EMPLOYEE_ID = h.EMPLOYEE_ID 
 
-RIGHT JOIN jobs AS cargo
+INNER JOIN jobs AS cargo
 ON cargo.JOB_ID = h.JOB_ID
 
-RIGHT JOIN departments AS d
+INNER JOIN departments AS d
 ON d.DEPARTMENT_ID = h.DEPARTMENT_ID
-WHERE CONCAT(FIRST_NAME, ' ',LAST_NAME) IS NOT NULL
+
 ORDER BY CONCAT(FIRST_NAME, ' ',LAST_NAME) DESC,
 cargo.JOB_TITLE;
