81e1d1e296acb4f7ab0a4f3acb522228e573b45c
Misael Bueno <misa.hbueno@gmail.com>
2021-07-28 19:26:57 -0300

    desafio 7 corrigido

--- desafio7.sql
+++ desafio7.sql
@@ -1,4 +1,4 @@
-SELECT CONCAT(FIRST_NAME, ' ', LAST_NAME) AS `Nome completo`,
+SELECT UCASE(CONCAT(FIRST_NAME, ' ', LAST_NAME)) AS `Nome completo`,
 START_DATE AS `Data de início`,
 SALARY AS 'Salário'
 FROM hr.employees AS E
