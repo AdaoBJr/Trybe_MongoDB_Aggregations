a7e3f7166e5fd859d5d80ae5fb59092bc2cb24a7
Raphael Gumieri <raphaelgumieri@hotmail.com>
2021-07-28 11:02:08 -0300

    desafio 1 - adicionado ordenação asc

--- desafio1.sql
+++ desafio1.sql
@@ -1,1 +1,1 @@
-SELECT country_name AS 'País', CASE WHEN region_id = 1 THEN 'incluído' ELSE 'não incluído' END AS 'Status Inclusão' FROM hr.countries;
+SELECT country_name AS 'País', CASE WHEN region_id = 1 THEN 'incluído' ELSE 'não incluído' END AS 'Status Inclusão' FROM hr.countries order by country_name;
