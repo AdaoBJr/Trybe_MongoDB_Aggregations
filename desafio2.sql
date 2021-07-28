c14085e98dbfcd0fdce57c1801faf30b89cba4fb
Alessandra Rezende <alessandra.rosarezende@gmail.com>
2021-07-27 21:00:41 -0300

    Requisito2

--- desafio2.sql
+++ desafio2.sql
@@ -6,6 +6,6 @@ SELECT
     WHEN MAX_SALARY BETWEEN 20001 AND 30000 THEN 'Alto'
     WHEN MAX_SALARY > 30000 THEN 'Altíssimo'
     ELSE 'não classificado'
-	END AS 'Nível'
+END AS 'Nível'
 FROM hr.jobs
 ORDER BY JOB_TITLE;
