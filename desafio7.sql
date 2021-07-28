780ce35ca1c817b3836b2eab66d8c310eaafeadf
Henrique 'Rikos' Zózimo <hzozimo@gmail.com>
2021-07-28 15:39:48 -0300

    passei a vassourinha no desafio 7

--- desafio7.sql
+++ desafio7.sql
@@ -1,8 +1,11 @@
 SELECT 
     CONCAT(e.first_name, ' ', e.last_name) AS 'Nome completo',
     jh.start_date AS 'Data de início do cargo',
-    e.salary as 'Salário'
-from hr.employees as e
-inner join hr.job_history as jh on e.EMPLOYEE_ID = jh.EMPLOYEE_ID
-where month(jh.start_date) in (01, 02, 03)
-order by CONCAT(e.first_name, ' ', e.last_name) asc, jh.START_DATE asc;
+    e.salary AS 'Salário'
+FROM
+    hr.employees AS e
+        INNER JOIN
+    hr.job_history AS jh ON e.EMPLOYEE_ID = jh.EMPLOYEE_ID
+WHERE
+    MONTH(jh.start_date) IN (01 , 02, 03)
+ORDER BY CONCAT(e.first_name, ' ', e.last_name) ASC , jh.START_DATE ASC;
