9ebf304e76da2b344d4845744df3535ef7309292
Hugo Simoes <hugolfsimoes@gmail.com>
2021-07-28 17:35:28 -0300

    cosnsertando logica do 7

--- desafio7.sql
+++ desafio7.sql
@@ -1,6 +1,7 @@
-SELECT UCASE(CONCAT(e.FIRST_NAME, " ", e.LAST_NAME)) AS `Nome completo`,
+SELECT DISTINCT UCASE(CONCAT(e.FIRST_NAME, " ", e.LAST_NAME)) AS `Nome completo`,
 h.START_DATE AS `Data de início do cargo`,
 e.SALARY AS Salário
 FROM hr.job_history AS h
 INNER JOIN hr.employees AS e ON e.EMPLOYEE_ID = h.EMPLOYEE_ID
+WHERE MONTH(h.START_DATE) IN(1,2,3)
 ORDER BY CONCAT(e.FIRST_NAME, " ", e.LAST_NAME), h.START_DATE;
