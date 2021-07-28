8581a48e2ccccae88891ca578b7004d819287d0c
Angelo Bittencourt <angelogpbl.info@gmail.com>
2021-07-28 17:43:09 -0300

    desafio 8

---
+++ desafio8.sql
@@ -0,0 +1,11 @@
+SELECT
+  c.ContactName AS "Nome de contato",
+  s.ShipperName AS "Empresa que fez o envio",
+  o.OrderDate AS "Data do pedido"
+FROM w3schools.customers c
+INNER JOIN w3schools.orders o
+  ON c.CustomerID = o.CustomerID
+INNER JOIN w3schools.shippers s
+  ON o.ShipperID = s.ShipperID
+WHERE NOT s.ShipperName = 'Federal Shipping'
+ORDER BY c.ContactName, s.ShipperName, o.OrderDate;
