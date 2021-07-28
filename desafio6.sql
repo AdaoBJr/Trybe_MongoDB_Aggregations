cb62174bb4c6c171361776149735522ebd2f9e01
Daniel <dsouzaborini@gmail.com>
2021-07-28 18:52:12 -0300

    fix req 6

--- desafio6.sql
+++ desafio6.sql
@@ -4,10 +4,10 @@ SELECT
   jh.START_DATE 'Data de início do cargo',
   d.DEPARTMENT_NAME 'Departamento'
 FROM hr.employees e
-INNER JOIN hr.jobs j
-  ON e.JOB_ID = j.JOB_ID
 INNER JOIN hr.job_history jh
   ON e.EMPLOYEE_ID = jh.EMPLOYEE_ID
+INNER JOIN hr.jobs j
+  ON j.JOB_ID = jh.JOB_ID
 INNER JOIN hr.departments d
   ON e.DEPARTMENT_ID = d.DEPARTMENT_ID
 ORDER BY CONCAT(FIRST_NAME, ' ', LAST_NAME) DESC, j.JOB_TITLE;
