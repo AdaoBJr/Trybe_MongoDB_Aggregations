9e26258af5dd8160ca3d4799ca4217f25cd5c048
Matheus Monteiro <monteiromsh@gmail.com>
2021-07-28 18:07:30 -0300

    alteracao em desafio 3

--- desafio3.sql
+++ desafio3.sql
@@ -1,1 +1,1 @@
-SELECT job_title as Cargo, (max_salary - min_salary) as 'Diferença entre Salários' FROM hr.jobs ORDER BY (max_salary - min_salary), job_title;
+SELECT job_title as Cargo, (max_salary - min_salary) as 'Diferença entre salários máximo e mínimo' FROM hr.jobs ORDER BY (max_salary - min_salary), job_title;
