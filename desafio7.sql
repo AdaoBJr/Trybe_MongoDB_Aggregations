7ada1667200a4dc668b32208ef311f2bca8fbea8
Gabriel Essênio <gabriel.essenio@gmail.com>
2021-07-28 19:39:42 -0300

    Desafio do 1 ao 7 do projeto vocabylary-booster

--- desafio7.sql
+++ desafio7.sql
@@ -1,6 +1,7 @@
-SELECT UCASE(CONCAT(e.FIRST_NAME, ' ', e.LAST_NAME)) AS 'Nome completo', 
+SELECT UCASE(CONCAT(e.FIRST_NAME,' ', e.LAST_NAME)) AS 'Nome completo', 
 jhist.START_DATE as 'Data de início',
 e.SALARY as 'Salário'
 FROM hr.employees as e
 INNER JOIN hr.job_history as jhist ON e.JOB_ID = jhist.JOB_ID
+WHERE MONTH(jhist.START_DATE) IN(01,02,03)
 ORDER BY  `Nome completo` ASC, `Data de início` ASC;
