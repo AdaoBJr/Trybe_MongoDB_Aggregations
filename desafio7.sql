49c018cadf031fe2f737757f7b0aedb28182c691
Carlos <cemargato@gmail.com>
2021-07-28 17:27:53 -0300

    acerto-no-alias-salary

--- desafio7.sql
+++ desafio7.sql
@@ -1,7 +1,8 @@
 SELECT UCASE(CONCAT(E.FIRST_NAME, ' ', E.LAST_NAME)) AS 'Nome completo',
 JH.START_DATE AS 'Data de início',
-E.SALARY  FROM hr.employees AS E
+E.SALARY AS 'Salário' 
+FROM hr.job_history AS JH
 INNER JOIN
-hr.job_history AS JH ON JH.EMPLOYEE_ID = E.EMPLOYEE_ID
+hr.employees AS E ON JH.EMPLOYEE_ID = E.EMPLOYEE_ID
 WHERE MONTH(JH.START_DATE) <= 3
 ORDER BY `Nome completo`, `Data de início`;
