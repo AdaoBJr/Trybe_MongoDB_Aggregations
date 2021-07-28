1c374423bce53f22c4808919c54e4107859a085a
joao966 <joaooliveira966@gmail.com>
2021-07-28 04:20:50 -0300

    FIX: request-6 and ADD: request-7

--- desafio6.sql
+++ desafio6.sql
@@ -3,4 +3,4 @@ FROM hr.employees e
 INNER JOIN hr.job_history jh ON e.EMPLOYEE_ID = jh.EMPLOYEE_ID
 INNER JOIN hr.departments d ON d.DEPARTMENT_ID = jh.DEPARTMENT_ID
 INNER JOIN hr.jobs j ON j.JOB_ID = jh.JOB_ID
-ORDER BY CONCAT(e.FIRST_NAME, ' ', e.LAST_NAME) DESC, j.JOB_TITLE;
+ORDER BY `Nome completo` DESC, j.JOB_TITLE;
---
+++ desafio7.sql
@@ -0,0 +1,1 @@
+select concat(ucase(funcionarios.first_name), " ", ucase(funcionarios.last_name)) as 'Nome completo', funcionarios.HIRE_DATE as 'Data de início', salary as 'Salário' from hr.employees as funcionarios inner join hr.job_history as inicio on inicio.EMPLOYEE_ID = funcionarios.EMPLOYEE_ID where month(inicio.start_date) in ('1', '2', '3') order by `Nome completo`, `Data de início`;
