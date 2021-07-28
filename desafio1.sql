55f00264d954bbbba35ef0f31add7bd92d418f35
Lotar Lucas <amaral.lucas.lotar@gmail.com>
2021-07-28 20:47:02 -0300

    Fixed requirement one again

--- desafio1.sql
+++ desafio1.sql
@@ -1,1 +1,5 @@
-SELECT COUNTRY_NAME AS País IF(REGION_ID = 1, "incluído", "não incluído" ) AS 'Status Inclusão' FROM countries ORDER BY COUNTRY_NAME ASC;
+SELECT
+  country_name AS 'País',
+  IF(region_id = 1, 'incluído', 'não incluído') AS 'Status Inclusão'
+FROM countries
+ORDER BY country_name;
