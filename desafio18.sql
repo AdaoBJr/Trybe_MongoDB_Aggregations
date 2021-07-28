0463b101344aacd99f46ac74cc83cbce22c99d52
riquee <strikedv@hotmail.com>
2021-07-28 10:01:01 -0300

    req18 finalizado

---
+++ desafio18.sql
@@ -0,0 +1,11 @@
+/* REFERENCIA PARA CONVERTER DATA
+https://stackoverflow.com/questions/10637581/mysql-date-format-dd-mm-yyyy-select-query
+ */
+SELECT
+	CONCAT(E.FIRST_NAME, ' ',E.LAST_NAME ) 'Nome completo',
+    DATE_FORMAT(JH.START_DATE, '%d/%m/%Y') 'Data de início',
+    DATE_FORMAT(JH.END_DATE, '%d/%m/%Y') 'Data de rescisão',
+    ROUND(DATEDIFF(JH.END_DATE, JH.START_DATE) / 365, 2) 'Anos trabalhados'
+FROM hr.employees E
+	JOIN hr.job_history JH ON JH.EMPLOYEE_ID = E.EMPLOYEE_ID
+ORDER BY `Nome completo`, `Anos trabalhados`;
