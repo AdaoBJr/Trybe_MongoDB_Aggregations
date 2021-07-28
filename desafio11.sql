0bc3e03ddcb6c8fbd735728476c7c70e9f7b0cc2
Gabriel Miranda <gabriel.mirandab92@gmail.com>
2021-07-28 16:34:42 -0300

    arruma exercicio 11

--- desafio11.sql
+++ desafio11.sql
@@ -1,3 +1,4 @@
 SELECT c1.ContactName as 'Nome', c1.Country as 'País', (SELECT COUNT(c2.Country) FROM w3schools.customers as c2 WHERE c1.Country = c2.Country AND c1.ContactName <> c2.ContactName) as 'Número de compatriotas'
 FROM w3schools.customers as c1
+HAVING `Número de compatriotas` <> 0
 ORDER BY `Nome`;
