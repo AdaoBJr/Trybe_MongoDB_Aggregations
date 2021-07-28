9499d858187f4aa6f33cb105715f61de5a23a4ad
riquee <strikedv@hotmail.com>
2021-07-28 12:08:24 -0300

    code climate mil tretas

--- desafio10.sql
+++ desafio10.sql
@@ -4,8 +4,8 @@ SELECT
     MAX(o.Quantity) 'Máxima',
     ROUND(AVG(o.Quantity),2) 'Média'
 FROM w3schools.products p 
-	JOIN w3schools.order_details o ON p.ProductID = o.ProductID
+JOIN w3schools.order_details o ON p.ProductID = o.ProductID
 GROUP BY `Produto`
 HAVING 
-		AVG(o.Quantity) > 20.00
+	AVG(o.Quantity) > 20.00
 ORDER BY `Média`;
