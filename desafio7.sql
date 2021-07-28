309011f74d2091add85d3515e6e3cfdd96d653f4
JonathanFRocha <jonathanferreira.contato@gmail.com>
2021-07-28 14:51:41 -0300

    Sixth and Seventh finished

---
+++ desafio7.sql
@@ -0,0 +1,9 @@
+SELECT
+ CONCAT(UCASE(e.FIRST_NAME), " ", UCASE(e.LAST_NAME)) AS `Nome completo`,
+ jh.START_DATE AS `Data de início`,
+ e.SALARY as "Salário"
+ FROM hr.job_history AS jh
+ INNER JOIN hr.employees AS e ON e.EMPLOYEE_ID = jh.EMPLOYEE_ID
+ WHERE MONTH(jh.START_DATE) IN (1,2,3)
+ ORDER BY `Nome completo`, `Data de início`;
+ 
