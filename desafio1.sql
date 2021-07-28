d072d91601cad79200abd48242df1e4c1fda7b82
root <diegoserafim1@gmail.com>
2021-07-28 19:36:43 -0300

    Exibe os países indicando se eles se encontram na região formada pela Europa

--- desafio1.sql
+++ desafio1.sql
@@ -1,1 +1,8 @@
--- Iniciando o projeto --
+-- Iniciando o projeto
+# Exibe os países indicando se eles se encontram na região formada pela Europa
+
+SELECT countri.COUNTRY_NAME AS País,
+CASE WHEN region.REGION_NAME = 'Europe' THEN 'incluído'
+ELSE 'não incluído'
+END AS 'Status Inclusão' FROM hr.countries AS countri INNER JOIN hr.regions AS region
+ON countri.REGION_ID = region.REGION_ID ORDER BY COUNTRY_NAME;
