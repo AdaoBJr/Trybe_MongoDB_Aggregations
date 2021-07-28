8fdf70810d131336f93f3205ea0e1f4b858bb5de
riquee <strikedv@hotmail.com>
2021-07-28 14:57:13 -0300

    correcao...

--- desafio20.sql
+++ desafio20.sql
@@ -5,12 +5,12 @@ IN in_email VARCHAR(20))
 BEGIN
 SELECT 
 CONCAT(t2.FIRST_NAME, ' ', t2.LAST_NAME) 'Nome completo',
-D.DEPARTMENT_NAME 'Departamento',
-t3.JOB_TITLE 'Cargo'
+t3.DEPARTMENT_NAME 'Departamento',
+t4.JOB_TITLE 'Cargo'
 FROM job_history AS t1
 JOIN employees AS t2 ON t2.EMPLOYEE_ID = t1.EMPLOYEE_ID
-JOIN departments D ON D.DEPARTMENT_ID = t1.DEPARTMENT_ID
-JOIN jobs AS t3 ON t3.JOB_ID = t1.JOB_ID
+JOIN departments AS t3 ON t3.DEPARTMENT_ID = t1.DEPARTMENT_ID
+JOIN jobs AS t4 ON t4.JOB_ID = t1.JOB_ID
 WHERE 
 t2.EMAIL = in_email
 ORDER BY `Departamento`, `Cargo`;
