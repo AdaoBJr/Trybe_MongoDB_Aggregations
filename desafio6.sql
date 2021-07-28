4c50a5c30893d61ac4ff128680efd52cbce253e2
Fernanda Porto Oliveira <fernanda.oliveira.mec@gmail.com>
2021-07-28 16:06:47 -0300

    Desafio 6

---
+++ desafio6.sql
@@ -0,0 +1,16 @@
+SELECT  CONCAT(FIRST_NAME, ' ',LAST_NAME) AS 'Nome completo', 
+cargo.JOB_TITLE AS Cargo,
+e.HIRE_DATE AS 'Data de início do cargo' ,
+d.DEPARTMENT_NAME AS Departamento
+
+FROM job_history AS h
+LEFT JOIN employees AS e
+ON e.EMPLOYEE_ID = h.EMPLOYEE_ID 
+
+RIGHT JOIN jobs AS cargo
+ON cargo.JOB_ID = h.JOB_ID
+
+RIGHT JOIN departments AS d
+ON d.DEPARTMENT_ID = h.DEPARTMENT_ID
+WHERE CONCAT(FIRST_NAME, ' ',LAST_NAME) IS NOT NULL
+ORDER BY CONCAT(FIRST_NAME, ' ',LAST_NAME) DESC;
