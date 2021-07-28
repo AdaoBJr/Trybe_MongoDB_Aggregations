8b076efe007ec7a364d4f11d3aac985f73e4a6c1
Joao Vanelli <vanellijoao@gmail.com>
2021-07-28 19:22:26 -0300

    Req1

--- desafio1.sql
+++ desafio1.sql
@@ -2,4 +2,5 @@ SELECT
     countries.COUNTRY_NAME AS País,
     IF(countries.REGION_ID = 1, 'incluído', 'não incluído') AS 'Status Inclusão'
 FROM
-    hr.countries;
+    hr.countries
+ORDER BY countries.COUNTRY_NAME;
