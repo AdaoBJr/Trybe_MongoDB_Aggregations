ab0e3f2e93e2bcd34d980df4fd1c722f592d3097
Cristian Bugs <cristian.bugs@gmail.com>
2021-07-28 20:52:14 -0300

    change departments

--- desafio6.sql
+++ desafio6.sql
@@ -9,5 +9,5 @@ INNER JOIN hr.job_history AS jh
 INNER JOIN hr.jobs AS j
   ON jh.JOB_ID = j.JOB_ID
 INNER JOIN hr.departments AS d
-  ON e.DEPARTMENT_ID = d.DEPARTMENT_ID
+  ON jh.DEPARTMENT_ID = d.DEPARTMENT_ID
 ORDER BY CONCAT(e.FIRST_NAME, ' ', e.LAST_NAME) DESC, j.job_title;
