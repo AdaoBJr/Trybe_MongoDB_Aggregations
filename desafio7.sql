e9848fd7b8715cd3b5316902446a55612c1dad4f
Fernanda Porto Oliveira <fernanda.oliveira.mec@gmail.com>
2021-07-28 16:55:11 -0300

    Desafio 7

---
+++ desafio7.sql
@@ -0,0 +1,17 @@
+SELECT  CONCAT(UCASE(FIRST_NAME), ' ',UCASE(LAST_NAME)) AS 'Nome completo', 
+
+h.START_DATE AS 'Data de início',
+e.SALARY AS 'Salário'
+
+FROM job_history AS h
+INNER JOIN employees AS e
+ON e.EMPLOYEE_ID = h.EMPLOYEE_ID 
+
+INNER JOIN jobs AS cargo
+ON cargo.JOB_ID = h.JOB_ID
+
+INNER JOIN departments AS d
+ON d.DEPARTMENT_ID = h.DEPARTMENT_ID
+
+ORDER BY CONCAT(FIRST_NAME, ' ',LAST_NAME),
+h.START_DATE;
