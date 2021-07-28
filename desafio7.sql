8a58b5446efe730a1b837112fc727598c3305acd
dennis <dennismarcelo15@gmail.com>
2021-07-28 17:51:02 -0300

    desafio 7 correções

--- desafio7.sql
+++ desafio7.sql
@@ -1,5 +1,5 @@
 SELECT 
-    concat(FIRST_NAME, ' ',LAST_NAME ) AS 'Nome completo',
+    UCASE(concat(FIRST_NAME, ' ',LAST_NAME )) AS 'Nome completo',
     START_DATE AS 'Data de início',
     SALARY AS 'Salário'
 FROM
