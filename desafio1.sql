b1d8310d1913a7b61035b9928e9c32238ceb9412
Carlos <cemargato@gmail.com>
2021-07-28 01:16:47 -0300

    requisitos-1-2-3

--- desafio1.sql
+++ desafio1.sql
@@ -1,1 +1,2 @@
-git
+SELECT country_name AS Pais,
+IF(region_id = 1, 'incluido', 'não incluído') AS 'Status Inclusão' FROM hr.countries ORDER BY country_name;
---
+++ desafio3.sql
@@ -0,0 +1,1 @@
+SELECT job_title AS CARGO, ( max_salary - min_salary) AS "Diferença entre salários máximo e mínimo" FROM hr.jobs ORDER BY 2, job_title;
