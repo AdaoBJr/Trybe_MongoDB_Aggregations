7f33520d1b3713026a38c57041423fbf5f722e18
Mariana Mohr <marianamohr@gmail.com>
2021-07-28 19:03:39 -0300

    req 11 - feito

---
+++ desafio11.sql
@@ -0,0 +1,15 @@
+select * from w3schools.customers;
+SELECT 
+    c1.ContactName AS 'Nome',
+    c1.Country AS 'País',
+    (SELECT 
+            COUNT(*)
+        FROM
+            w3schools.customers c2
+        WHERE
+            c1.Country = c2.Country and c1.ContactName <> c2.ContactName
+                ) AS 'Número de compatriotas'
+FROM
+    w3schools.customers AS c1
+    ORDER BY c1.ContactName ;
+  
