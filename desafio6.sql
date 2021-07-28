7921a80101926cc803cbff34f96a366af24d6d89
Carlos <cemargato@gmail.com>
2021-07-28 16:43:46 -0300

    acerto-no-nome-e-ordenacao

--- desafio6.sql
+++ desafio6.sql
@@ -1,4 +1,4 @@
-SELECT CONCAT(FIRST_NAME,' ',LAST_NAME) AS 'Nome completo',
+SELECT CONCAT(E.FIRST_NAME, ' ', E.LAST_NAME) AS 'Nome completo',
 J.JOB_TITLE AS 'Cargo',
 JH.START_DATE AS 'Data de início do cargo',
 D.DEPARTMENT_NAME AS 'Departamento'
@@ -10,4 +10,4 @@ INNER JOIN
 hr.jobs AS J ON JH.JOB_ID = J.JOB_ID
 INNER JOIN
 hr.departments AS D  ON D.DEPARTMENT_ID = JH.DEPARTMENT_ID
-ORDER BY 'Nome completo' DESC, 'Cargo';
+ORDER BY 1 DESC, 2;
