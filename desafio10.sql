b54a2e3d6dfab6ec5e294d95c477a80ce14a9220
Ederson Rodrigues Araujo <edersonrodara@gmail.com>
2021-07-28 19:48:09 -0300

    fix

--- desafio10.sql
+++ desafio10.sql
@@ -1,1 +1,11 @@
-SELECT p.ProductName AS Produto, min(Quantity) AS 'quantidade mínima', max(Quantity) AS 'quantidade máxima', round(avg(Quantity), 2) AS 'média de quantidade' FROM w3schools.order_details od INNER JOIN w3schools.products p ON od.ProductID = p.ProductID GROUP BY Produto HAVING avg(Quantity) > 20 ORDER BY `média de quantidade`, Produto;
+SELECT
+  p.ProductName AS Produto,
+  MIN(Quantity) AS 'quantidade mínima',
+  MAX(Quantity) AS 'quantidade máxima',
+  ROUND(AVG(Quantity), 2) AS 'média de quantidade'
+FROM w3schools.order_details od
+INNER JOIN w3schools.products p
+  ON od.ProductID = p.ProductID
+GROUP BY Produto
+HAVING AVG(Quantity) > 20
+ORDER BY `média de quantidade`, Produto;
