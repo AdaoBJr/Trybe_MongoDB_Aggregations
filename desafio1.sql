48016142b2f6de29745f59a8f6e79c22896bf02d
Huggo Parcelly <h.parcelly@gmail.com>
2021-07-28 15:02:55 -0300

    correçao incluso/incluido no status

--- desafio1.sql
+++ desafio1.sql
@@ -1,5 +1,5 @@
 SELECT C.COUNTRY_NAME 'País', 
-IF(R.REGION_NAME = 'Europe', 'incluso', 'não incluso') 'Status Inclusão' 
+IF(R.REGION_NAME = 'Europe', 'incluído', 'não incluído') 'Status Inclusão' 
 FROM hr.countries C
 INNER JOIN hr.regions R
 ON R.REGION_ID = C.REGION_ID
