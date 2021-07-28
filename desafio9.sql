d7755d34279a969ec9242db65ebc30bb5f17d722
Felippe <felippecorrea2010@gmail.com>
2021-07-28 18:44:12 -0300

    DONE: desafio9.sql

---
+++ desafio9.sql
@@ -0,0 +1,1 @@
+SELECT CONCAT(FirstName, ' ', LastName) AS 'Nome Completo', COUNT(*) AS 'Total de pedidos' FROM w3schools.employees AS e INNER JOIN w3schools.orders AS o ON o.EmployeeID = e.EmployeeID GROUP BY o.EmployeeID ORDER BY COUNT(*);
