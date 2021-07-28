698925917c8fa9c335be8852d0346628e566fa7c
renanpbraga <renanpbraga@gmail.com>
2021-07-28 18:54:14 -0300

    adiciona Req10

---
+++ desafio10.sql
@@ -0,0 +1,11 @@
+SELECT
+  p.ProductName AS 'Produto',
+  MIN(o.Quantity) AS 'Mínima',
+  MAX(o.Quantity) AS 'Máxima',
+  ROUND(AVG(o.Quantity), 2) AS 'Média'
+FROM products AS p
+INNER JOIN order_details AS o
+  ON p.ProductID = o.ProductID
+GROUP BY p.ProductName
+HAVING AVG(o.Quantity) > 20.00
+ORDER BY AVG(o.Quantity), p.ProductName;
