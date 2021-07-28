3b71b9cb31db4ba9e79b658ec97de337bca29fd1
legalvao1 <legalvao1@gmail.com>
2021-07-28 17:06:56 -0300

    Req 4 - Media salarial e posiçao- erro tab code climate - 4

--- desafio4.sql
+++ desafio4.sql
@@ -2,8 +2,7 @@ SELECT
   J.JOB_TITLE AS 'Cargo', 
     ROUND(AVG(E.SALARY), 2) AS 'Média salarial',
     CASE 
-			WHEN ROUND(AVG(E.SALARY), 2) >= 2000 AND 
-				ROUND(AVG(E.SALARY), 2) <= 5800 THEN 'Júnior'
+		WHEN ROUND(AVG(E.SALARY), 2) >= 2000 AND ROUND(AVG(E.SALARY), 2) <= 5800 THEN 'Júnior'
       WHEN ROUND(AVG(E.SALARY), 2) <= 7500 THEN 'Pleno'
       WHEN ROUND(AVG(E.SALARY), 2) <= 10500 THEN 'Sênior'
       ELSE 'CEO'
