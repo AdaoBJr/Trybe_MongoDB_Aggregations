8ea7cc52f04a95c471992f0f03df7870fe3d3bad
Ederson Rodrigues Araujo <edersonrodara@gmail.com>
2021-07-28 19:16:20 -0300

    teste desafio 11 fix

--- desafio11.sql
+++ desafio11.sql
@@ -1,1 +1,1 @@
-SELECT t1.ContactName AS Nome, t1.Country AS País, count(t1.Country) FROM w3schools.customers t1, w3schools.customers t2 WHERE t1.ContactName <> t2.ContactName AND t1.Country = t2.Country GROUP BY Nome, País ORDER BY Nome;
+SELECT t1.ContactName AS Nome, t1.Country AS País, COUNT(t1.Country) FROM w3schools.customers t1, w3schools.customers t2 WHERE t1.Country = t2.Country AND t1.ContactName <> t2.ContactName GROUP BY t1.ContactName , t1.Country ORDER BY Nome;
