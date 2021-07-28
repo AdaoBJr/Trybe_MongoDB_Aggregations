e18df4b17062b6cec32c45ba1610eaedc2659488
denis-rossati <denis.rossati@hotmail.com>
2021-07-28 17:13:41 -0300

    requisito 18 - correção

--- desafio18.sql
+++ desafio18.sql
@@ -5,4 +5,6 @@ CONCAT(DAY(job_history.END_DATE), "/", MONTH(job_history.END_DATE), "/", YEAR(jo
 YEAR(job_history.END_DATE) - YEAR(job_history.START_DATE) AS "Anos trabalhados"
 FROM hr.job_history AS job_history
 INNER JOIN hr.employees AS employees
-ON job_history.EMPLOYEE_ID = employees.EMPLOYEE_ID;
+ON job_history.EMPLOYEE_ID = employees.EMPLOYEE_ID
+ORDER BY `Nome completo` ASC,
+`Anos trabalhados` ASC;
