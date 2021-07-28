aca90a8e57c9612a149e22801fce4b2e83324285
Cristian Bugs <cristian.bugs@gmail.com>
2021-07-28 20:46:29 -0300

    desafio6 change order of INNER JOIN

--- desafio6.sql
+++ desafio6.sql
@@ -1,13 +1,13 @@
 SELECT
   CONCAT(e.FIRST_NAME, ' ', e.LAST_NAME) AS 'Nome completo',
   j.job_title AS 'Cargo',
-  h.start_date AS 'Data de início do cargo',
-  d.department_name AS 'Departamento'
+  jh.start_date AS 'Data de início do cargo',
+  d.DEPARTMENT_NAME AS 'Departamento'
 FROM hr.employees AS e
+INNER JOIN hr.job_history AS jh
+  ON e.EMPLOYEE_ID = jh.EMPLOYEE_ID
 INNER JOIN hr.jobs AS j
-  ON e.JOB_ID = j.JOB_ID
-INNER JOIN hr.job_history AS h
-  ON e.EMPLOYEE_ID = h.EMPLOYEE_ID
+  ON jh.JOB_ID = j.JOB_ID
 INNER JOIN hr.departments AS d
-  ON h.DEPARTMENT_ID = d.DEPARTMENT_ID
+  ON e.DEPARTMENT_ID = d.DEPARTMENT_ID
 ORDER BY CONCAT(e.FIRST_NAME, ' ', e.LAST_NAME) DESC, j.job_title;
