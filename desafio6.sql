6f7095c8c7a837dc92802095f5cca00fb97c013a
Jonathan Souza <souzajonathan570@gmail.com>
2021-07-28 19:41:59 -0300

    correcao de code climate

--- desafio6.sql
+++ desafio6.sql
@@ -1,12 +1,13 @@
-SELECT CONCAT(e.FIRST_NAME, ' ', e.LAST_NAME) AS "Nome completo",
-	j.JOB_TITLE AS "Cargo",
+SELECT
+  CONCAT(e.FIRST_NAME, ' ', e.LAST_NAME) AS "Nome completo",
+  j.JOB_TITLE AS "Cargo",
   jh.START_DATE AS "Data de início do cargo",
   d.DEPARTMENT_NAME AS "Departamento"
 FROM hr.employees AS e
 INNER JOIN hr.job_history AS jh
-ON e.EMPLOYEE_ID = jh.EMPLOYEE_ID
+  ON e.EMPLOYEE_ID = jh.EMPLOYEE_ID
 INNER JOIN hr.jobs AS j
-ON jh.JOB_ID = j.JOB_ID
+  ON jh.JOB_ID = j.JOB_ID
 INNER JOIN hr.departments AS d
-ON d.DEPARTMENT_ID = jh.DEPARTMENT_ID
+  ON d.DEPARTMENT_ID = jh.DEPARTMENT_ID
 ORDER BY CONCAT(FIRST_NAME, ' ', LAST_NAME) DESC, JOB_TITLE;
