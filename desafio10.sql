72edc7d294e017a872e6006475f1e118f713bcb0
Huggo Parcelly <h.parcelly@gmail.com>
2021-07-28 18:58:56 -0300

    exibe os produtos com media a cima de 20

--- desafio10.sql
+++ desafio10.sql
@@ -1,9 +1,10 @@
-SELECT P.ProductName Produto,
-MIN(OD.Quantity) Mínima,
-MAX(OD.Quantity) Máxima,
-ROUND(AVG(OD.Quantity)) Média
+SELECT P.ProductName 'Produto',
+MIN(OD.Quantity) 'Mínima',
+MAX(OD.Quantity) 'Máxima',
+ROUND(AVG(OD.Quantity), 2) 'Média'
 FROM w3schools.products P
 INNER JOIN w3schools.order_details OD
 ON OD.ProductID = P.ProductID
 GROUP BY OD.ProductID
+HAVING ROUND(AVG(OD.Quantity), 2) > 20.00
 ORDER BY Média, Produto;
