742ca6546698b3b3bb592198d8c11414ab8062f2
denis-rossati <denis.rossati@hotmail.com>
2021-07-27 21:53:57 -0300

    requisito 3 - correção

--- desafio3.sql
+++ desafio3.sql
@@ -1,8 +1,5 @@
 SELECT
 jobs.JOB_TITLE AS "Cargo",
 jobs.MAX_SALARY - jobs.MIN_SALARY AS "Diferença entre salários máximo e mínimo"
-FROM hr.employees employees
-INNER JOIN hr.jobs jobs
-WHERE employees.JOB_ID = jobs.JOB_ID
-GROUP BY jobs.JOB_TITLE
+FROM hr.jobs AS jobs
 ORDER BY `Diferença entre salários máximo e mínimo` ASC, Cargo ASC;
