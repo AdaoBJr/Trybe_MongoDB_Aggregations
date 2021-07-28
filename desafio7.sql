4338aafe5c29d4bb88568b5a507685f54067d2cd
Carlos <cemargato@gmail.com>
2021-07-28 17:20:37 -0300

    retirnado-linha

--- desafio7.sql
+++ desafio7.sql
@@ -1,8 +1,7 @@
-SELECT * FROM hr.job_history;
 SELECT UCASE(CONCAT(E.FIRST_NAME, ' ', E.LAST_NAME)) AS 'Nome completo',
 JH.START_DATE AS 'Data de início',
 E.SALARY  FROM hr.employees AS E
 INNER JOIN
 hr.job_history AS JH ON JH.EMPLOYEE_ID = E.EMPLOYEE_ID
 WHERE MONTH(JH.START_DATE) <= 3
-ORDER BY 1, 2;
+ORDER BY `Nome completo`, `Data de início`;
