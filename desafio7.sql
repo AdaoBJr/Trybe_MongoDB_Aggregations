85391ac7d084d26168e20ddb2088bebbac5d97f8
EderPaiva <edergomesdepaiva12@gmail.com>
2021-07-28 14:59:59 -0300

    requisito 7, upper case and coluna com nom errado

--- desafio7.sql
+++ desafio7.sql
@@ -1,9 +1,9 @@
 -- https://www.eversql.com/sql-order-of-operations-sql-query-order-of-execution/
 select UCASE(concat(employees.FIRST_NAME,' ',employees.LAST_NAME)) as 'Nome completo', 
-historico.START_DATE as 'Data de início do cargo',
+historico.START_DATE as 'Data de início',
 employees.SALARY as 'Salário'
 from hr.job_history as historico  
 inner join hr.employees as employees
 on employees.EMPLOYEE_ID = historico.EMPLOYEE_ID
 where month(historico.START_DATE) between 1 and 3
-order by `Nome Completo` ASC, `Data de início do cargo`ASC
+order by `Nome Completo` ASC, `Data de início`ASC
