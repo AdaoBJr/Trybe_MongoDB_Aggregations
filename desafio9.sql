c2c813aad4a2bef1338da84113214cf8a99bacae
Maiara Borsatti <maiara.borsatti@gmail.com>
2021-07-28 18:27:40 -0300

    desafio 9

---
+++ desafio9.sql
@@ -0,0 +1,8 @@
+SELECT
+  CONCAT(LastName, ' ', LastName) AS 'Nome completo',
+  COUNT(*) AS 'Total de pedidos'
+FROM w3schools.employees AS e
+INNER JOIN w3schools.orders AS o
+  ON e.EmployeeID = o.EmployeeID
+GROUP BY o.EmployeeID
+ORDER BY COUNT(*);
