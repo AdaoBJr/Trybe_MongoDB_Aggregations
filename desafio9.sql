5d7958cb2bc7127adddee9e226a37de1f5c249ea
Jessie <miss_sc_jessi@hotmail.com>
2021-07-28 20:25:39 -0300

    desafio 9

---
+++ desafio9.sql
@@ -0,0 +1,9 @@
+SELECT 
+    CONCAT(e.FirstName, ' ', e.LastName) AS 'Nome Completo',
+    COUNT(*) AS 'Total de pedidos'
+FROM
+    employees AS e
+        INNER JOIN
+    orders AS o ON e.EmployeeID = o.EmployeeID
+GROUP BY `Nome Completo`
+ORDER BY `Total de pedidos`;
