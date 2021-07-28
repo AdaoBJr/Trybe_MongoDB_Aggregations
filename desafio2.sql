df8dc8904fe9b2821f79d488d5bb18426475e1a4
denis-rossati <denis.rossati@hotmail.com>
2021-07-27 21:52:02 -0300

    requisito 2 - correção

--- desafio2.sql
+++ desafio2.sql
@@ -9,8 +9,5 @@ WHEN jobs.MAX_SALARY BETWEEN 20001 AND 30000
 THEN "Alto"
 ELSE "Altíssimo"
 END AS "Nível"
-FROM hr.employees employees
-INNER JOIN hr.jobs jobs
-WHERE employees.JOB_ID = jobs.JOB_ID
-GROUP BY jobs.JOB_TITLE
+FROM hr.jobs jobs
 ORDER BY Cargo ASC;
