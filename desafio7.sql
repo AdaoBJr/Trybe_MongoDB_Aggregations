a8c91060aacdae817f571c7183837a9f27a8edb2
EderPaiva <edergomesdepaiva12@gmail.com>
2021-07-28 14:42:26 -0300

    requisito 7 esta identico porque nao vai

--- desafio7.sql
+++ desafio7.sql
@@ -1,6 +1,5 @@
 -- https://www.eversql.com/sql-order-of-operations-sql-query-order-of-execution/
-select 
-concat(employees.FIRST_NAME,' ',employees.LAST_NAME) as 'Nome completo', 
+select concat(employees.FIRST_NAME,' ',employees.LAST_NAME) as 'Nome completo', 
 historico.START_DATE as 'Data de início do cargo',
 employees.SALARY as 'Salário'
 from hr.job_history as historico  
