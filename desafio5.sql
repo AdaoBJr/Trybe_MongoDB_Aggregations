9fc7a2710ae3dbe5d02a5c0e84903727a5b5474e
Daniel <dsouzaborini@gmail.com>
2021-07-28 18:04:20 -0300

    fix req 5

--- desafio5.sql
+++ desafio5.sql
@@ -3,5 +3,5 @@ SELECT
   MAX_SALARY - MIN_SALARY 'Variação Salarial',
   ROUND(MIN_SALARY / 12, 2) 'Média mínima mensal',
   ROUND(MAX_SALARY / 12, 2) 'Média máxima mensal'
-FROM hr.jobs j
+FROM hr.jobs
 ORDER BY MAX_SALARY - MIN_SALARY, JOB_TITLE;
