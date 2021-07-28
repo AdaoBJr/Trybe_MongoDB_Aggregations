4cc714194ea04c1114280ca4db8f5b743faf8b2b
Joao Vanelli <vanellijoao@gmail.com>
2021-07-28 19:34:14 -0300

    Req2

--- desafio2.sql
+++ desafio2.sql
@@ -1,11 +1,11 @@
 SELECT 
-    jobs.JOB_TITLE AS Cargo,
-    CASE
-		    WHEN jobs.MAX_SALARY <= 10000 THEN 'Baixo'
-        WHEN jobs.MAX_SALARY > 10000 AND jobs.MAX_SALARY <= 20000 THEN 'Médio'
-        WHEN jobs.MAX_SALARY > 20000 AND jobs.MAX_SALARY <= 30000 THEN 'Alto'
-        ELSE 'Altíssimo'
-	  END AS Nível
+  jobs.JOB_TITLE AS Cargo,
+  CASE
+    WHEN jobs.MAX_SALARY <= 10000 THEN 'Baixo'
+    WHEN jobs.MAX_SALARY > 10000 AND jobs.MAX_SALARY <= 20000 THEN 'Médio'
+    WHEN jobs.MAX_SALARY > 20000 AND jobs.MAX_SALARY <= 30000 THEN 'Alto'
+    ELSE 'Altíssimo'
+  END AS Nível
 FROM
-    hr.jobs
+  hr.jobs
 ORDER BY jobs.JOB_TITLE;
