1a4b31ec805c8e1b6d500e435963191468b939b8
Carlos <cemargato@gmail.com>
2021-07-28 01:22:27 -0300

    ajustes-1-2-3

--- desafio1.sql
+++ desafio1.sql
@@ -1,2 +1,2 @@
-SELECT country_name AS Pais,
-IF(region_id = 1, 'incluido', 'não incluído') AS 'Status Inclusão' FROM hr.countries ORDER BY country_name;
+SELECT COUNTRY_NAME AS Pais,
+IF(REGION_ID = 1, 'incluido', 'não incluído') AS 'Status Inclusão' FROM hr.countries ORDER BY COUNTRY_NAME;
