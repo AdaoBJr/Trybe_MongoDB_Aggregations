fbff40eff8ed399c5bf7e891f42a54e5de887282
aythan azevedo <aythan16@gmail.com>
2021-07-28 16:51:31 -0300

    desafio1

---
+++ desafio1.sql
@@ -0,0 +1,8 @@
+SELECT COUNTRY_NAME AS País,
+	IF(EXISTS
+		(SELECT *
+		    FROM hr.regions AS regions
+			WHERE regions.REGION_NAME = 'Europe'
+			AND c.REGION_ID = r.REGION_ID ), 'incluído', 'não incluído') AS 'Status Inclusão'
+FROM hr.countries AS c
+ORDER BY País ;
