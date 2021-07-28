fe00eb6395cd8400dc212e1fedaa03779d0091a6
arthur-menezes202 <arthurmcorrea282@gmail.com>
2021-07-28 17:17:48 -0300

    erros na 1

--- desafio1.sql
+++ desafio1.sql
@@ -1,1 +1,6 @@
-SELECT COUNTRY_NAME AS "Pais", IF(REGION_ID = 1, "incluído", "não incluído") AS "Status Inclusão"  FROM hr.countries ORDER BY COUNTRY_NAME ASC;
+SELECT 
+    c.COUNTRY_NAME AS 'País',
+    IF(r.REGION_NAME = 'Europe', 'incluído', 'não incluído') AS 'Status Inclusão'
+FROM countries c
+INNER JOIN regions r ON c.REGION_ID = r.REGION_ID
+ORDER BY c.COUNTRY_NAME;
