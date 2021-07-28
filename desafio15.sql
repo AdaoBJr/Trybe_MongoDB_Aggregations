15abe75387e2d0f72d832e445d0e4b3b4338fe89
Alessandra Rezende <alessandra.rosarezende@gmail.com>
2021-07-28 14:11:15 -0300

    Requisito15

--- desafio15.sql
+++ desafio15.sql
@@ -3,8 +3,8 @@ DELIMITER $$
 CREATE PROCEDURE buscar_media_por_cargo(IN cargo_name VARCHAR(20))
 BEGIN
 SELECT ROUND(AVG(e.SALARY), 2) 'Média de salários por cargo'
-FROM hr.employees e
-INNER JOIN hr.jobs j ON e.JOB_ID = j.JOB_ID
+FROM hr.jobs j
+INNER JOIN hr.employees e ON e.JOB_ID = j.JOB_ID
 WHERE j.JOB_TITLE = cargo_name;
 END $$
 DELIMITER ;
