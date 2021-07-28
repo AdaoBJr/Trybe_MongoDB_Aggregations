b224030e6f5d5493d61f325388ccefb1f6b0ced4
EderPaiva <edergomesdepaiva12@gmail.com>
2021-07-28 14:15:01 -0300

    desafio7 ok?

--- desafio7.sql
+++ desafio7.sql
@@ -1,9 +1,10 @@
 -- https://www.eversql.com/sql-order-of-operations-sql-query-order-of-execution/
 select 
 concat(employees.FIRST_NAME,' ',employees.LAST_NAME) as 'Nome completo', 
-historico.START_DATE as 'Data de início do cargo'
+historico.START_DATE as 'Data de início do cargo',
+employees.SALARY as 'Salário'
 from hr.job_history as historico  
 inner join hr.employees as employees
 on employees.EMPLOYEE_ID = historico.EMPLOYEE_ID
 where month(historico.START_DATE) between 1 and 3
-order by `Nome Completo` DESC
+order by `Nome Completo` ASC
