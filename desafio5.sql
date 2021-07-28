40770394c5a0aa488ae5618fe5aa05cad50c87d9
Johnny-batata <you@example.com>
2021-07-28 16:55:50 -0300

    requirement 5 done

---
+++ desafio5.sql
@@ -0,0 +1,11 @@
+SELECT 
+    j.JOB_TITLE AS 'Cargo',
+    (j.MAX_SALARY - j.MIN_SALARY) AS 'Variação Salarial',
+    ROUND(MIN(e.SALARY), 2) AS 'Média mínima mensal',
+    ROUND(MAX(e.SALARY), 2) AS 'Média máxima mensal'
+FROM
+    hr.jobs j
+        INNER JOIN
+    hr.employees e ON j.JOB_ID = e.JOB_ID
+GROUP BY JOB_TITLE , MAX_SALARY , MIN_SALARY
+ORDER BY j.MAX_SALARY - j.MIN_SALARY , JOB_TITLE;
