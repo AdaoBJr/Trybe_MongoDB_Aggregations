6c953f33fb3182ccb98cbd8fc808e6781c3be3d6
riquee <strikedv@hotmail.com>
2021-07-28 09:58:09 -0300

    req9 finalizado

---
+++ desafio9.sql
@@ -0,0 +1,7 @@
+SELECT
+	CONCAT(e.FirstName, ' ', e.LastName) 'Nome de contato',
+    COUNT(*) 'Total de pedidos'
+FROM w3schools.orders o
+	JOIN w3schools.employees e ON e.EmployeeID = o.EmployeeID
+GROUP BY `Nome de contato`
+ORDER BY `Total de pedidos`;
