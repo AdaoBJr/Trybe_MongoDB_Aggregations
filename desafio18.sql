fb047020f01f2a80a1a19884e2867449ad7791a1
Thiago Marchini <thiago@msn.com>
2021-07-28 16:41:32 -0300

    Correçao Desafio 18

--- desafio18.sql
+++ desafio18.sql
@@ -1,7 +1,7 @@
 SELECT
 CONCAT(e.FIRST_NAME, ' ', e.LAST_NAME) AS 'Nome completo',
-CONCAT(DAY(h.START_DATE), '/', MONTH(h.START_DATE), '/', YEAR(h.START_DATE)) AS 'Data de início',
-CONCAT(DAY(h.END_DATE), '/', MONTH(h.END_DATE), '/', YEAR(h.END_DATE)) AS 'Data de rescisão',
+DATE_FORMAT(h.START_DATE, '%d/%m/%y') AS 'Data de início',
+DATE_FORMAT(h.END_DATE, '%d/%m/%y') AS 'Data de rescisão',
 ROUND(DATEDIFF(h.END_DATE, h.START_DATE) / 365, 2) AS 'Anos trabalhados'
 FROM hr.job_history AS h
 INNER JOIN hr.employees as e
