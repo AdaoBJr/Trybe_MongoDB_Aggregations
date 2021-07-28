c26b0841e4cbcee6de8f24f5179219eb2777acc2
renanpbraga <renanpbraga@gmail.com>
2021-07-28 16:44:52 -0300

    corrige Req5

--- desafio5.sql
+++ desafio5.sql
@@ -4,7 +4,8 @@ SELECT
   ROUND(AVG(min_salary / 12), 2) AS 'Média mínima mensal',
   ROUND(AVG(max_salary / 12), 2) AS 'Média máxima mensal'
 FROM jobs
-GROUP BY max_salary,
-         min_salary,
-         job_title
+GROUP BY
+          max_salary,
+          min_salary,
+          job_title
 ORDER BY max_salary - min_salary, job_title;
