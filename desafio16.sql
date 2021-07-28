d1f78fb3212ff02be1a7452821091fe4debc0a21
denis-rossati <denis.rossati@hotmail.com>
2021-07-28 16:40:03 -0300

    requisito 16 - desenvolvimento

---
+++ desafio16.sql
@@ -0,0 +1,17 @@
+USE hr;
+DELIMITER $$;
+
+CREATE FUNCTION buscar_quantidade_de_empregos_por_funcionario(input_email VARCHAR(30))
+RETURNS NUMERIC READS SQL DATA
+BEGIN
+DECLARE response INT;
+SELECT COUNT(job_history.JOB_ID)
+FROM hr.job_history AS job_history
+INNER JOIN hr.employees AS employees
+ON employees.EMAIL = input_email
+WHERE employees.EMPLOYEE_ID = job_history.EMPLOYEE_ID
+INTO response;
+RETURN response;
+END $$;
+
+DELIMITER ;
