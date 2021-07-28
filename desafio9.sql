5480b320896d86bd14a5ab7d17d435fb3e4f525e
Daniel <darkdanielr@gmail.com>
2021-07-28 19:45:06 -0300

    corrigindo o 9

--- desafio9.sql
+++ desafio9.sql
@@ -1,5 +1,3 @@
-select * from w3schools.employees;
-select * from w3schools.orders;
 select CONCAT(FirstName, ' ', LastName) as 'Nome completo', COUNT(*) 'Total de pedidos' from w3schools.employees as emplo
 inner join w3schools.orders as ord on ord.EmployeeID = emplo.EmployeeID
 group by ord.EmployeeID
