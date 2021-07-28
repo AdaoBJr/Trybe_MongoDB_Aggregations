b29e8ea25c372d6b6390096508dea75464e29d59
denis-rossati <denis.rossati@hotmail.com>
2021-07-27 21:45:35 -0300

    requisito 3 - correção

--- desafio3.sql
+++ desafio3.sql
@@ -4,4 +4,5 @@ jobs.MAX_SALARY - jobs.MIN_SALARY AS "Diferença entre salários máximo e míni
 FROM hr.employees employees
 INNER JOIN hr.jobs jobs
 WHERE employees.JOB_ID = jobs.JOB_ID
+GROUP BY jobs.JOB_TITLE
 ORDER BY `Diferença entre salários máximo e mínimo` ASC, Cargo ASC;
