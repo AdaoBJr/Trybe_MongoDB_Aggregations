b4b48565dee60ecd290681bcd5cd8ac5d6775331
lucas <lucasportellaagu@gmail.com>
2021-07-28 17:50:38 -0300

    req 11

---
+++ desafio11.sql
@@ -0,0 +1,19 @@
+SELECT 
+    *
+FROM
+    (SELECT 
+        cust1.ContactName AS `Nome`,
+            cust1.Country AS País,
+            (SELECT 
+                    COUNT(cust2.Country) - 1
+                FROM
+                    w3schools.customers AS cust2
+                WHERE
+                    cust2.Country = cust1.Country
+                GROUP BY cust2.Country
+                HAVING (COUNT(cust2.Country) - 1) > 0) AS Número de compatriotas
+    FROM
+        w3schools.customers AS cust1
+    ORDER BY Nome) AS algo
+WHERE
+    Número de compatriotas IS NOT NULL;
