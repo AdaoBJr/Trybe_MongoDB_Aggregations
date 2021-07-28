18f6c47fe6484864c47bba30ccd66213ba05aeca
Renato GSouza <renatomacedo1995@gmail.com>
2021-07-28 20:38:09 -0300

    req11 com group country

--- desafio11.sql
+++ desafio11.sql
@@ -5,6 +5,6 @@ C1.Country 'País',
 FROM
 customers C1
 ,customers C2 WHERE C1.Country = C2.Country
-GROUP BY `Nome`
+GROUP BY `Nome`, C1.Country
 HAVING `Número de compatriotas` > 0    
 ORDER BY `Nome`;
