78059fbf80499acb6c3a6dfbfc8639eb1a329706
Hugo Simoes <hugolfsimoes@gmail.com>
2021-07-28 17:07:45 -0300

    consertando departamento

--- desafio6.sql
+++ desafio6.sql
@@ -1,4 +1,4 @@
-SELECT CONCAT(e.FIRST_NAME, " ", e.LAST_NAME) AS `Nome completo`, j.JOB_TITLE AS Cargo, HIRE_DATE AS `Data de início do cargo`, d.DEPARTMENT_NAME
+SELECT CONCAT(e.FIRST_NAME, " ", e.LAST_NAME) AS `Nome completo`, j.JOB_TITLE AS Cargo, HIRE_DATE AS `Data de início do cargo`, d.DEPARTMENT_NAME AS Departamento
 FROM hr.employees AS e
 INNER JOIN hr.jobs AS j
 ON e.JOB_ID = j.JOB_ID
