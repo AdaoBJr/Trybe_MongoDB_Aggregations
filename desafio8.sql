524d468ddd9bc6d30a0d7508ee9d6e73bae52c08
Jefferson_Andrade <jffrsncrls@gmail.com>
2021-07-28 18:59:58 -0300

    erro de lint2

--- desafio8.sql
+++ desafio8.sql
@@ -7,6 +7,5 @@ INNER JOIN w3schools.orders AS O
   ON O.CustomerID = C.CustomerID
 INNER JOIN w3schools.shippers AS S
   ON S.ShipperID = O.ShipperID
-WHERE 
-	S.ShipperName IN ('Speedy Express' , 'United Package') 
+WHERE S.ShipperName IN ('Speedy Express', 'United Package') 
 ORDER BY `Nome de contato`,`Empresa que fez o envio`, `Data do pedido`;
