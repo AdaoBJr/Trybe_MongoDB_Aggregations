2970003564a1ef94bc6a2da951eba2d02d73a27a
Pedro Amaral <amaralph1995@gmail.com>
2021-07-28 18:13:52 -0300

    Corrige acento na resposta do desafio 1

--- desafio1.sql
+++ desafio1.sql
@@ -1,4 +1,4 @@
-SELECT c.country_name AS 'País', IF(r.region_name = 'Europe', 'incluído', 'não incluido') AS 'Status Inclusão'
+SELECT c.country_name AS 'País', IF(r.region_name = 'Europe', 'incluído', 'não incluído') AS 'Status Inclusão'
 FROM hr.countries AS c
 INNER JOIN hr.regions AS r ON r.region_id = c.region_id
 ORDER BY c.country_name;
