c3dcf12671cd1e6cb3b6cd40585e363dd3e77fb4
legalvao1 <legalvao1@gmail.com>
2021-07-28 17:00:38 -0300

    Req 4 - Media salarial e posiçao- erro tab code climate

--- desafio4.sql
+++ desafio4.sql
@@ -3,9 +3,9 @@ SELECT
     ROUND(AVG(E.SALARY), 2) AS 'Média salarial',
     CASE 
 		WHEN ROUND(AVG(E.SALARY), 2) >= 2000 AND ROUND(AVG(E.SALARY), 2) <= 5800 THEN 'Júnior'
-        WHEN ROUND(AVG(E.SALARY), 2) <= 7500 THEN 'Pleno'
-        WHEN ROUND(AVG(E.SALARY), 2) <= 10500 THEN 'Sênior'
-        ELSE 'CEO'
+		WHEN ROUND(AVG(E.SALARY), 2) <= 7500 THEN 'Pleno'
+		WHEN ROUND(AVG(E.SALARY), 2) <= 10500 THEN 'Sênior'
+		ELSE 'CEO'
     END AS 'Senioridade'
 FROM hr.jobs J
 INNER JOIN hr.employees E
