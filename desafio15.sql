f363bb919722e2ba977bfed4d4c3307bbd96ea6d
lucas <lucasportellaagu@gmail.com>
2021-07-28 20:54:00 -0300

    fix round

--- desafio15.sql
+++ desafio15.sql
@@ -2,7 +2,7 @@ USE hr;
 DELIMITER $$
 CREATE PROCEDURE buscar_media_por_cargo(IN profession VARCHAR(100))
 BEGIN
-SELECT ROUND(AVG(emp.SALARY))AS 'Média salarial' FROM hr.employees AS emp
+SELECT ROUND(AVG(emp.SALARY), 2)AS 'Média salarial' FROM hr.employees AS emp
 INNER JOIN hr.jobs AS job
 ON emp.JOB_ID = job.JOB_ID
 WHERE profession = JOB_TITLE
