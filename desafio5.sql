b0edbbada40f9c572d4be17cf4b5810641eafe51
EderPaiva <edergomesdepaiva12@gmail.com>
2021-07-28 12:55:15 -0300

    desafio 5 muito mais facil que o 4

---
+++ desafio5.sql
@@ -0,0 +1,4 @@
+select JOB_TITLE as Cargo, (MAX_SALARY - MIN_SALARY) as 'Variação Salarial', 
+round(MIN_SALARY/12,2) as 'Média mínima mensal', round(MAX_SALARY/12,2) as 'Média máxima mensal'
+from hr.jobs
+order by `Variação Salarial` ASC, Cargo ASC
