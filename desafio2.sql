cab385786d2464859409fba23c9f6132e56a91ab
Diegho <dieghonm@gmail.com>
2021-07-28 16:33:42 -0300

    Desafio 1 e 2

---
+++ desafio2.sql
@@ -0,0 +1,9 @@
+SELECT JOB_TITLE as "Cargo",
+case
+when MAX_SALARY >= 5000 and MAX_SALARY <= 10000 then 'Baixo'
+when MAX_SALARY >= 10001 and MAX_SALARY <= 20000 then 'Médio'
+when MAX_SALARY >= 20001 and MAX_SALARY <= 30000 then 'Alto'
+when MAX_SALARY >= 30000  then 'Altíssimo'
+end as 'Nível'
+FROM hr.jobs
+order by JOB_TITLE
