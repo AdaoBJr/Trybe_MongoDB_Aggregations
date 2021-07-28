406ec0b30b9f5a34d532d73d6b057d5cb1d78bb1
Jefferson_Andrade <jffrsncrls@gmail.com>
2021-07-28 13:51:43 -0300

    FIXED: Countries

--- desafio1.sql
+++ desafio1.sql
@@ -1,1 +1,1 @@
-SELECT COUNTRY_NAME AS 'País' , IF(REGION_ID = 4, 'incluído','não incluído') AS 'Status Inclusão' FROM hr.countries;
+SELECT COUNTRY_NAME AS 'País' , IF(REGION_ID = 4, 'incluído','não incluído') AS 'Status Inclusão' FROM hr.countrie;
