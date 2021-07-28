b4c2a791d6929563c9747870b8a8236278c3c338
Carlos Henrique Pereira Vieira <78491183+CIGR2021@users.noreply.github.com>
2021-07-28 15:54:49 -0300

    Ordenando...

--- desafio1.sql
+++ desafio1.sql
@@ -1,4 +1,5 @@
 USE hr;
 SELECT COUNTRY_NAME AS 'País',
 (CASE WHEN REGION_ID = 1 THEN 'incluído' ELSE 'não incluído' END) AS 'Status Inclusão'
-FROM hr.countries;
+FROM hr.countries
+ORDER BY COUNTRY_NAME;
