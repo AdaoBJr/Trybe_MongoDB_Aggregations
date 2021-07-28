5b496c2285e8cee2f62db56253e87825a38d1a95
Paulo Xavier <pxavierf@gmail.com>
2021-07-28 16:32:27 -0300

    desafio11 fixing

--- desafio11.sql
+++ desafio11.sql
@@ -3,7 +3,8 @@ Country AS 'País',
 (
 SELECT (COUNT(Country) - 1) 
 FROM w3schools.customers
-WHERE Country = `País`
+WHERE Country = `País` 
 ) AS 'Número de compatriotas'
 FROM w3schools.customers
+HAVING `Número de compatriotas` <> 0 
 ORDER BY `Nome`;
