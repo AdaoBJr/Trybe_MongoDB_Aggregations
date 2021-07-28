4fdb1ca2d94dffc054585d6df250d014e02df2c6
Alessandra Rezende <alessandra.rosarezende@gmail.com>
2021-07-27 22:05:13 -0300

    Requisito7

--- desafio7.sql
+++ desafio7.sql
@@ -1,4 +1,4 @@
-SELECT CONCAT(e.FIRST_NAME, ' ', e.LAST_NAME) 'Nome completo', jh.START_DATE 'Data de início', e.SALARY 'Salário'
+SELECT UCASE(CONCAT(e.FIRST_NAME, ' ', e.LAST_NAME)) 'Nome completo', jh.START_DATE 'Data de início', e.SALARY 'Salário'
 FROM hr.employees e
 INNER JOIN hr.job_history jh ON e.EMPLOYEE_ID = jh.EMPLOYEE_ID
 INNER JOIN hr.departments d ON d.DEPARTMENT_ID = jh.DEPARTMENT_ID
