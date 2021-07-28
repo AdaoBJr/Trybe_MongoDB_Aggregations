92b08c3d7aa965434cb4a48af14cb33b773f5cfc
Leandro Reis <lleandrofr@gmail.com>
2021-07-28 16:54:16 -0300

    displays all products that have already had an associated order requiring a quantity of that product greater than 80

---
+++ desafio13.sql
@@ -0,0 +1,9 @@
+SELECT 
+    p.ProductName AS 'Produto', p.Price AS 'Preço'
+FROM
+    products p
+        INNER JOIN
+    order_details od ON od.ProductID = p.ProductID
+WHERE
+    od.Quantity > 80
+ORDER BY Produto;
