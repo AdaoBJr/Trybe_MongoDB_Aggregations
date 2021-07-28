94e5be0730034642c3e2f95f1474987bf1d221eb
Adão Benites <adaobjr@gmail.com>
2021-07-28 18:32:44 -0400

    feat: :sparkles: Req-09

---
+++ desafio9.sql
@@ -0,0 +1,10 @@
+SELECT
+  CONCAT(e.FirstName,' ',e.LastName) AS 'Nome completo',
+  COUNT(od.EmployeeID) AS 'Total de pedidos'
+
+FROM
+    w3schools.orders AS od
+    INNER JOIN w3schools.employees AS e ON od.EmployeeID = e.EmployeeID
+
+GROUP BY 1
+ORDER BY 2;
