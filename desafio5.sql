d779313e35e51db2574caacc188bd71b05bdc26e
Camila Arruda <camilaam1802@gmail.com>
2021-07-28 18:34:22 -0300

    erros

--- desafio5.sql
+++ desafio5.sql
@@ -3,5 +3,5 @@ SELECT job_title AS Cargo,
 round(AVG(min_salary/12),2) AS 'Média mínima mensal',
 round(AVG(max_salary/12),2) AS 'Média máxima mensal'
 FROM jobs
-GROUP BY job_title
+GROUP BY job_title, `Variação Salarial`
 ORDER BY (max_salary - min_salary) ASC, job_title ASC;
