e7d5edac1b0d5bd5c4285ccb3a468e31565c3182
Jessie <miss_sc_jessi@hotmail.com>
2021-07-28 19:39:36 -0300

    desafio 7

---
+++ desafio7.sql
@@ -0,0 +1,13 @@
+SELECT 
+    UCASE(CONCAT(e.FIRST_NAME, ' ', e.LAST_NAME)) AS 'Nome completo',
+    jh.START_DATE AS 'Data de início do cargo',
+    e.SALARY AS 'Salário'
+FROM
+    job_history AS jh
+        INNER JOIN
+    employees AS e ON jh.EMPLOYEE_ID = e.EMPLOYEE_ID
+        INNER JOIN
+    jobs AS j ON jh.JOB_ID = j.JOB_ID
+WHERE
+    MONTH(START_DATE) IN (1 , 2, 3)
+ORDER BY `Nome completo` , j.JOB_TITLE;
