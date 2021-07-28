3ffbe7cd13cd82327d76b949cefd33bfdc5e65a9
Daniel <darkdanielr@gmail.com>
2021-07-28 19:14:51 -0300

    desafio8.4

--- desafio6.sql
+++ desafio6.sql
@@ -2,4 +2,4 @@ select CONCAT(FIRST_NAME,' ', LAST_NAME) AS 'Nome completo', j.JOB_TITLE as 'Car
 inner join hr.departments AS dep ON emplo.DEPARTMENT_ID = dep.DEPARTMENT_ID
 inner join hr.job_history as hist on emplo.EMPLOYEE_ID = hist.EMPLOYEE_ID
 inner join hr.jobs as j on emplo.JOB_ID = j.JOB_ID
-order by `Nome Completo`, `Cargo`;
+order by `Nome Completo` DESC, `Cargo`;
