8e95899cc6feb4f53908924cc6a12ccb749c25c8
joao966 <joaooliveira966@gmail.com>
2021-07-28 03:35:50 -0300

    ADD: request-05

---
+++ desafio5.sql
@@ -0,0 +1,1 @@
+select job_title as 'Cargo', max_salary - min_salary as 'Variação Salarial', round(min_salary / 12, 2) as 'Média mínima mensal', round(max_salary / 12, 2) as 'Média máxima mensal' from hr.jobs order by max_salary - min_salary, cargo;
