333692059d6fc524fe54bcee71a1ba6932e0406c
Renan0211 <renan.antunes.rocha@gmail.com>
2021-07-28 20:31:24 -0300

    erro de style

--- desafio11.sql
+++ desafio11.sql
@@ -1,9 +1,12 @@
 SELECT 
-    c1.ContactName AS Nome, c1.Country AS País, COUNT(*) AS 'Número de compatriotas'
+    c1.ContactName AS Nome,
+    c1.Country AS País,
+    COUNT(*) AS 'Número de compatriotas'
 FROM
     w3schools.customers AS c1,
     w3schools.customers AS c2
-    WHERE c1.Country = c2.Country
-    GROUP BY Nome, País
-    HAVING `Número de compatriotas` > 1
- ORDER BY Nome;
+WHERE
+    c1.Country = c2.Country
+GROUP BY Nome , País
+HAVING Número de compatriotas > 1
+ORDER BY Nome;
