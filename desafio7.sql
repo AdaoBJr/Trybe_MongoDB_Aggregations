a206dab26a96c8c8993402463854cea98d981808
Daniel <darkdanielr@gmail.com>
2021-07-28 19:11:28 -0300

    desafio8.3

--- desafio7.sql
+++ desafio7.sql
@@ -1,4 +1,4 @@
-select CONCAT(FIRST_NAME,' ', LAST_NAME) AS 'Nome completo', j.START_DATE as 'Data de início', SALARY as 'Salário' FROM hr.employees as emplo
+select UPPER(CONCAT(FIRST_NAME,' ', LAST_NAME)) AS 'Nome completo', j.START_DATE as 'Data de início', SALARY as 'Salário' FROM hr.employees as emplo
 inner join hr.job_history as j on emplo.EMPLOYEE_ID = j.EMPLOYEE_ID
 where month(j.START_DATE) in (1, 2, 3)
 order by `Nome Completo`, j.START_DATE;
