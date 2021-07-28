cf27bea182205d4881d4a5f0cdd83b4e3fc6e0c0
Paulo Xavier <pxavierf@gmail.com>
2021-07-28 13:53:50 -0300

    desafio7

---
+++ desafio7.sql
@@ -0,0 +1,10 @@
+SELECT 
+UPPER(CONCAT(e.FIRST_NAME, ' ', e.LAST_NAME)) AS 'Nome completo',
+jh.START_DATE AS 'Data de início',
+e.SALARY AS 'Salário'
+FROM hr.employees AS e
+INNER JOIN hr.job_history AS jh
+ON jh.EMPLOYEE_ID = e.EMPLOYEE_ID
+WHERE
+MONTH(jh.START_DATE) = 01 OR MONTH(jh.START_DATE) = 02 OR MONTH(jh.START_DATE) = 03 
+ORDER BY `Nome completo`, `Data de início`;
