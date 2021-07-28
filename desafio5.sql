2188704033820276ddb46d8b55db1dcb56559d03
Thiago Leite <leite.tjs@gmail.com>
2021-07-28 19:34:10 -0300

    fix requi 5 agai

--- desafio5.sql
+++ desafio5.sql
@@ -1,6 +1,6 @@
-SELECT job_title AS `Cargo`,
+SELECT job_title AS 'Cargo',
 ROUND((max_salary - min_salary), 2) AS 'Variação Salarial',
 ROUND((min_salary / 12), 2) AS 'Média mínima mensal',
 ROUND((max_salary  / 12), 2) AS 'Média máxima mensal'
 FROM hr.jobs
-ORDER BY `Variação salarial`, `Cargo`;
+ORDER BY `Variação salarial`, 'Cargo';
