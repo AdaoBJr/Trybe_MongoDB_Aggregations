b720505626162e64d9156c5935aea0b336c55b6d
EderPaiva <edergomesdepaiva12@gmail.com>
2021-07-28 13:30:45 -0300

    rei do inner join ?

---
+++ desafio6.sql
@@ -0,0 +1,12 @@
+select 
+concat(employees.FIRST_NAME,' ',employees.LAST_NAME) as 'Nome Completo', 
+jobs.JOB_TITLE as Cargo,
+historico.START_DATE as 'Data de início do cargo',
+departments.DEPARTMENT_NAME AS 'Departamento'
+from hr.job_history as historico
+inner join hr.employees as employees
+on employees.EMPLOYEE_ID = historico.EMPLOYEE_ID
+inner join hr.departments as departments
+on historico.DEPARTMENT_ID = departments.DEPARTMENT_ID
+inner join hr.jobs as jobs
+on historico.JOB_ID = jobs.JOB_ID
