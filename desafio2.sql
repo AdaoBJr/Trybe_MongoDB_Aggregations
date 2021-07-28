ab6b0aef062bab2c43ceb2b73a24959103256894
Thiago Leite <leite.tjs@gmail.com>
2021-07-28 16:11:33 -0300

    quick fix requi1 and requi2

--- desafio2.sql
+++ desafio2.sql
@@ -1,9 +1,9 @@
-SELECT job_title AS 'Cargos', max_salary,
+SELECT job_title AS 'Cargos',
 CASE
   WHEN max_salary <= 10000 THEN 'Baixo'
   WHEN max_salary > 10001 AND max_salary <= 20000 THEN 'Médio'
   WHEN max_salary > 20001 AND max_salary <= 30000 THEN 'Alto'
   ELSE 'Altíssimo'
-END AS 'Nível'
+END AS 'Níveis'
 FROM hr.jobs
 ORDER BY job_title ASC;
