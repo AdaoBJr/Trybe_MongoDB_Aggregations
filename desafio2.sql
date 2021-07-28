234f6788a46ceeb01033e234f9cb9056332a3c1b
isTorres <ismaeltorres612@gmail.com>
2021-07-28 17:01:31 -0300

    add req 4 and trying fix the others

--- desafio2.sql
+++ desafio2.sql
@@ -1,12 +1,13 @@
 SELECT * FROM hr.jobs;
 -- job_title, min_salary and max_salary
 
-SELECT 
-JOB_TITLE as 'Cargo',
+SELECT JOB_TITLE as 'Cargo',
 CASE 
-when MAX_SALARY < 10000 then 'Baixo'
-when MAX_SALARY > 10001 and MAX_SALARY < 20000 then 'Médio'
-when MAX_SALARY > 20001 and MAX_SALARY < 30000 then 'Alto'
-ELSE 'Altíssimo'
+when MAX_SALARY <= 10000 then 'Baixo'
+when MAX_SALARY >= 10001 and MAX_SALARY <= 20000 then 'Médio'
+when MAX_SALARY >= 20001 and MAX_SALARY <= 30000 then 'Alto'
+when MAX_SALARY > 30000 then 'Altíssimo'
+ELSE 'não cadastrado'
 END AS 'Nivel'
-FROM hr.jobs;
+FROM hr.jobs
+ORDER BY JOB_TITLE;
--- desafio3.sql
+++ desafio3.sql
@@ -4,4 +4,4 @@ SELECT * FROM hr.jobs;
 SELECT 
 JOB_TITLE as 'Cargo', (MAX_SALARY - MIN_SALARY) AS 'Diferença entre salários máximo e mínimo'
 FROM hr.jobs
-ORDER BY 'Diferença entre salários máximo e mínimo';
+ORDER BY (MAX_SALARY - MIN_SALARY), JOB_TITLE;
