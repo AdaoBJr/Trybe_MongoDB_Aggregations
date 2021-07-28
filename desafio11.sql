b3c158acce01e4a18c0bb3eec0f365f79e886b2e
Mariana Mohr <marianamohr@gmail.com>
2021-07-28 19:18:38 -0300

    Ajuste req 11

--- desafio11.sql
+++ desafio11.sql
@@ -7,7 +7,8 @@ SELECT
             w3schools.customers c2
         WHERE
             c1.Country = c2.Country
-                AND c1.ContactName <> c2.ContactName) AS 'Número de compatriotas'
+                AND c1.ContactName <> c2.ContactName) AS `Número de compatriotas`
 FROM
     w3schools.customers AS c1
+    having `Número de compatriotas` <> 0
 ORDER BY c1.ContactName;
