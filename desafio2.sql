41feec95ed5328c074336722c8c3d61a400c741e
isTorres <ismaeltorres612@gmail.com>
2021-07-27 22:17:31 -0300

    remove tabs

--- desafio2.sql
+++ desafio2.sql
@@ -2,11 +2,11 @@ SELECT * FROM hr.jobs;
 -- job_title, min_salary and max_salary
 
 SELECT 
-	JOB_TITLE as 'Cargo',
-	CASE 
-		when MAX_SALARY < 10000 then 'Baixo'
-        when MAX_SALARY > 10001 and MAX_SALARY < 20000 then 'Médio'
-        when MAX_SALARY > 20001 and MAX_SALARY < 30000 then 'Alto'
-        ELSE 'Altíssimo'
-	END AS 'Nivel'
+JOB_TITLE as 'Cargo',
+CASE 
+when MAX_SALARY < 10000 then 'Baixo'
+when MAX_SALARY > 10001 and MAX_SALARY < 20000 then 'Médio'
+when MAX_SALARY > 20001 and MAX_SALARY < 30000 then 'Alto'
+ELSE 'Altíssimo'
+END AS 'Nivel'
 FROM hr.jobs;
--- desafio3.sql
+++ desafio3.sql
@@ -2,6 +2,6 @@ SELECT * FROM hr.jobs;
 -- job_title, min_salary and max_salary
 
 SELECT 
-	JOB_TITLE as 'Cargo', (MAX_SALARY - MIN_SALARY) AS 'Diferença entre salários máximo e mínimo'
+JOB_TITLE as 'Cargo', (MAX_SALARY - MIN_SALARY) AS 'Diferença entre salários máximo e mínimo'
 FROM hr.jobs
 ORDER BY 'Diferença entre salários máximo e mínimo';
