3b71a920498a68285ba747bf1bce6ada0caa50b0
Leandro Reis <lleandrofr@gmail.com>
2021-07-28 11:45:47 -0300

    req 2 - order positions by salary level

---
+++ desafio2.sql
@@ -0,0 +1,14 @@
+SELECT 
+    jobs.JOB_TITLE AS 'Cargo',
+    CASE
+        WHEN jobs.MAX_SALARY BETWEEN 5000 AND 10000 THEN 'Baixo'
+        WHEN jobs.MAX_SALARY BETWEEN 10001 AND 20000 THEN 'Médio'
+        WHEN jobs.MAX_SALARY BETWEEN 20001 AND 30000 THEN 'Alto'
+        WHEN jobs.MAX_SALARY > 30000 THEN 'Altíssimo'
+        ELSE 'Não especificado'
+    END AS 'Nível'
+FROM
+    hr.jobs jobs
+ORDER BY Cargo ASC
+
+-- reference: https://www.w3schools.com/mysql/func_mysql_case.asp
