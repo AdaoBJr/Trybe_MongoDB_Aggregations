2c4f9d3d43e5e01e56f7de3b96b34299d98d4a2c
Henrique 'Rikos' Zózimo <hzozimo@gmail.com>
2021-07-28 15:26:54 -0300

    desafio 6 feito

---
+++ desafio6.sql
@@ -0,0 +1,13 @@
+SELECT 
+    CONCAT(e.first_name, ' ', e.last_name) AS 'Nome completo',
+    j.job_title AS 'Cargo',
+    jh.start_date AS 'Data de início do cargo',
+    d.department_name AS 'Departamento'
+FROM
+    hr.employees AS e
+        INNER JOIN
+    hr.job_history AS jh ON e.EMPLOYEE_ID = jh.EMPLOYEE_ID
+        INNER JOIN
+    hr.jobs AS j ON jh.job_id = j.job_id
+        INNER JOIN
+    hr.departments AS d ON jh.DEPARTMENT_ID = d.DEPARTMENT_ID;
