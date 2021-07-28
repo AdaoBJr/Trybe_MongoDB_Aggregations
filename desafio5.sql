aee0b1e38fce112c35bc8b8fd0b83f79f20677d6
Thiago Kachinsky <tjk091@gmail.com>
2021-07-27 22:03:39 -0300

    Requisito 5

--- desafio5.sql
+++ desafio5.sql
@@ -1,5 +1,5 @@
 SELECT JOB_TITLE AS 'Cargo', MAX_SALARY - MIN_SAlARY AS 'Variação Salarial', 
 ROUND(MIN_SALARY, 2) AS 'Média mínima mensal',
 ROUND(MAX_SALARY, 2) AS 'Média máxima mensal'
-FROM jobs
+FROM hr.jobs
 ORDER BY MAX_SALARY - MIN_SAlARY, JOB_TITLE;
