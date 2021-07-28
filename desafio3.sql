d74f94377ce8df64f9ef59639cb346f085cb7266
CamilaCode7 <CamilaCode7>
2021-07-28 18:13:32 -0300

    corrigindo erros

--- desafio3.sql
+++ desafio3.sql
@@ -3,5 +3,4 @@ SELECT
   (MAX_SALARY - MIN_SALARY)
   AS 'Diferença entre salários máximo e mínimo'
 FROM hr.jobs
-WHERE MAX_SALARY <> MIN_SALARY
-ORDER BY JOB_TITLE;
+ORDER BY MAX_SALARY <> MIN_SALARY, JOB_TITLE;
