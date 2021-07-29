2a83d3f03f095d12c4f8be766f4c7a924f1d5022
aythan azevedo <aythan16@gmail.com>
2021-07-28 22:24:53 -0300

    desafio 7 recode 3

--- desafio7.sql
+++ desafio7.sql
@@ -1,7 +1,8 @@
-SELECT UCASE(CONCAT(employees.FIRST_NAME, ' ', employees.LAST_NAME)) AS 'Nome completo',
-		hrs.START_DATE AS 'Data de início',
-		employees.SALARY AS 'Salário'
-FROM hr.job_history hrs
-INNER JOIN hr.employees employees ON employees.EMPLOYEE_ID = hrs.EMPLOYEE_ID
-WHERE MONTH(hrs.START_DATE) IN (1, 2, 3)
-ORDER BY UCASE(CONCAT(employees.FIRST_NAME, ' ', employees.LAST_NAME)), hrs.START_DATE;
+SELECT UCASE(CONCAT(e.FIRST_NAME, ' ', e.LAST_NAME)) AS 'Nome completo',
+        h.START_DATE AS 'Data de início',
+        e.SALARY AS 'Salário'
+FROM hr.job_history h
+INNER JOIN hr.employees e ON e.EMPLOYEE_ID = h.EMPLOYEE_ID
+WHERE MONTH(h.START_DATE) IN (1, 2, 3)
+ORDER BY UCASE(CONCAT(e.FIRST_NAME, ' ', e.LAST_NAME)),
+          h.START_DATE;
