b1035ce96e57e8fc4a5862a94d27f76c7a16a419
Raphael Gumieri <raphaelgumieri@hotmail.com>
2021-07-29 13:03:44 -0300

    desafio 17 - correcao

--- desafio17.sql
+++ desafio17.sql
@@ -4,7 +4,6 @@ CREATE TRIGGER insert_new_OrderDate
 BEFORE INSERT ON w3schools.orders
 FOR EACH ROW
 BEGIN
-SET NEW.OrderDate = DATE(NOW()),
-NEW.acao = 'INSERT';
+SET NEW.OrderDate = DATE(NOW());
 END
 DELIMITER $$ ;
