2f70405901f4021cd8de9e38d39bc8bbfb32a514
legalvao1 <legalvao1@gmail.com>
2021-07-29 12:11:14 -0300

    Req 11 - Clientes que possuem compatriotas - correto

--- desafio11.sql
+++ desafio11.sql
@@ -1,11 +1,11 @@
 SELECT 
 c1.ContactName AS 'Nome',
 c1.Country AS 'País',
-(
-SELECT 
-COUNT(c2.Country)
-FROM w3schools.customers c2
-WHERE c2.Country = c1.Country AND c1.ContactName <> c2.ContactName
-) AS 'Número de compatriotas'
-FROM w3schools.customers as c1
+COUNT(c1.Country) AS 'Número de compatriotas'
+FROM w3schools.customers c1, w3schools.customers c2
+WHERE c2.Country = c1.Country 
+AND c1.ContactName <> c2.ContactName 
+GROUP BY c1.ContactName, c1.Country
+HAVING `Número de compatriotas` > 0
 ORDER BY c1.ContactName;
+
