4feb8d06f049c7514a7630b5105c05758a3b351d
dennis <dennismarcelo15@gmail.com>
2021-07-28 22:35:36 -0300

    desafio 12

---
+++ desafio12.sql
@@ -0,0 +1,13 @@
+SELECT 
+    concat(e1.FIRST_NAME, ' ', e1.LAST_NAME) AS 'Nome completo funcionário 1',
+    e1.SALARY AS 'Salário funcionário 1',
+    e1.PHONE_NUMBER AS 'Telefone funcionário 1',
+    concat(e2.FIRST_NAME, ' ', e2.LAST_NAME) AS 'Nome completo funcionário 2',
+	e2.SALARY AS 'Salário funcionário 2',
+    e2.PHONE_NUMBER AS 'Telefone funcionário 2'
+FROM
+    employees e1,
+    employees e2
+WHERE e1.JOB_ID = e2.JOB_ID AND e1.FIRST_NAME != e2.FIRST_NAME
+ORDER BY e1.FIRST_NAME, e2.FIRST_NAME
+LIMIT 100000;
