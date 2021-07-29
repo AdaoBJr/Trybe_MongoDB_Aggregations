a95925112ed1a0a2a87c6e5943a81c2c670fcf1f
Gabriel Essênio <gabriel.essenio@gmail.com>
2021-07-29 20:31:07 -0300

    Resolvendo erros de lint

--- desafio12.sql
+++ desafio12.sql
@@ -10,5 +10,3 @@ ON e1.JOB_ID = e2.JOB_ID
 WHERE
     CONCAT(e1.FIRST_NAME, ' ', e1.LAST_NAME) <> CONCAT(e2.FIRST_NAME, ' ', e2.LAST_NAME)
 ORDER BY `Nome completo funcionário 1` ASC, `Nome completo funcionário 2` ASC;
-
-
