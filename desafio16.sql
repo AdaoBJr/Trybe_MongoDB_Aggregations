fb02728d589511f8cc587a90a426c9cfb36f9dad
Fernanda Porto Oliveira <fernanda.oliveira.mec@gmail.com>
2021-07-29 12:50:51 -0300

    Desafio 16

---
+++ desafio16.sql
@@ -0,0 +1,17 @@
+USE hr;
+DELIMITER $$
+
+CREATE FUNCTION buscar_quantidade_de_empregos_por_funcionario(email VARCHAR(300))
+RETURNS INT READS SQL DATA
+BEGIN
+    DECLARE jobsCount INT;
+    SELECT COUNT(*)
+    FROM hr.job_history AS t1
+    INNER JOIN hr.employees AS t2
+    ON t1.EMPLOYEE_ID = t2.EMPLOYEE_ID
+    WHERE t2.EMAIL = email INTO jobsCount;
+    RETURN jobsCount;
+END $$
+
+DELIMITER ;
+  SELECT buscar_quantidade_de_empregos_por_funcionario('NKOCHHAR');
