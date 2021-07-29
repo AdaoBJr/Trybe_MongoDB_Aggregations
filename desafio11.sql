10b5fc996a788a749ae9323a45ed0e0ad073222f
Fernanda Porto Oliveira <fernanda.oliveira.mec@gmail.com>
2021-07-28 21:15:32 -0300

    Desafio 11

---
+++ desafio11.sql
@@ -0,0 +1,12 @@
+SELECT
+  t1.ContactName AS 'Nome',
+  t1.Country AS 'País',
+  (SELECT
+    COUNT(*)
+  FROM customers AS t2
+  WHERE t1.Country = t2.Country
+  AND t1.ContactName <> t2.ContactName)
+  AS 'Número de compatriotas'
+FROM customers AS t1
+HAVING `Número de compatriotas` <> 0
+ORDER BY t1.ContactName;
