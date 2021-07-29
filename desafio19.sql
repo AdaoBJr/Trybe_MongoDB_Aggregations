97ebe7fef687f4ccd257bcd0f83bf94c12d0d7ad
André Arnoni <andre_arnoni@yahoo.com.br>
2021-07-29 11:18:04 -0300

    executados requisitos 19 e 20

---
+++ desafio19.sql
@@ -0,0 +1,15 @@
+DELIMITER $$
+CREATE FUNCTION exibir_quantidade_pessoas_contratadas_por_mes_e_ano(use_month INT, use_year INT)
+RETURNS INT READS SQL DATA
+BEGIN
+	DECLARE total_hire INT;
+SELECT
+    COUNT(*) AS 'total'
+FROM
+    hr.employees AS e
+WHERE
+    MONTH(e.HIRE_DATE) = use_month
+        AND YEAR(e.HIRE_DATE) = use_year INTO total_hire;
+    RETURN total_hire;
+END $$
+DELIMITER ;
