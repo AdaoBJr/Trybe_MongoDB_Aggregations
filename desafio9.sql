495cbfcd2450a998ca8b09ee76f3fb9ba95f2701
Victor Lopes <victor.cabrera0209@gmail.com>
2021-07-29 00:49:55 -0300

    Ninth Requirement

---
+++ desafio9.sql
@@ -0,0 +1,1 @@
+SELECT CONCAT(e.FirstName, ' ',e.LastName )AS 'Nome completo', COUNT(o.OrderID) AS 'Total de pedidos' FROM w3schools.employees e INNER JOIN orders o ON o.EmployeeID = e.EmployeeID GROUP BY e.EmployeeID ORDER BY COUNT(o.OrderID);
