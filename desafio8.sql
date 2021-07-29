809f745f25781decf57f8442f50a24904375eac5
Diegho <dieghonm@gmail.com>
2021-07-29 00:46:56 -0300

    Desafio 9 e 10

--- desafio8.sql
+++ desafio8.sql
@@ -1,7 +1,8 @@
-select c.CustomerName as "Nome de contato",
-s.ShipperName as "Empresa que fez o envio",
-o.OrderDate as "Data do pedido"
+select c.ContactName as 'Nome de contato',
+s.ShipperName as 'Empresa que fez o envio',
+o.OrderDate as 'Data do pedido'
 from w3schools.customers as c
-inner join w3schools.orders as o on c.CustomerID = o.CustomerID and o.ShipperID < 3
+inner join w3schools.orders as o on c.CustomerID = o.CustomerID
 inner join w3schools.shippers as s on s.ShipperID = o.ShipperID
+where o.ShipperID in (1,2)
 order by `Nome de contato`, `Empresa que fez o envio`, `Data do pedido`
