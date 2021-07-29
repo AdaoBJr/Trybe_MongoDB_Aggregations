9d831c511a00f2de7eeb2c5fd6111e017cba6a01
EderPaiva <edergomesdepaiva12@gmail.com>
2021-07-29 10:10:01 -0300

    requisito 14 alias

--- desafio14.sql
+++ desafio14.sql
@@ -1,5 +1,6 @@
-select distinct Country from w3schools.customers
-union 
-select distinct Country from w3schools.suppliers
-order by Country
+-- https://pt.stackoverflow.com/questions/79846/uma-query-mysql-com-crases-vs-sem
+select distinct Country as 'País' from w3schools.customers 
+union
+select distinct Country from w3schools.suppliers as suppliers
+order by `País`
 limit 5
