8782cb3cfdc5b3a172f8ae8e001d5ddfcff92b63
isTorres <ismaeltorres612@gmail.com>
2021-07-29 13:26:07 -0300

    fix req 11

--- desafio11.sql
+++ desafio11.sql
@@ -1,9 +1,7 @@
-SELECT * FROM w3schools.customers;
--- CustomerName AS `Nome`,
--- COUNT(*) - 1 AS `Compatriotas`
-
-SELECT CustomerName AS `Nome`, Country AS `País`, COUNT(*) - 1 AS `Compatriotas`
-FROM w3schools.customers
-GROUP BY Country;
-
-
+SELECT t1.ContactName AS `Nome`, t1.Country AS `País`,
+(SELECT COUNT(*) FROM w3schools.customers t2
+WHERE t1.Country = t2.Country 
+AND t1.ContactName <> t2.ContactName) `Número de compatriotas`
+FROM w3schools.customers t1
+HAVING `Número de compatriotas` <> 0
+ORDER BY t1.ContactName;
