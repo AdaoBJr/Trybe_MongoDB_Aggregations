5b6a9b2d8fba02ed50dc7ad00184af809dd17a17
root <diegoserafim1@gmail.com>
2021-07-29 12:36:37 -0300

    relatório que mostra o histórico de cargos das pessoas empregadas

---
+++ desafio6.sql
@@ -0,0 +1,12 @@
+-- Iniciando o projeto
+# relatório que mostra o histórico de cargos das pessoas empregadas
+
+SELECT CONCAT(employe.FIRST_NAME, ' ', LAST_NAME) AS 'Nome completo',
+job.JOB_TITLE AS 'Cargo',
+job_h.START_DATE AS 'Data de início do cargo',
+departament.DEPARTMENT_NAME AS 'Departamento'
+FROM hr.job_history AS job_h
+INNER JOIN hr.employees AS employe ON job_h.EMPLOYEE_ID = employe.EMPLOYEE_ID
+INNER JOIN hr.jobs AS job ON job_h.JOB_ID = job.JOB_ID
+INNER JOIN hr.departments AS departament ON job_h.DEPARTMENT_ID = departament.DEPARTMENT_ID
+ORDER BY 'Nome completo' DESC, 'Cargo' ASC;
