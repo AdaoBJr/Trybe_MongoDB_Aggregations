4e0efc49434f6df0bc72c875c45dbebe8864e2a6
Gabriel Miranda <gabriel.mirandab92@gmail.com>
2021-07-29 08:59:39 -0300

    arruma exercicio 18

--- desafio18.sql
+++ desafio18.sql
@@ -1,8 +1,8 @@
 SELECT
 CONCAT(FIRST_NAME, ' ', LAST_NAME) as 'Nome completo',
-CONCAT(DAY(jh.START_DATE), '/', MONTH(jh.START_DATE), '/', YEAR(jh.START_DATE)) as 'Data de início',
-CONCAT(DAY(jh.END_DATE), '/', MONTH(jh.END_DATE), '/', YEAR(jh.END_DATE)) as 'Data de rescisão',
-(YEAR(jh.END_DATE) - YEAR(jh.START_DATE)) as 'Anos trabalhados'
+CONCAT(SUBSTRING(jh.START_DATE, 9,2), '/', SUBSTRING(jh.START_DATE, 6, 2), '/', SUBSTRING(jh.START_DATE, 1, 4)) as 'Data de início',
+CONCAT(SUBSTRING(jh.END_DATE, 9,2), '/', SUBSTRING(jh.END_DATE, 6, 2), '/', SUBSTRING(jh.END_DATE, 1, 4)) as 'Data de rescisão',
+ROUND(DATEDIFF(jh.END_DATE,jh.START_DATE)/365, 2) as 'Anos trabalhados'
 FROM hr.employees as e
 INNER JOIN hr.job_history as jh
 ON jh.EMPLOYEE_ID = e.EMPLOYEE_ID
