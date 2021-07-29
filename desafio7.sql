3c56f1eaea57993beeb1daab933494ab694a7d53
Victor Lopes <victor.cabrera0209@gmail.com>
2021-07-28 23:04:08 -0300

    Seventh Requirement trying fourth time

--- desafio7.sql
+++ desafio7.sql
@@ -1,1 +1,12 @@
-SELECT DISTINCT UPPER(CONCAT(e.FIRST_NAME, ' ', e.LAST_NAME)) AS 'Nome completo', h.START_DATE AS 'Data de início', e.SALARY AS Salário FROM hr.employees e INNER JOIN job_history h  ON e.EMPLOYEE_ID = h.EMPLOYEE_ID AND ((h.START_DATE LIKE '%-01-%') OR (h.START_DATE LIKE '%-02-%') OR (h.START_DATE LIKE '%-03-%')) ORDER BY CONCAT(e.FIRST_NAME, ' ', e.LAST_NAME) , h.START_DATE;
+SELECT
+    UPPER(CONCAT(e.FIRST_NAME, ' ', e.LAST_NAME)) AS 'Nome completo',
+    h.START_DATE AS 'Data de início',
+    e.SALARY AS Salário
+FROM
+    hr.employees e
+        INNER JOIN
+    job_history h ON e.EMPLOYEE_ID = h.EMPLOYEE_ID
+        AND ((h.START_DATE LIKE '%-01-%')
+        OR (h.START_DATE LIKE '%-02-%')
+        OR (h.START_DATE LIKE '%-03-%'))
+ORDER BY CONCAT(e.FIRST_NAME, ' ', e.LAST_NAME) , h.START_DATE;
