1756e375fb6096c551b0774d8e2ced13b9b93bf7
Hugo Simoes <hugolfsimoes@gmail.com>
2021-07-28 21:06:02 -0300

    alterando ordem

--- desafio15.sql
+++ desafio15.sql
@@ -1,8 +1,9 @@
+USE hr;
 DELIMITER $$
 CREATE PROCEDURE buscar_media_por_cargo(IN cargo VARCHAR(30))
 BEGIN
-	SELECT ROUND(AVG(e.SALARY),2) AS 'Média salarial' FROM hr.employees AS e
-    INNER JOIN hr.jobs AS j ON e.JOB_ID = j.JOB_ID
-    WHERE cargo = j.JOB_TITLE;
+SELECT ROUND(AVG(e.SALARY),2) AS 'Média salarial' FROM hr.employees AS e
+INNER JOIN hr.jobs AS j ON e.JOB_ID = j.JOB_ID
+WHERE cargo = j.JOB_TITLE;
 END $$
 DELIMITER ;
