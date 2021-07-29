958bcc823f943c73eeaa2cd529539dd7b6c8fbf3
CamilaCode7 <CamilaCode7>
2021-07-29 11:15:41 -0300

    adicionando req 6

--- desafio6.sql
+++ desafio6.sql
@@ -1,10 +1,12 @@
 select
-  concat(E.FIRST_NAME,' ', E.LAST_NAME)  as `Nome completo`,
+  concat(E.FIRST_NAME, ' ', E.LAST_NAME)  as `Nome completo`,
   J.JOB_TITLE as 'Cargo',
   JH.START_DATE as 'Data de início do cargo',
   D.DEPARTMENT_NAME as 'Departamento'
 from hr.employees as E
-inner join hr.job_history as JH on JH.employee_id  = E.employee_id
-inner join hr.jobs as J on E.job_id = J.job_id 
-inner join hr.departments as D on D.department_id = E.department_id
+inner join hr.job_history as JH
+on E.employee_id = JH.employee_id
+inner join hr.jobs as J on JH.job_id = J.job_id
+inner join hr.departments as D
+on JH.department_id = D.department_id
 order by `Nome completo` desc, Cargo asc;
