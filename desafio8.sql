7494ddd6207d5876d0fadc6974149b70d41ba6a4
legalvao1 <legalvao1@gmail.com>
2021-07-28 22:29:50 -0300

    Req 8 - pessoas consumidoras cujos pedidos já foram enviados pelas empresas 'Speedy Express' ou 'United Package'

---
+++ desafio8.sql
@@ -0,0 +1,8 @@
+SELECT 
+C.ContactName AS 'Nome de contato',
+S.ShipperName AS 'Empresa que fez o envio',
+O.OrderDate AS 'Data do pedido'
+FROM w3schools.customers C
+INNER JOIN w3schools.orders O ON O.CustomerID = C.CustomerID
+INNER JOIN w3schools.shippers S ON S.ShipperID = O.ShipperID 
+ORDER BY `Nome de contato`, `Empresa que fez o envio`, `Data do pedido`;
