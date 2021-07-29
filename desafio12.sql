4ec2b23f122f43f4780117096d82aa1ee5f9c33c
Raphael Gumieri <raphaelgumieri@hotmail.com>
2021-07-29 11:18:39 -0300

    desafio 12 - completo

---
+++ desafio12.sql
@@ -0,0 +1,12 @@
+select * from hr.employees;
+SELECT
+  CONCAT(e1.FIRST_NAME, ' ', e1.LAST_NAME) AS 'Nome completo funcionário 1', 
+  e1.SALARY AS 'Salário funcionário 1',
+  e1.PHONE_NUMBER AS 'Telefone funcionário 1',
+  CONCAT(e2.FIRST_NAME, ' ', e2.LAST_NAME) AS 'Nome completo funcionário 2',
+  e2.SALARY AS 'Salário funcionário 2',
+  e2.PHONE_NUMBER AS 'Telefone funcionário 2'
+FROM hr.employees AS e1, hr.employees AS e2
+WHERE e1.JOB_ID = e2.JOB_ID and e1.EMPLOYEE_ID <> e2.EMPLOYEE_ID
+GROUP BY `Nome completo funcionário 1`, `Nome completo funcionário 2`, e1.SALARY, e2.SALARY, e1.PHONE_NUMBER, e2.PHONE_NUMBER
+ORDER BY `Nome completo funcionário 1`, `Nome completo funcionário 2`;
