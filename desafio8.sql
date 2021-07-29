830e6b79d757b6f0001bfd90b2e69111f2840283
root <diegoserafim1@gmail.com>
2021-07-29 16:20:42 -0300

    Consumidores cujos pedidos já foram enviados pelas empresas Speedy Express ou United Package

---
+++ desafio8.sql
@@ -0,0 +1,11 @@
+-- Iniciando o projeto
+# Consumidores cujos pedidos já foram enviados pelas empresas Speedy Express ou United Package
+
+SELECT customer.ContactName AS 'Nome de contato',
+shipper.ShipperName AS 'Empresa que fez o envio',
+orders.OrderDate AS 'Data do pedido'
+FROM w3schools.customers AS customer
+INNER JOIN w3schools.orders AS orders ON customer.CustomerID = orders.CustomerID
+INNER JOIN w3schools.shippers AS shipper ON orders.ShipperID = shipper.ShipperID
+WHERE orders.ShipperID IN (1, 2)
+ORDER BY `Nome de contato` ASC, `Empresa que fez o envio` ASC, `Data do pedido` ASC;
