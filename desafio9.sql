af34bf945de1e1c23f29f27c7a00763ab3479d97
Cristian Bugs <cristian.bugs@gmail.com>
2021-07-28 21:23:09 -0300

    didnt save the previous query

--- desafio9.sql
+++ desafio9.sql
@@ -1,1 +1,8 @@
-SELECT concat(FirstName, ' ', LastName) AS 'Nome Completo' FROM w3schools.employees; 
+SELECT concat(e.FirstName, ' ', e.LastName) AS 'Nome Completo',
+COUNT(o.EmployeeID) AS 'Total de Pedidos'
+FROM w3schools.employees as e
+INNER JOIN
+w3schools.orders AS o
+ON o.employeeID = e.employeeID
+GROUP BY o.EmployeeID
+ORDER BY COUNT(o.EmployeeID);
