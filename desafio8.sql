d0c1f3d0157bf60b18c487df16a6f864085f7a88
André Arnoni <andre_arnoni@yahoo.com.br>
2021-07-29 12:11:53 -0300

    arrumado requisito 8

--- desafio8.sql
+++ desafio8.sql
@@ -1,9 +1,9 @@
 SELECT customers.ContactName AS 'Nome de contato', shippers.ShipperName AS 'Empresa que fez o envio', orders.OrderDate AS 'Data do pedido'
 FROM w3schools.customers AS customers
-INNER JOIN w3schools.shippers AS shippers
 INNER JOIN w3schools.orders AS orders
-ON shippers.ShipperID = orders.ShipperID
 ON orders.CustomerID = customers.CustomerID
+INNER JOIN w3schools.shippers AS shippers
+ON shippers.ShipperID = orders.ShipperID
 WHERE shippers.ShipperName = 'Speedy Express'
 OR shippers.ShipperName = 'United Package'
-ORDER BY customers.ContactName, shippers.ShipperName, orders.OrderDate ASC;
+ORDER BY customers.ContactName ASC, shippers.ShipperName ASC, orders.OrderDate ASC;
