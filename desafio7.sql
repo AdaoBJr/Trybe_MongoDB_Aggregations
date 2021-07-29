625d6418e1952764e4542a38c065728407ffa042
Cristian Bugs <cristian.bugs@gmail.com>
2021-07-28 21:49:55 -0300

    save the updates

--- desafio7.sql
+++ desafio7.sql
@@ -5,4 +5,5 @@ SELECT
 FROM hr.employees AS e
 INNER JOIN hr.job_history AS h
   ON e.EMPLOYEE_ID = h.EMPLOYEE_ID
+  WHERE MONTH(h.start_date) IN (1, 2, 3)
 ORDER BY ucase(CONCAT(e.first_name, ' ', e.last_name)) ASC, h.start_date ASC;
