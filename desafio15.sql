dcd5597b804aa76b2425f428980627718ef43c0f
Renan0211 <renan.antunes.rocha@gmail.com>
2021-07-29 13:19:02 -0300

    resolve erro de styles

--- desafio15.sql
+++ desafio15.sql
@@ -3,12 +3,12 @@ DELIMITER $$
 
 CREATE PROCEDURE buscar_media_por_cargo(IN jobName VARCHAR(30))
 BEGIN
-	SELECT 
-		ROUND(AVG(SALARY)) 
-    FROM 
-		employees 
+SELECT
+	ROUND(AVG(SALARY))
+    FROM
+	employees
     WHERE JOB_ID = (
-		SELECT JOB_ID FROM jobs WHERE JOB_TITLE = jobName
+	SELECT JOB_ID FROM jobs WHERE JOB_TITLE = jobName
     );
 END $$
 
