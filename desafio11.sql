d47d2881dec7f3e76672b4d9d1d7745b4e72dd7d
Renan0211 <renan.antunes.rocha@gmail.com>
2021-07-29 11:29:25 -0300

    refatora o desafio 11

--- desafio11.sql
+++ desafio11.sql
@@ -1,12 +1,12 @@
 SELECT 
     c1.ContactName AS Nome,
     c1.Country AS País,
-    COUNT(*) AS 'Número de compatriotas'
+    (COUNT(c2.country) - 1) AS 'Número de compatriotas'
 FROM
     w3schools.customers AS c1,
     w3schools.customers AS c2
 WHERE
-    c1.Country = c2.Country AND c1.ContactName <> c2.ContactName
-GROUP BY Nome , País
-HAVING `Número de compatriotas` > 1
+    c1.Country = c2.Country
+GROUP BY c1.Country , c2.Country , c1.ContactName
+HAVING Número de compatriotas > 0
 ORDER BY Nome;
