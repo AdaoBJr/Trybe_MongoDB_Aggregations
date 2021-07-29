0450bda8e496c899a68526eb4b9d4d25b91bcba9
Gabriel Miranda <gabriel.mirandab92@gmail.com>
2021-07-29 08:45:54 -0300

    add exercicios 17, 18 e 19

---
+++ desafio18.sql
@@ -0,0 +1,9 @@
+SELECT
+	CONCAT(FIRST_NAME, ' ', LAST_NAME) as 'Nome completo',
+	CONCAT(DAY(jh.START_DATE), '/', MONTH(jh.START_DATE), '/', YEAR(jh.START_DATE)) as 'Data de início',
+	CONCAT(DAY(jh.END_DATE), '/', MONTH(jh.END_DATE), '/', YEAR(jh.END_DATE)) as 'Data de rescisão',
+	(YEAR(jh.END_DATE) - YEAR(jh.START_DATE)) as 'Anos trabalhados'
+FROM hr.employees as e
+INNER JOIN hr.job_history as jh
+ON jh.EMPLOYEE_ID = e.EMPLOYEE_ID
+ORDER BY `Nome completo`, `Anos trabalhados`;
