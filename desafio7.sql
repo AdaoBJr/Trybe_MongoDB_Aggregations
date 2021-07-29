846a777b44195995ea9673b5844e4368467f1554
aythan azevedo <aythan16@gmail.com>
2021-07-28 22:17:47 -0300

    desafio 7 recode

--- desafio7.sql
+++ desafio7.sql
@@ -3,7 +3,6 @@ SELECT UCASE(CONCAT(employees.FIRST_NAME, ' ', employees.LAST_NAME)) AS 'Nome co
 		employees.SALARY AS 'Salário'
 FROM hr.job_history hrs
 INNER JOIN hr.employees employees ON employees.EMPLOYEE_ID = hrs.EMPLOYEE_ID
-WHERE MONTH(hrs.START_DATE) IN (1,
-								2,
-								3)
-ORDER BY UCASE(CONCAT(employees.FIRST_NAME, ' ', employees.LAST_NAME)), hrs.START_DATE;
+WHERE MONTH(hrs.START_DATE) IN (1, 2, 3)
+ORDER BY UCASE(CONCAT(employees.FIRST_NAME, ' ', employees.LAST_NAME)),
+		  hrs.START_DATE;
