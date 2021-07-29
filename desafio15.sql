1e12f8054c0e1a382c9e0ddee65be3f858ec5289
Jefferson_Andrade <jffrsncrls@gmail.com>
2021-07-29 11:17:20 -0300

    Done 15

---
+++ desafio15.sql
@@ -0,0 +1,14 @@
+USE hr; 
+DELIMITER $$ 
+
+CREATE PROCEDURE buscar_media_por_cargo(IN job VARCHAR(100))
+BEGIN 
+SELECT ROUND(AVG(E.SALARY),2) FROM hr.employees AS E
+INNER JOIN hr.jobs AS J
+ON J.JOB_ID = E.JOB_ID
+WHERE J.JOB_TITLE = job
+GROUP BY E.JOB_ID;
+
+END $$ 
+
+DELIMITER ;
