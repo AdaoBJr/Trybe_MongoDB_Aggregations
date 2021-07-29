bcd97e83a6096efd784c70bc0e9a7240c3f302d7
Leandro Reis <lleandrofr@gmail.com>
2021-07-29 12:09:03 -0300

    fix codeclimate issue

--- desafio17.sql
+++ desafio17.sql
@@ -3,9 +3,9 @@ USE w3schools;
 DELIMITER $$
 CREATE TRIGGER UpdateOrderDate
 	AFTER INSERT ON orders
-    FOR EACH ROW
+  FOR EACH ROW
 BEGIN
-	SET NEW.OrderDate = NOW();
+  SET NEW.OrderDate = NOW();
 END $$
 
 DELIMITER ;
