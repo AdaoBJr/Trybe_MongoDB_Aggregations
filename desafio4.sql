4d82ed589fd8b003efd8bac929d7d619f6e2456f
root <diegoserafim1@gmail.com>
2021-07-28 22:04:03 -0300

    erro apresentado no github, não é mostrado no localhost

--- desafio4.sql
+++ desafio4.sql
@@ -1,8 +1,7 @@
 -- Iniciando o projeto
 # média salarial e o nível de senioridade das pessoas empregadas, agrupadas pelo cargo
  
-SELECT jobs.JOB_TITLE AS 'CARGO',
-ROUND(AVG(employees.SALARY), 2) AS 'Média salarial',
+SELECT jobs.JOB_TITLE AS 'CARGO', ROUND(AVG(employees.SALARY), 2) AS 'Média salarial',
 CASE
 WHEN ROUND(AVG(employees.SALARY), 2) BETWEEN 2000 AND 5800 THEN 'Júnior'
 WHEN ROUND(AVG(employees.SALARY), 2) BETWEEN 5801 AND 7500 THEN 'Pleno'
@@ -10,5 +9,7 @@ WHEN ROUND(AVG(employees.SALARY), 2) BETWEEN 7501 AND 10500 THEN 'Sênior'
 ELSE 'CEO'
 END AS 'Senioridade'
 FROM hr.jobs
-INNER JOIN hr.employees ON employees.JOB_ID = jobs.JOB_ID
-GROUP BY JOB_TITLE ORDER BY ROUND(AVG(employees.SALARY), 2) ASC, JOB_TITLE ASC;
+INNER JOIN hr.employees
+ON employees.JOB_ID = jobs.JOB_ID
+GROUP BY JOB_TITLE
+ORDER BY ROUND(AVG(employees.SALARY), 2) ASC, JOB_TITLE ASC;
