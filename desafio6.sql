1c37507d65be6fc1b970e1d9b098b89500372252
MarceloPelegrino <pelegrinocorrea@gmail>
2021-07-29 15:40:14 -0300

    Desafio 6 - Corrigindo Code Climate 1

--- desafio6.sql
+++ desafio6.sql
@@ -1,14 +1,14 @@
-SELECT
-	CONCAT(FIRST_NAME, ' ', LAST_NAME) AS 'Nome completo',
+SELECT 
+    CONCAT(FIRST_NAME, ' ', LAST_NAME) AS 'Nome completo',
     job.JOB_TITLE AS Cargo,
     his.START_DATE AS 'Data de início do cargo',
     de.DEPARTMENT_NAME AS Departamento
 FROM
-	hr.employees AS em
-        INNER JOIN job_history AS his
-        ON em.EMPLOYEE_ID = his.EMPLOYEE_ID
-		INNER JOIN jobs AS job
-        ON his.JOB_ID = job.JOB_ID
-        INNER JOIN departments AS de
-        ON his.DEPARTMENT_ID = de.DEPARTMENT_ID
-ORDER BY `Nome completo` DESC, `Cargo`;
+    hr.employees AS em
+        INNER JOIN
+    job_history AS his ON em.EMPLOYEE_ID = his.EMPLOYEE_ID
+        INNER JOIN
+    jobs AS job ON his.JOB_ID = job.JOB_ID
+        INNER JOIN
+    departments AS de ON his.DEPARTMENT_ID = de.DEPARTMENT_ID
+ORDER BY `Nome completo` DESC , `Cargo`;
