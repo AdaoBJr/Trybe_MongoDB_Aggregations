7bb642a3ac844d6f27d3a130bc2a523664a1e49c
Joao Vanelli <vanellijoao@gmail.com>
2021-07-29 12:18:24 -0300

    Req4

--- desafio4.sql
+++ desafio4.sql
@@ -1,8 +1,7 @@
 SELECT 
-	(
-    SELECT jobs.JOB_TITLE
-    FROM hr.jobs
-    WHERE jobs.JOB_ID = hr.employees.JOB_ID
+	(SELECT jobs.JOB_TITLE
+  FROM hr.jobs
+  WHERE jobs.JOB_ID = hr.employees.JOB_ID
   ) AS Cargo,
   ROUND(SUM(employees.SALARY) / COUNT(*)) AS 'Média salarial',
   CASE
