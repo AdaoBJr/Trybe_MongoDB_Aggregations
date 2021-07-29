08dd5de1d028ddd4a831e3a8c0f517cf1a68b607
EderPaiva <edergomesdepaiva12@gmail.com>
2021-07-29 09:11:40 -0300

    self bug join bugou minha mente

---
+++ desafio11.sql
@@ -0,0 +1,14 @@
+-- vinicius-vasconcelos
+-- Fernando-Maia / https://github.com/tryber/sd-010-b-mysql-vocabulary-booster/tree/Fernando-Maia-mySql-vocabulary-booster
+select 
+customersA.ContactName as Nome, 
+customersA.Country as País,
+count(customersB.Country) as 'Número de compatriotas'
+from w3schools.customers as customersA,
+w3schools.customers as customersB
+where 
+customersA.Country = customersB.Country 
+And 
+customersA.ContactName <> customersB.ContactName
+group by customersA.ContactName
+order by customersA.ContactName;
