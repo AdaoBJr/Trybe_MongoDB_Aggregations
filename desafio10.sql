ff85dd101158f4d5848d54a366100568374a28ca
bidaffonso <bidaffondo@gmail.com>
2021-07-29 19:52:42 -0400

    Requisito 10 Teste 1

---
+++ desafio10.sql
@@ -0,0 +1,12 @@
+SELECT 
+    ProductName AS Produto,
+    MIN(order_details.Quantity) AS Mínima,
+    MAX(order_details.Quantity) AS Máxima,
+    ROUND(AVG(order_details.Quantity), 2) AS Média
+FROM
+    w3schools.products
+        INNER JOIN
+    w3schools.order_details ON order_details.ProductID = products.ProductID
+GROUP BY products.ProductID
+HAVING Média > 20
+ORDER BY Média ASC , Produto ASC;
