90f83e6fd3d6c986b867038b2f13bb286c3bb378
dennis <dennismarcelo15@gmail.com>
2021-07-28 23:40:29 -0300

    desafio 13 correçẽos

--- desafio13.sql
+++ desafio13.sql
@@ -1,8 +1,7 @@
 use w3school;
-SELECT p.ProductName as Produto, p.Price as Preço
+SELECT p.ProductName as 'Produto', p.Price as 'Preço'
 FROM
     order_details od
 INNER JOIN products p
-ON p.ProductID = od.ProductID
-WHERE od.Quantity >80
+ON p.ProductID = od.ProductID and od.Quantity >80
 order by Produto;
