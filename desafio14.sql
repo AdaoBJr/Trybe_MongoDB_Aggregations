79ec1d3414dae57893c234c07d0fbff389465853
Johnny-batata <you@example.com>
2021-07-28 22:24:35 -0300

    requirement 14

--- desafio14.sql
+++ desafio14.sql
@@ -1,5 +1,10 @@
-SELECT * FROM w3schools.customers;
-SELECT * FROM w3schools.shippers;
-SELECT * FROM w3schools.suppliers;
-
-
+ SELECT DISTINCT
+    Country AS 'País'
+FROM
+    w3schools.customers 
+UNION SELECT DISTINCT
+    Country AS 'País'
+FROM
+    w3schools.suppliers
+ORDER BY `País`
+LIMIT 5;
