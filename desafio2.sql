39aa54aac7af6d96f305956991a081276d54eaef
brunamfguimaraes <brunamfguimaraes@gmail.com>
2021-07-28 23:08:33 -0300

    Adicionando requisito 3 e corrigindo o 2

--- desafio2.sql
+++ desafio2.sql
@@ -1,14 +1,12 @@
 SELECT 
     JOB_TITLE AS Cargo,
     CASE
-        WHEN e.SALARY BETWEEN 5000 AND 10000 THEN 'Baixo'
-        WHEN e.SALARY BETWEEN 1001 AND 20000 THEN 'Médio'
-        WHEN e.SALARY BETWEEN 2001 AND 30000 THEN 'Alto'
-        WHEN e.SALARY > 30000 THEN 'Altíssimo'
+        WHEN MAX_SALARY BETWEEN 5000 AND 10000 THEN 'Baixo'
+        WHEN MAX_SALARY BETWEEN 1001 AND 20000 THEN 'Médio'
+        WHEN MAX_SALARY BETWEEN 2001 AND 30000 THEN 'Alto'
+        WHEN MAX_SALARY > 30000 THEN 'Altíssimo'
         ELSE 'Usuário sem salário'
     END AS Nível
 FROM
     hr.jobs
-        INNER JOIN
-    employees AS e ON jobs.JOB_ID = e.JOB_ID
 ORDER BY Cargo ASC;
