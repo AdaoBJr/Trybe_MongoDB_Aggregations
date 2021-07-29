8f07747ff3f37d738c8eb5df192a7ee801100e1b
Carlos Vieira <appencigr@gmail.com>
2021-07-29 19:17:57 -0300

    Desafio 8 - Exibe todas as pessoas consumidoras cujos pedidos já foram enviados pelas empresas 'Speedy Express' ou 'United Package' - Feito

---
+++ desafio8.sql
@@ -0,0 +1,10 @@
+SELECT UPPER(CONCAT(wemployees.FirstName,' ', wemployees.LastName)) AS `Nome de contato`,
+shippers.ShipperName AS `Empresa que fez o envio`,
+orders.OrderDate AS `Data do pedido`
+FROM w3schools.orders AS orders
+JOIN w3schools.shippers AS shippers
+ON shippers.ShipperID = orders.ShipperID
+JOIN w3schools.employees AS wemployees
+ON wemployees.EmployeeID = orders.EmployeeID
+WHERE shippers.ShipperName IN ('Speedy Express', 'United Package')
+ORDER BY `Nome de contato`, `Empresa que fez o envio`, `Data do pedido`;
