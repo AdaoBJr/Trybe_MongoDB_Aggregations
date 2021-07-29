4347f697f840d57ff06c490f72fdbe1f37ba9bb3
Fernanda Porto Oliveira <fernanda.oliveira.mec@gmail.com>
2021-07-29 09:58:02 -0300

    Ajustando order by do desafio 3

--- desafio3.sql
+++ desafio3.sql
@@ -3,4 +3,5 @@ JOB_TITLE AS Cargo,
 MAX_SALARY - MIN_SALARY AS "Diferença entre salários máximo e mínimo"
 FROM hr.jobs
 ORDER BY
-`Diferença entre salários máximo e mínimo`;
+`Diferença entre salários máximo e mínimo`
+`Cargo`;
