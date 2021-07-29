ae31c5a7b51512d802bf69ecc0dc456aa3e23097
Jessie <miss_sc_jessi@hotmail.com>
2021-07-28 21:50:03 -0300

    desafio 11 arrumado

--- desafio11.sql
+++ desafio11.sql
@@ -1,11 +1,11 @@
 SELECT 
     c1.ContactName AS 'Nome',
     c1.Country AS 'País',
-    COUNT(c1.ContactName) - 1 AS 'Compatriotas'
+    COUNT(c1.ContactName) AS 'Número de compatriotas'
 FROM
     customers AS c1,
     customers AS c2
 WHERE
-    c1.Country = c2.Country
-GROUP BY c1.ContactName
+    c1.Country = c2.Country AND c1.CustomerID <> c2.CustomerID
+GROUP BY c1.ContactName, c1.Country
 ORDER BY c1.ContactName;
