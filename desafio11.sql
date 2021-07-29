7611bf02e6167abf18949b39127470149066ca8b
Cristian Bugs <cristian.bugs@gmail.com>
2021-07-29 17:39:51 -0300

    desafio11

--- desafio11.sql
+++ desafio11.sql
@@ -1,5 +1,11 @@
 SELECT
-  Country,
-  COUNT(*)
-FROM w3schools.customers
-GROUP BY Country;
+  cs.ContactName AS 'Nome',
+  cs.Country AS 'País',
+  COUNT(c.Country) - 1 AS 'Número de compatriotas'
+FROM w3schools.customers cs
+LEFT JOIN w3schools.customers AS c
+  ON cs.Country = c.Country
+GROUP BY cs.ContactName,
+         cs.Country
+HAVING COUNT(c.Country) > 1
+ORDER BY cs.ContactName;
