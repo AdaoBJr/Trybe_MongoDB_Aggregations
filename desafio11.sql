dbf123b6d5874e756edebb4031f3a454b28fe994
EderPaiva <edergomesdepaiva12@gmail.com>
2021-07-29 09:57:25 -0300

    req 11 passa pelo amor de deus

--- desafio11.sql
+++ desafio11.sql
@@ -1,8 +1,4 @@
--- vinicius-vasconcelos
--- Fernando-Maia / https://github.com/tryber/sd-010-b-mysql-vocabulary-booster/tree/Fernando-Maia-mySql-vocabulary-booster
-select 
-customersA.ContactName as 'Nome', 
-customersA.Country as 'País',
+select customersA.ContactName as 'Nome', customersA.Country as 'País',
 count(customersB.Country) as 'Número de compatriotas'
 from w3schools.customers as customersA,
 w3schools.customers as customersB
