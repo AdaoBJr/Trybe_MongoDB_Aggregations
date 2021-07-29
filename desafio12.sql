3266ba61efdce33db20cc3e91b099e9f8d86c36c
Cristian Bugs <cristian.bugs@gmail.com>
2021-07-29 18:11:57 -0300

    code climate desafio12

--- desafio12.sql
+++ desafio12.sql
@@ -6,7 +6,7 @@ SELECT
   e2.SALARY AS 'Salário funcionário 2',
   e2.PHONE_NUMBER AS 'Telefone funcionário 2'
 FROM hr.employees AS e1,
-     hr.employees AS e2
+      hr.employees AS e2
 WHERE e1.JOB_ID = e2.JOB_ID
 AND e1.FIRST_NAME <> e2.FIRST_NAME
 ORDER BY Concat(e1.FIRST_NAME, ' ', e1.LAST_NAME), Concat(e2.FIRST_NAME, ' ', e2.LAST_NAME);
