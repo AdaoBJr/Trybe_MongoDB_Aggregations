fccd2460340f877b12508106f3ef2a09f9792a9d
dennis <dennismarcelo15@gmail.com>
2021-07-29 01:06:09 -0300

    desafio 15 correções

--- desafio15.sql
+++ desafio15.sql
@@ -3,7 +3,7 @@ DELIMITER $$
 CREATE PROCEDURE buscar_media_por_cargo(IN cargo varchar(50))
 BEGIN
 SELECT 
-    round(avg(SALARY), 2)
+    round(avg(SALARY), 2) as 'Média salarial'
 FROM
     employees e
 INNER JOIN jobs j 
