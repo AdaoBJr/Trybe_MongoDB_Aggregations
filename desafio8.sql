08da8d7edcefbd638bdf2866c1bcf80b7f04dfd6
Victor Lopes <victor.cabrera0209@gmail.com>
2021-07-28 23:35:41 -0300

    Eigth Requirement

---
+++ desafio8.sql
@@ -0,0 +1,1 @@
+SELECT c.ContactName AS 'Nome de contato', s.ShipperName AS 'Empresa que fez o envio', o.orderDate AS 'Data do pedido' FROM w3schools.orders o INNER JOIN customers c ON c.CustomerID = o.CustomerID INNER JOIN shippers s ON s.ShipperID = o.ShipperID AND (s.ShipperName = 'Speedy Express' OR s.ShipperName = 'United Package') ORDER BY c.ContactName, s.ShipperName, o.OrderDate
