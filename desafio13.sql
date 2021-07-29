12329d4b1b4b588f93712848f877261c99ea7c25
dennis <dennismarcelo15@gmail.com>
2021-07-28 23:26:52 -0300

    desafio 13 correçẽos3

---
+++ desafio13.sql
@@ -0,0 +1,10 @@
+use w3school;
+SELECT 
+	p.ProductName as Produto,
+	p.Price as Preço
+FROM
+    order_details od
+INNER JOIN products p
+ON p.ProductID = od.ProductID
+WHERE od.Quantity >80
+order by Produto;
