3bc8aeb55020b570de9aec78ec7848821acfbf31
EderPaiva <edergomesdepaiva12@gmail.com>
2021-07-29 09:50:58 -0300

    tentando fazer o requisito 11 funcionar

--- desafio11.sql
+++ desafio11.sql
@@ -1,8 +1,8 @@
 -- vinicius-vasconcelos
 -- Fernando-Maia / https://github.com/tryber/sd-010-b-mysql-vocabulary-booster/tree/Fernando-Maia-mySql-vocabulary-booster
 select 
-customersA.ContactName as Nome, 
-customersA.Country as País,
+customersA.ContactName as 'Nome', 
+customersA.Country as 'País',
 count(customersB.Country) as 'Número de compatriotas'
 from w3schools.customers as customersA,
 w3schools.customers as customersB
@@ -10,5 +10,5 @@ where
 customersA.Country = customersB.Country 
 And 
 customersA.ContactName <> customersB.ContactName
-group by customersA.ContactName
-order by customersA.ContactName;
+group by `Nome`
+order by `Nome` ASC;
