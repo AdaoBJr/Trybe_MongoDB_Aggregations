b9fe7e9dfd6dd8d6866da8320868bbe64bbfab01
Cristian Bugs <cristian.bugs@gmail.com>
2021-07-28 21:46:19 -0300

    desafio7

--- desafio7.sql
+++ desafio7.sql
@@ -1,7 +1,7 @@
 SELECT
-  ucase(CONCAT(e.first_name, ' ', e.last_name)) AS 'Nome Completo',
+  ucase(CONCAT(e.first_name, ' ', e.last_name)) AS 'Nome completo',
   h.start_date AS 'Data de início',
-  e.SALARY
+  e.SALARY as 'Salário'
 FROM hr.employees AS e
 INNER JOIN hr.job_history AS h
   ON e.EMPLOYEE_ID = h.EMPLOYEE_ID
