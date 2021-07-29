b79788beeab43b3524fec6cee3538fc7d4e03bf5
andersontrkz <trkz.anderson@gmail.com>
2021-07-29 11:41:18 -0300

    Challange 09

---
+++ desafio9.sql
@@ -0,0 +1,11 @@
+SELECT 
+    CONCAT(table_employees.FirstName,
+            ' ',
+            table_employees.LastName) 'Nome completo',
+    COUNT(*) 'Total de pedidos'
+FROM
+    w3schools.employees table_employees
+        INNER JOIN
+    w3schools.orders table_orders ON table_employees.EmployeeID = table_orders.EmployeeID
+GROUP BY table_orders.EmployeeID
+ORDER BY 'Total de pedidos';
