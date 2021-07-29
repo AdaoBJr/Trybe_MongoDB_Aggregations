e2402d62b1d8da0ab6a13deceec8171f498b235e
Letycia Barreto <letyciasbarreto@gmail.com>
2021-07-29 09:44:15 -0300

    desafio 11

---
+++ desafio11.sql
@@ -0,0 +1,11 @@
+SELECT 
+    c.CustomerName AS 'Nome',
+    c.country AS 'País',
+    COUNT(c.Country) - 1 AS 'Número de compatriotas'
+FROM
+    w3schools.customers AS c
+        LEFT JOIN
+    w3schools.customers AS ct ON ct.Country = c.Country
+GROUP BY Nome , País
+HAVING `Número de compatriotas`
+ORDER BY Nome;
