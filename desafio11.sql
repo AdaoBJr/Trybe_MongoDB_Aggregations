59e497dd7d4a669b855411eba37c0fac026453f4
Pedro Amaral <amaralph1995@gmail.com>
2021-07-28 22:58:03 -0300

    Corrige desafio 11

--- desafio11.sql
+++ desafio11.sql
@@ -1,6 +1,7 @@
 SELECT
 a.contactname AS 'Nome',
 a.country AS 'País',
-(SELECT (COUNT(country) - 1) FROM w3schools.customers AS b WHERE b.country = a.country) AS 'Número de compatriotas'
+(SELECT (COUNT(b.country) - 1) FROM w3schools.customers AS b WHERE b.country = a.country) AS 'Número de compatriotas'
 FROM w3schools.customers AS a
+WHERE a.country NOT IN (SELECT country FROM w3schools.customers GROUP BY country HAVING COUNT(*) = 1)
 ORDER BY a.contactname;
