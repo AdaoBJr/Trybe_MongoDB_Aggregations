d8ef7d91c7f381330dfbbe3bebaffd7b648481de
AndersonPedrosa35 <anderson.loiro222@gmail.com>
2021-07-29 19:59:19 -0300

    Correção code climate

--- desafio8.sql
+++ desafio8.sql
@@ -2,5 +2,5 @@ SELECT ContactName AS "Nome de contato", ShipperName AS "Empresa que fez o envio
 OrderDate AS "Data do pedido" FROM  w3schools.orders AS o
 INNER JOIN  w3schools.customers AS c ON o.CustomerID = c.CustomerID
 INNER JOIN w3schools.shippers AS s ON o.ShipperID = s.ShipperID
- WHERE o.ShipperID IN (1, 2)
- ORDER BY ContactName, ShipperName, OrderDate;
+  WHERE o.ShipperID IN (1, 2)
+  ORDER BY ContactName, ShipperName, OrderDate;
