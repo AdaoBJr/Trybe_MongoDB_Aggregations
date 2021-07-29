5d25651b2325790b74d34a12f01aa2a5fb2bff0c
André Arnoni <andre_arnoni@yahoo.com.br>
2021-07-29 11:22:18 -0300

    arrumado code climate desafio 15

--- desafio15.sql
+++ desafio15.sql
@@ -1,10 +1,10 @@
 DELIMITER $$
 CREATE PROCEDURE buscar_media_por_cargo(IN cargo VARCHAR(50))
 BEGIN
-	SELECT ROUND(avg(e.SALARY), 2) AS 'Média salarial'
-	FROM hr.employees AS e
-	INNER JOIN hr.jobs AS j
-	ON e.JOB_ID = j.JOB_ID
-	WHERE j.JOB_TITLE = cargo;
+SELECT ROUND(avg(e.SALARY), 2) AS 'Média salarial'
+FROM hr.employees AS e
+INNER JOIN hr.jobs AS j
+ON e.JOB_ID = j.JOB_ID
+WHERE j.JOB_TITLE = cargo;
 END $$
 DELIMITER ;
