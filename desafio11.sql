38820d8b2026a4485ae0a239d6fbbda635b26796
Jefferson_Andrade <jffrsncrls@gmail.com>
2021-07-29 09:55:07 -0300

    test

--- desafio11.sql
+++ desafio11.sql
@@ -2,10 +2,8 @@ SELECT
   C.ContactName AS 'Nome',
   C.Country AS 'País',
   COUNT(C1.Country)
-FROM w3schools.customers AS C,
-		w3schools.customers AS C1
+FROM w3schools.customers AS C, w3schools.customers AS C1
 WHERE C.Country = C1.Country
 AND C.ContactName <> C1.ContactName
-GROUP BY C.ContactName,
-				C.Country
+GROUP BY C.ContactName, C.Country
 ORDER BY C.ContactName;
