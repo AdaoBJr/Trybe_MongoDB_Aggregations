d4f795d00a227471a5884c3924f8f63bb344774b
aythan azevedo <aythan16@gmail.com>
2021-07-28 22:03:51 -0300

    desafio 6,7

---
+++ desafio7.sql
@@ -0,0 +1,7 @@
+SELECT UCASE(CONCAT(employees.FIRST_NAME, ' ', employees.LAST_NAME)) AS 'Nome completo',
+		hrs.START_DATE AS 'Data de início',
+		employees.SALARY AS 'Salário'
+FROM hr.job_history hrs
+INNER JOIN hr.employees employees ON employees.EMPLOYEE_ID = hrs.EMPLOYEE_ID
+WHERE MONTH(hrs.START_DATE) IN (1, 2, 3)
+ORDER BY UCASE(CONCAT(employees.FIRST_NAME, ' ', employees.LAST_NAME)) DESC , hrs.START_DATE;
