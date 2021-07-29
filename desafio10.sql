bba63dcf3985cfef5f7f8862fd6d77c9a0f0fcd5
Lotar Lucas <amaral.lucas.lotar@gmail.com>
2021-07-29 13:49:04 -0300

    done requirement 10

--- desafio10.sql
+++ desafio10.sql
@@ -2,10 +2,10 @@ SELECT
   p.ProductName as "Produto",
   MIN(od.Quantity) as "Mínima",
   MAX(od.Quantity) as "Máxima",
-  AVG(od.Quantity) as "Média"
-FROM order_details as od
-INNER JOIN products as p
-  ON p.ProductID = od.ProductID
-WHERE AVG(od.Quantity) > 20.00
+  ROUND(AVG(od.Quantity),2) as "Média"
+FROM products as p
+INNER JOIN order_details as od
+  ON od.ProductID = p.ProductID
 GROUP BY p.ProductName
+HAVING AVG(od.Quantity) > 20.00
 ORDER BY AVG(od.Quantity), p.ProductName;
