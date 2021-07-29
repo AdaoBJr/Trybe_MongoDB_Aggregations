f199c7fe9b22f3158560d8f8ea1a8a767bafaf80
André Arnoni <andre_arnoni@yahoo.com.br>
2021-07-29 11:24:13 -0300

    arrumado code climate desafio 20

--- desafio20.sql
+++ desafio20.sql
@@ -1,15 +1,15 @@
 DELIMITER $$
 CREATE PROCEDURE exibir_historico_completo_por_funcionario(IN email VARCHAR(50))
 BEGIN
-	SELECT concat(e.FIRST_NAME, ' ', e.LAST_NAME) AS 'Nome completo',  d.DEPARTMENT_NAME AS 'Departamento', j.JOB_TITLE AS 'Cargo'
-	FROM hr.employees AS e
-    INNER JOIN hr.job_history AS hist
-    ON e.EMPLOYEE_ID = hist.EMPLOYEE_ID
-	INNER JOIN hr.departments AS d
-	ON d.DEPARTMENT_ID = hist.DEPARTMENT_ID
-	INNER JOIN hr.jobs AS j
-    ON j.JOB_ID = hist.JOB_ID
-    WHERE e.EMAIL = email
-    ORDER BY d.DEPARTMENT_NAME, j.JOB_TITLE;
+SELECT concat(e.FIRST_NAME, ' ', e.LAST_NAME) AS 'Nome completo',  d.DEPARTMENT_NAME AS 'Departamento', j.JOB_TITLE AS 'Cargo'
+FROM hr.employees AS e
+INNER JOIN hr.job_history AS hist
+ON e.EMPLOYEE_ID = hist.EMPLOYEE_ID
+INNER JOIN hr.departments AS d
+ON d.DEPARTMENT_ID = hist.DEPARTMENT_ID
+INNER JOIN hr.jobs AS j
+ON j.JOB_ID = hist.JOB_ID
+WHERE e.EMAIL = email
+ORDER BY d.DEPARTMENT_NAME, j.JOB_TITLE;
 END $$
 DELIMITER ;
