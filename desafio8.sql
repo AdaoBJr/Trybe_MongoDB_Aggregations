92ebf555d8536a5eb45fa26155abaea9d047353d
legalvao1 <legalvao1@gmail.com>
2021-07-28 22:34:51 -0300

    Req 8 - pessoas consumidoras cujos pedidos já foram enviados pelas empresas 'Speedy Express' ou 'United Package' - correto

--- desafio8.sql
+++ desafio8.sql
@@ -5,4 +5,5 @@ O.OrderDate AS 'Data do pedido'
 FROM w3schools.customers C
 INNER JOIN w3schools.orders O ON O.CustomerID = C.CustomerID
 INNER JOIN w3schools.shippers S ON S.ShipperID = O.ShipperID 
+WHERE S.ShipperName IN('Speedy Express','United Package')
 ORDER BY `Nome de contato`, `Empresa que fez o envio`, `Data do pedido`;
