8bcac26615be24f60ad28313f1f0ac6b390a4d7f
root <diegoserafim1@gmail.com>
2021-07-29 20:06:13 -0300

    clientes que possuem compatriotas, quantidade de compatriotas para cada cliente* nãopassou noteste, mas agora vai

--- desafio11.sql
+++ desafio11.sql
@@ -1,11 +1,11 @@
 -- Iniciando o projeto
 # clientes que possuem compatriotas, quantidade de compatriotas para cada cliente
 
-SELECT customer_n.ContactName AS 'Nome',
-customer_p. AS 'País',
+SELECT custom_n.ContactName AS 'Nome',
+custom_p.Country AS 'País',
 COUNT(*) - 1 AS 'Número de compatriotas'
-FROM w3schools.customers AS customer_p
-INNER JOIN w3schools.customers AS customer_n ON customer_p.Country = customer_n.Country
+FROM w3schools.customers AS custom_p
+INNER JOIN w3schools.customers AS custom_n ON custom_p.Country = custom_n.Country
 GROUP BY 2, 1
 HAVING `Número de compatriotas` <> 0
 ORDER BY 1 ASC;
