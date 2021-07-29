91012b142de2bcc9a947b193ff369058a1fefd24
andersontrkz <trkz.anderson@gmail.com>
2021-07-29 20:57:48 -0300

    Fix order by conflict + freeformatter

--- desafio4.sql
+++ desafio4.sql
@@ -1,15 +1,29 @@
-SELECT 
-    table1.job_title AS 'Cargo',
-    AVG(salary) AS 'Média salarial',
-    CASE
-        WHEN AVG(table2.salary) BETWEEN 2000 AND 5800 THEN 'Júnior'
-        WHEN AVG(table2.salary) BETWEEN 5801 AND 7500 THEN 'Pleno'
-        WHEN AVG(table2.salary) BETWEEN 7501 AND 10500 THEN 'Sênior'
-        ELSE 'CEO'
-    END AS 'Senioridade'
+SELECT
+  table1.job_title AS 'Cargo',
+  ROUND(AVG(table2.salary), 2) AS 'Média salarial',
+  CASE
+    WHEN
+      AVG(table2.salary) BETWEEN 2000 AND 5800 
+    THEN
+      'Júnior' 
+    WHEN
+      AVG(table2.salary) BETWEEN 5801 AND 7500 
+    THEN
+      'Pleno' 
+    WHEN
+      AVG(table2.salary) BETWEEN 7501 AND 10500 
+    THEN
+      'Sênior' 
+    ELSE
+      'CEO' 
+  END
+  AS 'Senioridade' 
 FROM
-    hr.jobs AS table1
-        JOIN
-    hr.employees AS table2 ON table1.job_id = table2.job_id
-GROUP BY 1
-ORDER BY 2 , 1;
+  hr.jobs AS table1 
+  INNER JOIN
+    hr.employees AS table2 
+    ON table1.job_id = table2.job_id 
+GROUP BY
+  1 
+ORDER BY
+  2, 1;
--- desafio6.sql
+++ desafio6.sql
@@ -1,16 +1,19 @@
-SELECT 
-    CONCAT(table_employees.first_name,
-            ' ',
-            table_employees.last_name) AS 'Nome completo',
-    table_jobs.job_title AS 'Cargo',
-    DATE(table_history.start_date) AS 'Data de início do cargo',
-    table_departments.department_name AS 'Departmento'
+SELECT
+  CONCAT(table_employees.first_name, ' ', table_employees.last_name) AS 'Nome completo',
+  table_jobs.job_title AS 'Cargo',
+  DATE(table_history.start_date) AS 'Data de início do cargo',
+  table_departments.department_name AS 'Departmento' 
 FROM
-    hr.job_history AS table_history
-        INNER JOIN
-    hr.employees AS table_employees ON table_employees.employee_id = table_history.employee_id
-        INNER JOIN
-    hr.jobs AS table_jobs ON table_history.job_id = table_jobs.job_id
-        INNER JOIN
-    hr.departments AS table_departments ON table_departments.department_id = table_history.department_id
-ORDER BY 'Nome Completo' DESC , table_jobs.job_title;
+  hr.job_history AS table_history 
+  INNER JOIN
+    hr.employees AS table_employees 
+    ON table_employees.employee_id = table_history.employee_id 
+  INNER JOIN
+    hr.jobs AS table_jobs 
+    ON table_history.job_id = table_jobs.job_id 
+  INNER JOIN
+    hr.departments AS table_departments 
+    ON table_departments.department_id = table_history.department_id 
+ORDER BY
+  `Nome Completo` DESC,
+  table_jobs.job_title;
