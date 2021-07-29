4526b7ce2ed48c78e8f95e2c532aee6f15c7d032
Lara Capila <laracapilak@gmail.com>
2021-07-29 16:17:19 -0300

    Challenge 4 done

---
+++ desafio4.sql
@@ -0,0 +1,13 @@
+SELECT j.JOB_TITLE AS `Cargo`,
+ROUND(AVG(s.SALARY), 2) AS `Média salarial`,
+CASE
+  WHEN AVG(s.SALARY) BETWEEN 2000 AND 5800 THEN "Júnior"
+  WHEN AVG(s.SALARY) BETWEEN 5801 AND 7500 THEN "Pleno"
+  WHEN AVG(s.SALARY) BETWEEN 7501 AND 10500 THEN "Sênior"
+  WHEN AVG(s.SALARY) > 10500 THEN "CEO"
+END AS `Senioridade`
+FROM hr.jobs AS j
+INNER JOIN hr.employees AS s
+ON s.JOB_ID = j.JOB_ID
+GROUP BY  `Cargo`
+ORDER BY `Média salarial` ASC, `Cargo` ASC;
