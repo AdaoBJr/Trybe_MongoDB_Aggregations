f1c77c3c4e6f5aea4401e6a6fc6cb9fa0b753057
Jefferson_Andrade <jffrsncrls@gmail.com>
2021-07-29 10:34:54 -0300

    Done 13

--- desafio11.sql
+++ desafio11.sql
@@ -1,7 +1,7 @@
 SELECT
   C.ContactName AS 'Nome',
   C.Country AS 'País',
-  COUNT(C1.Country) AS 'Número de compatriotas',
+  COUNT(C1.Country) AS 'Número de compatriotas'
 FROM w3schools.customers AS C, w3schools.customers AS C1
 WHERE C.Country = C1.Country
 AND C.ContactName <> C1.ContactName
