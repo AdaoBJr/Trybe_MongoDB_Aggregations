cdaad409f1c7319f6c815bd105aefff4050e20df
Daniel Ceci <danielceci96@gmail.com>
2021-07-29 18:49:52 -0300

    torcendo pra que dessa vez passe o req 6

--- desafio6.sql
+++ desafio6.sql
@@ -4,9 +4,10 @@ x.START_DATE as "Data de início do cargo",
 d.DEPARTMENT_NAME as 'Departamento'
 FROM hr.jobs j inner join hr.job_history x on  j.JOB_ID = x.JOB_ID   -- JOB_ID é a coluna em comum entre essas 2 tabelas
 inner join hr.employees e on e.EMPLOYEE_ID = x.EMPLOYEE_ID -- tentava o JOB_ID E NUNCA FICAVA IGUAL o challengeResults
-inner join hr.departments d  on d.DEPARTMENT_ID = e.DEPARTMENT_ID 
+inner join hr.departments d  on d.DEPARTMENT_ID = x.DEPARTMENT_ID  -- só deu certo de vez quando usei a tabela X. Antes eu tava tentando o DEPARTMENT_ID na tabela E(employees). O DEPARTMENT_ID em job_history faz mais sentido pra essa correlação, porque envolve CARGOS. Employees envolve pessoas.
 ORDER BY concat(e.FIRST_NAME, " ", e.LAST_NAME) DESC, `Cargo` ASC;
 
+-- Acabei usando o job_history 3 vezes pra fazer correlação com outras tabelas.
 -- IMPORTANTE*** 
 -- Tem que fazer um ON do lado de cada inner Join *******
 -- *****A ORDEM DOS INNER JOINS IMPORTA. Além disso, é começar os inner joins pelas tabelas com mais informação
