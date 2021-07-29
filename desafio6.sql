fe5184775146a523a5e53d9a7ee4222d8dbca675
Daniel Ceci <danielceci96@gmail.com>
2021-07-29 14:11:49 -0300

    FIX: requisito 6

--- desafio6.sql
+++ desafio6.sql
@@ -2,12 +2,16 @@ SELECT concat(e.FIRST_NAME, " ", e.LAST_NAME) as 'Nome completo',
 j.JOB_TITLE as 'Cargo', 
 x.START_DATE as "Data de início do cargo", 
 d.DEPARTMENT_NAME as 'Departamento'
-FROM hr.employees e inner join hr.jobs j  on e.JOB_ID = j.JOB_ID       -- JOB_ID é a coluna em comum entre essas 2 tabelas
-inner join hr.job_history x inner join hr.departments d on x.DEPARTMENT_ID = d.DEPARTMENT_ID -- DEPARTMENT_ID é a coluna em comum entre essas 2 tabelas
-ORDER BY concat(e.FIRST_NAME, " ", e.LAST_NAME) DESC, j.JOB_TITLE ASC;
+FROM hr.jobs j inner join hr.job_history x on  j.JOB_ID = x.JOB_ID   -- JOB_ID é a coluna em comum entre essas 2 tabelas
+inner join hr.employees e on e.EMPLOYEE_ID = x.EMPLOYEE_ID -- tentava o JOB_ID E NUNCA FICAVA IGUAL o challengeResults
+inner join hr.departments d  on d.DEPARTMENT_ID = d.DEPARTMENT_ID 
+ORDER BY concat(e.FIRST_NAME, " ", e.LAST_NAME) DESC, `Cargo` ASC;
 
--- Inner join com mais de 2 tabelas é assim: Escolhe 2 tabelas por vez pra fazer o inner join e depois usa o ON na coluna em comum.
--- Aí depois vai fazendo inner join + ON até abranger todas as tabelas usadas.
--- Se o núm. de tabelas for ímpar, é praticamente a mesma ideia do que se for número par de tabelas.
-
+-- IMPORTANTE*** 
+-- Tem que fazer um ON do lado de cada inner Join *******
+-- *****A ORDEM DOS INNER JOINS IMPORTA. Além disso, é começar os inner joins pelas tabelas com mais informação
+-- Nem sempre começa por essas maiores(esse requisito não segue essa ideia), mas é uma boa forma de começar.
+-- Requisito chatíssimo. Perdi horas na tentando . *************** Comecei o inner join com tabela JOBS
+-- e depois JOBS_HISTORY porque o requisito fala muito sobre o CARGO. Então faz sentido começar com ela.
+-- Aqui fui testando a ordem até bater com a forma  que estava no CHALLENGE.RESULTS(página de tests)
 -- Usei este Link pra me lembrar como uso o CONCAT no mysql:     https://www.w3schools.com/mysql/trymysql.asp?filename=trysql_func_mysql_concat2
