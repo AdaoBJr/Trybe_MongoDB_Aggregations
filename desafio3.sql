853ee4876ab9be93a24afa8aed0e23b5a894728d
Mariana Savoldi <marianasavodi123@gmail.com>
2021-07-29 19:28:02 -0300

    Desafio 3 test2

--- desafio3.sql
+++ desafio3.sql
@@ -1,4 +1,5 @@
 SELECT JOB_TITLE AS 'Cargo',
 (MAX_SALARY - MIN_SALARY) AS 'Diferença entre salários máximo e mínimo'
 FROM hr.jobs
-ORDER BY 'Diferença entre salários máximo e mínimo' ASC, JOB_TITLE ASC;
+ORDER BY 2, 1;
+-- https://riptutorial.com/sql/example/6529/order-by-alias 
