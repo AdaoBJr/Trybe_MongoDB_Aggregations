1f0e9560f08f7b753c4d158ace7c65f3ce0f07e2
Lotar Lucas <amaral.lucas.lotar@gmail.com>
2021-07-29 23:01:50 -0300

    done requirement 11

---
+++ desafio11.sql
@@ -0,0 +1,12 @@
+-- Realizado com ajuda de Rafael Mathias
+SELECT 
+    a.ContactName AS "Nome",
+    a.Country as "País",
+    COUNT(b.country) - 1 as "Número de compatriotas"
+FROM customers as a, customers as b
+WHERE a.country = b.country
+GROUP BY  a.Country, a.ContactName
+HAVING COUNT(b.country) - 1 <> 0
+ORDER BY a.ContactName;
+
+
