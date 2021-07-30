70832825a3c5fe97e425af528d34e685a1f17ddf
root <diegoserafim1@gmail.com>
2021-07-30 10:35:37 -0300

    tentando passar no padrão pedido

--- desafio18.sql
+++ desafio18.sql
@@ -6,5 +6,5 @@ DATE_FORMAT(job_h.START_DATE, '%d/%m/%y') AS 'Data de início',
 DATE_FORMAT(job_h.END_DATE, '%d/%m/%y') AS 'Data de rescisão',
 ROUND(DATEDIFF(job_h.END_DATE, job_h.START_DATE) / 365, 2) AS 'Anos trabalhados'
 FROM hr.job_history AS job_h
-INNER JOIN hr.employees AS employe ON job_h.EMPLOYEE_ID = employe.EMPLOYEE_ID
+INNER JOIN hr.employees AS employe ON employe.EMPLOYEE_ID = job_h.EMPLOYEE_ID
 ORDER BY 1 ASC, 4 ASC;
