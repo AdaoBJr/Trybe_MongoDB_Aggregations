3d73b1725946c227388e75ed913eff73ee3d6387
Gabriel Miranda <gabriel.mirandab92@gmail.com>
2021-07-30 14:43:16 -0300

    arruma exercicio 20

--- desafio20.sql
+++ desafio20.sql
@@ -6,8 +6,8 @@ SELECT CONCAT(e.FIRST_NAME, ' ', e.LAST_NAME) as 'Nome completo',
 d.DEPARTMENT_NAME as 'Departamento',
 j.JOB_TITLE as 'Cargo'
 FROM hr.employees as e
-INNER JOIN hr.departments as d ON d.DEPARTMENT_ID = e.DEPARTMENT_ID
 INNER JOIN hr.job_history as jh ON jh.EMPLOYEE_ID = e.EMPLOYEE_ID
+INNER JOIN hr.departments as d ON d.DEPARTMENT_ID = jh.DEPARTMENT_ID
 INNER JOIN hr.jobs as j ON j.JOB_ID = jh.JOB_ID
 WHERE email = e.EMAIL
 ORDER BY d.DEPARTMENT_NAME, j.JOB_TITLE;
