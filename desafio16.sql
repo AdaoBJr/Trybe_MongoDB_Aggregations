30a65f84350cfd755162975f46962672181bfca9
root <diegoserafim1@gmail.com>
2021-07-30 08:03:32 -0300

    buscar_quantidade_de_empregos_por_funcionario, ao receber o email de um funcionáro, retorna a quantidade de empregos presentes em seu histórico

---
+++ desafio16.sql
@@ -0,0 +1,15 @@
+-- Iniciando o projeto
+# buscar_quantidade_de_empregos_por_funcionario, ao receber o email de um funcionáro, retorna a quantidade de empregos presentes em seu histórico
+
+USE hr;
+DELIMITER $$
+CREATE FUNCTION buscar_quantidade_de_empregos_por_funcionario(email_f VARCHAR(30))
+RETURNS INT READS SQL DATA
+BEGIN
+DECLARE quantidade_e INT;
+SELECT COUNT(*) FROM hr.employees AS employe
+INNER JOIN hr.job_history AS job_h ON employe.EMPLOYEE_ID = job_h.EMPLOYEE_ID
+WHERE employe.EMAIL = email_f INTO quantidade_e;
+RETURN quantidade_e;
+END $$
+DELIMITER ;
