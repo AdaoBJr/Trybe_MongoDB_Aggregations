dfbcd15d4d70a5a0aa1e0a9cd159b11484092032
Jonathan Souza <souzajonathan570@gmail.com>
2021-07-30 00:26:25 -0300

    correcao de code climate

--- desafio16.sql
+++ desafio16.sql
@@ -7,8 +7,8 @@ RETURNS INTEGER READS SQL DATA
 BEGIN
 	DECLARE total_emprego INTEGER;
 	SELECT COUNT(jh.job_id) AS 'total de empregos'
-		FROM hr.job_history AS jh, hr.employees AS e
-		WHERE e.EMAIL IN(email) AND e.EMPLOYEE_ID = jh.EMPLOYEE_ID
+	FROM hr.job_history AS jh, hr.employees AS e
+	WHERE e.EMAIL IN(email) AND e.EMPLOYEE_ID = jh.EMPLOYEE_ID
   INTO total_emprego;
   RETURN total_emprego;
 END $$
