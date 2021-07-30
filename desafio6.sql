ae0cae2e5baca5bcbae803be9c925b998f3ccbe8
brunamfguimaraes <brunamfguimaraes@gmail.com>
2021-07-30 19:07:44 -0300

    Corrigindo requisito6

--- desafio6.sql
+++ desafio6.sql
@@ -4,11 +4,11 @@ SELECT
     jh.START_DATE AS 'Data de início do cargo',
     d.DEPARTMENT_NAME AS 'Departamento'
 FROM
-    employees AS e
+    hr.employees AS e
         INNER JOIN
-    job_history AS jh ON e.EMPLOYEE_ID = jh.EMPLOYEE_ID
+    hr.job_history AS jh ON e.EMPLOYEE_ID = jh.EMPLOYEE_ID
         INNER JOIN
-    departments AS d ON jh.DEPARTMENT_ID = d.DEPARTMENT_ID
+    hr.departments AS d ON jh.DEPARTMENT_ID = d.DEPARTMENT_ID
         INNER JOIN
-    jobs AS j ON j.JOB_ID = jh.JOB_ID
+    hr.jobs AS j ON j.JOB_ID = jh.JOB_ID
 ORDER BY e.FIRST_NAME DESC , e.LAST_NAME ASC;
