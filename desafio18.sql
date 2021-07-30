151e448016835357bc0f90037fdb5e00b806bf8f
root <diegoserafim1@gmail.com>
2021-07-30 11:03:58 -0300

    desafio-18 resalvendo..

--- desafio18.sql
+++ desafio18.sql
@@ -1,10 +1,9 @@
 -- Iniciando o projeto
 # histórico de cargos dos funcionários, data de início e de saída, anos em que ficou no cargo
 
-SELECT CONCAT(employe.FIRST_NAME, ' ', employe.LAST_NAME) AS 'Nome completo',
-DATE_FORMAT(job_h.START_DATE, '%d/%m/%y') AS 'Data de início',
-DATE_FORMAT(job_h.END_DATE, '%d/%m/%y') AS 'Data de rescisão',
-ROUND(DATEDIFF(job_h.END_DATE, job_h.START_DATE) / 365, 2) AS 'Anos trabalhados'
-FROM hr.job_history AS job_h
-INNER JOIN hr.employees AS employe ON employe.EMPLOYEE_ID = job_h.EMPLOYEE_ID
+SELECT CONCAT(employe.FIRST_NAME, ' ', employe.LAST_NAME) as 'Nome completo',
+DATE_FORMAT(job_h.START_DATE, '%d/%m/%y') as 'Data de início',
+aROUND(DATEDIFF(job_h.END_DATE, job_h.START_DATE) / 365, 2) as 'Anos trabalhados'
+FROM hr.job_history as job_h
+INNER JOIN hr.employees as employe ON employe.EMPLOYEE_ID = job_h.EMPLOYEE_ID
 ORDER BY 1, 4 ASC;
