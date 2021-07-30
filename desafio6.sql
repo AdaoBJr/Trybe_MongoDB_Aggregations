b440d52ecc4bf65961b25dab49d0c453c0ea0187
andersontrkz <trkz.anderson@gmail.com>
2021-07-29 21:09:48 -0300

    Fix order by conflict + freeformatter

--- desafio6.sql
+++ desafio6.sql
@@ -2,18 +2,18 @@ SELECT
   CONCAT(table_employees.first_name, ' ', table_employees.last_name) AS 'Nome completo',
   table_jobs.job_title AS 'Cargo',
   DATE(table_history.start_date) AS 'Data de início do cargo',
-  table_departments.department_name AS 'Departmento' 
+  table_department.department_name AS 'Departamento' 
 FROM
   hr.job_history AS table_history 
   INNER JOIN
     hr.employees AS table_employees 
-    ON table_employees.employee_id = table_history.employee_id 
+    ON table_employees.EMPLOYEE_ID = table_history.EMPLOYEE_ID 
   INNER JOIN
     hr.jobs AS table_jobs 
     ON table_history.job_id = table_jobs.job_id 
   INNER JOIN
-    hr.departments AS table_departments 
-    ON table_departments.department_id = table_history.department_id 
+    hr.departments AS table_department 
+    ON table_department.DEPARTMENT_ID = table_history.DEPARTMENT_ID 
 ORDER BY
   `Nome Completo` DESC,
   table_jobs.job_title;
