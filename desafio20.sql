97488500433a6c546a70688383dcef304a109978
Raphael Gumieri <raphaelgumieri@hotmail.com>
2021-07-29 21:35:44 -0300

    desafio 20 - correcao - remocao de uma coluna inutilizada

--- desafio20.sql
+++ desafio20.sql
@@ -4,9 +4,8 @@ CREATE PROCEDURE exibir_historico_completo_por_funcionario(email VARCHAR(50))
 BEGIN
 SELECT
   CONCAT(e.first_name, ' ', e.last_name) AS 'Nome completo',
-  j.job_title AS 'Cargo',
-  jh.start_date AS 'Data de início do cargo',
-  d.department_name AS 'Departamento'
+  d.department_name AS 'Departamento',
+  j.job_title AS 'Cargo'  
 FROM hr.employees AS e
 INNER JOIN hr.job_history AS jh
   ON e.EMPLOYEE_ID = jh.EMPLOYEE_ID
@@ -15,7 +14,7 @@ INNER JOIN hr.jobs AS j
 INNER JOIN hr.departments AS d
   ON jh.DEPARTMENT_ID = d.DEPARTMENT_ID
 WHERE e.EMAIL = email
-ORDER BY `Nome Completo` DESC, j.job_title;
+ORDER BY d.DEPARTMENT_NAME, j.job_title;
 END $$
 DELIMITER ;
 CALL exibir_historico_completo_por_funcionario('NKOCHHAR');
