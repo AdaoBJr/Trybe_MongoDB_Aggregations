8bfc365eb67ca962e181eb70d62bd1888a236444
dennis <dennismarcelo15@gmail.com>
2021-07-29 22:57:12 -0300

    desafio 19

---
+++ desafio19.sql
@@ -0,0 +1,18 @@
+DELIMITER $$
+
+CREATE FUNCTION exibir_quantidade_pessoas_contratadas_por_mes_e_ano(mes int, ano int)
+RETURNS INT READS SQL DATA
+BEGIN
+
+DECLARE total_pepleos int;
+SELECT 
+   COUNT(*) AS total
+FROM
+    hr.employees
+WHERE month(HIRE_DATE) = mes AND year(HIRE_DATE) =  ano
+into total_pepleos;
+return total_pepleos;
+
+END $$
+
+DELIMITER ; 
