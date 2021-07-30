41b05ce9b07e80ed29b19c4b013b42e2124ee12e
EderPaiva <edergomesdepaiva12@gmail.com>
2021-07-29 21:26:31 -0300

    aspas

--- desafio12.sql
+++ desafio12.sql
@@ -1,10 +1,10 @@
 -- Consulta no repositorio de Fabio Maturano
 -- https://github.com/tryber/sd-010-b-mysql-vocabulary-booster/pull/11
 select 
-concat(employeesA.FIRST_NAME,' ', employeesA.LAST_NAME) as 'Nome completo funcionário 1"',
+concat(employeesA.FIRST_NAME,' ', employeesA.LAST_NAME) as 'Nome completo funcionário 1',
 employeesA.SALARY as 'Salário funcionário 1',
 employeesA.PHONE_NUMBER as 'Telefone funcionário 1', 
-concat(employeesB.FIRST_NAME,' ', employeesB.LAST_NAME) as 'Nome completo funcionário 2"',
+concat(employeesB.FIRST_NAME,' ', employeesB.LAST_NAME) as 'Nome completo funcionário 2',
 employeesB.SALARY as 'Salário funcionário 2',
 employeesB.PHONE_NUMBER as 'Telefone funcionário 2'
 from
