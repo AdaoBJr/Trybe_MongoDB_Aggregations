360ea733cac9364adfcacd740027de09a4b4d571
dennis <dennismarcelo15@gmail.com>
2021-07-29 22:43:05 -0300

    desafio 18 correções

--- desafio18.sql
+++ desafio18.sql
@@ -1,7 +1,7 @@
 SELECT 
     concat(FIRST_NAME, ' ', LAST_NAME) AS 'Nome completo',
-    date_format(START_DATE, '%d/%m/%y') AS 'Data de início',
-    date_format(END_DATE, '%d/%m/%y') AS 'Data de rescisão',
+    date_format(START_DATE, '%d/%m/%Y') AS 'Data de início',
+    date_format(END_DATE, '%d/%m/%Y') AS 'Data de rescisão',
     round((DATEDIFF(END_DATE, START_DATE))/(365), 2) as 'Anos trabalhados'
 FROM
     job_history jh
