f7599532c0f0c0f5edb6d5e5ce2e917b0dae1d72
Abimaelr <abimaelr.albu@gmail.com>
2021-07-31 11:42:21 -0300

    Feature 8 fix

--- desafio8.sql
+++ desafio8.sql
@@ -1,5 +1,5 @@
 SELECT
-c.CustomerName as "Nome de contato",
+c.ContactName as "Nome de contato",
 s.ShipperName as "Empresa que fez o envio",
 o.OrderDate as "Data do pedido"
 FROM
@@ -8,7 +8,7 @@ INNER JOIN w3schools.orders as o
 INNER JOIN w3schools.shippers as s
 ON s.ShipperID = o.ShipperID and o.CustomerID = c.CustomerID
 WHERE 
-s.ShipperID = 1 OR s.ShipperID = 2
+(s.ShipperID = 1 OR s.ShipperID = 2)
 ORDER BY 
 `Nome de contato`, `Empresa que fez o envio`, `Data do pedido`
 ;
