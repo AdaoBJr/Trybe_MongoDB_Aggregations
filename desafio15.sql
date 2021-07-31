012707ca2ff2e4dd4407a5c50f261bd5edef7dbb
Lotar Lucas <amaral.lucas.lotar@gmail.com>
2021-07-31 15:57:21 -0300

    FIXED ROUND() FUNCTION IMPLEMENTED

--- desafio15.sql
+++ desafio15.sql
@@ -4,7 +4,7 @@ CREATE PROCEDURE buscar_media_por_cargo(IN cargo VARCHAR(50))
 
 BEGIN
   SELECT 
-  AVG(e.SALARY) as "Média salarial"
+  ROUND(AVG(e.SALARY),2) as "Média salarial"
   FROM employees as e
   INNER JOIN jobs as j
   ON e.JOB_ID = j.JOB_ID
