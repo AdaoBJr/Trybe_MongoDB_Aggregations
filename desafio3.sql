d3279b38c7304d7fb426ded0c560adacfd455e9a
Junior030 <juniorhenrique030@gmail.com>
2021-07-31 15:58:39 -0300

    Fim do requisito 03 V 2.2

--- desafio3.sql
+++ desafio3.sql
@@ -1,3 +1,4 @@
 SELECT JOB_TITLE AS Cargo, MAX_SALARY - MIN_SALARY AS 'Diferença entre salários máximo e mínimo'
 FROM jobs
-ORDER BY MAX_SALARY - MIN_SALARY;
+ORDER BY Cargo;
+-- SELECT * FROM hr.jobs;
