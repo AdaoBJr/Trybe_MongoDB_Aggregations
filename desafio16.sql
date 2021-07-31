3f8ee3ebb5bdb21653dd347698ddd80624f8f8dc
Lara Capila <laracapilak@gmail.com>
2021-07-31 13:49:10 -0300

    Challenge 16 done

---
+++ desafio16.sql
@@ -0,0 +1,13 @@
+USE hr;
+DELIMITER $$
+CREATE FUNCTION buscar_quantidade_de_empregos_por_funcionario(email VARCHAR(20))
+RETURNS INT READS SQL DATA
+BEGIN
+DECLARE quantidade_empregos VARCHAR
+SELECT COUNT(*)
+FROM hr.job_history AS h
+INNER JOIN hr.employees AS e ON e.EMPLOYEE_ID = h.EMPLOYEE_ID
+WHERE e.EMAIL = email INTO quantidade_empregos;
+RETURN quantidade_empregos
+END $$
+DELIMITER ;
