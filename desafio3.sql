57156d007273a19a6df1e8c2f5cac1ccd7a43dc4
Flavio Andrade <foa12381@gmail.com>
2021-07-31 10:12:16 -0300

    desafio3.2

--- desafio3.sql
+++ desafio3.sql
@@ -1,4 +1,4 @@
-SELECT JOB_TITLE as "Cargo" ,
+SELECT JOB_TITLE as "Cargo",
 MAX_SALARY - MIN_SALARY AS "Diferença entre salários máximo e mínimo"
 FROM `hr`.`jobs`
-order by "Diferença entre salários máximo e mínimo" ASC, Cargo Asc
+order by MAX_SALARY - MIN_SALARY ASC, Cargo Asc
