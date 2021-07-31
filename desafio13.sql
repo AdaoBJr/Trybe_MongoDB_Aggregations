aee382d66b4ea749d794564c2538db6d518aa7e4
Roger Franco <rogerfrancogalt@gmail.com>
2021-07-30 23:51:43 -0300

    Desafio 13

---
+++ desafio13.sql
@@ -0,0 +1,8 @@
+SELECT
+  p.ProductName AS 'Produto',
+  p.Price AS 'Preço'
+FROM w3schools.products AS p
+JOIN w3schools.order_details AS o
+  ON o.ProductID = p.ProductID
+WHERE o.Quantity > 80
+ORDER BY 1;
