390a72d5a5f42ab24336fe76a03cb14de85002e1
Emerson Saturnino <programador.emerson@hotmail.com>
2021-07-31 09:49:06 -0300

    Requisito 12

---
+++ desafio12.sql
@@ -0,0 +1,14 @@
+SELECT 
+    c.ContactName AS 'Nome',
+    c.Country AS 'País',
+    (SELECT 
+            COUNT(*)
+        FROM
+            w3schools.customers AS c1
+        WHERE
+            c1.Country = c.Country
+                AND c1.ContactName != c.ContactName) AS `Número de compatriotas`
+FROM
+    w3schools.customers AS c
+HAVING `Número de compatriotas` != 0
+ORDER BY c.ContactName;
