b65d3fb02c17e170bc7e600d71a899ca626c8392
Pedro Amaral <amaralph1995@gmail.com>
2021-07-31 03:18:19 -0300

    Corrige desafio 18

--- desafio18.sql
+++ desafio18.sql
@@ -1,7 +1,7 @@
 SELECT
 CONCAT(e.first_name, ' ', e.last_name) AS 'Nome completo',
-jh.start_date AS 'Data de início',
-jh.end_date AS 'Data de rescisão',
+DATE_FORMAT(jh.start_date, '%d/%m/%Y') AS 'Data de início',
+DATE_FORMAT(jh.end_date, '%d/%m/%Y') AS 'Data de rescisão',
 ROUND(DATEDIFF(jh.end_date, jh.start_date) / 365, 2) AS 'Anos trabahados'
 FROM hr.employees AS e
 INNER JOIN hr.job_history AS jh
