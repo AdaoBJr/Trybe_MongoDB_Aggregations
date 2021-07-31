d11c3adeb20ba30c3b4c056541157f43956f8799
Duda Wiltiner <eduardawiltiner@gmail.com>
2021-07-31 16:11:52 -0300

    desafio 1

--- desafio1.sql
+++ desafio1.sql
@@ -1,1 +1,4 @@
-SELECT 2*3 AS RESULT;
+SELECT c.COUNTRY_NAME AS 'País', IF(r.REGION_NAME = 'Europe', 'incluído', 'não incluído') AS 'Status Inclusão'
+FROM hr.countries c
+INNER JOIN hr.regions r
+    ON c.REGION_ID = r.REGION_ID;
