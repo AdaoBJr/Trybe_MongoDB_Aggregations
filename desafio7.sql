6d0656f20e49013533e5baa99bd136ff6b460db6
Emerson Saturnino <programador.emerson@hotmail.com>
2021-07-31 09:46:42 -0300

    Requisito 5 6 7 8

---
+++ desafio7.sql
@@ -0,0 +1,11 @@
+SELECT 
+    UCASE(CONCAT(e.first_name, ' ', e.last_name)) AS 'Nome completo',
+    jh.start_date AS 'Data de início',
+    e.salary AS 'Salário'
+FROM
+    hr.employees AS e
+        INNER JOIN
+    hr.job_history AS jh ON e.EMPLOYEE_ID = jh.EMPLOYEE_ID
+WHERE
+    MONTH(jh.start_date) IN (01 , 02, 03)
+ORDER BY CONCAT(e.first_name, ' ', e.last_name) ASC , jh.START_DATE ASC;
