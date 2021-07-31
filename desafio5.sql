dcb222d48ae6167e7e1839327676f9f39dde2c07
esionascimento <esio.392@protonmail.com>
2021-07-31 16:44:15 -0400

    desafio5.sql

---
+++ desafio5.sql
@@ -0,0 +1,1 @@
+SELECT j.JOB_TITLE as Cargo, ROUND(j.MAX_SALARY-j.MIN_SALARY) 'Variação Salarial', ROUND(j.MIN_SALARY/12,2) 'Média mínima mensal', ROUND(j.MAX_SALARY/12,2) 'Média máxima mensal' FROM hr.jobs j ORDER BY `Variação Salarial`, Cargo;
