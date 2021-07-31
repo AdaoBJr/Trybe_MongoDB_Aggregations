b7d5315821df916143a655d879e3ecad60998942
Carlos Henrique Pereira Vieira <78491183+CIGR2021@users.noreply.github.com>
2021-07-31 11:53:58 -0300

    Mudei a query.
    Reformulei a query, pois não da forma que eu havia buscado os dados não passava no desafio 8, então segue a nova query!

--- desafio8.sql
+++ desafio8.sql
@@ -1,10 +1,21 @@
-SELECT CONCAT(wemployees.FirstName,' ', wemployees.LastName) AS `Nome de contato`,
-shippers.ShipperName AS `Empresa que fez o envio`,
-orders.OrderDate AS `Data do pedido`
-FROM w3schools.orders AS orders
+-- SELECT CONCAT(wemployees.FirstName,' ', wemployees.LastName) AS `Nome de contato`,
+-- shippers.ShipperName AS `Empresa que fez o envio`,
+-- orders.OrderDate AS `Data do pedido`
+-- FROM w3schools.orders AS orders
+-- JOIN w3schools.shippers AS shippers
+-- ON shippers.ShipperID = orders.ShipperID
+-- JOIN w3schools.employees AS wemployees
+-- ON wemployees.EmployeeID = orders.EmployeeID
+-- WHERE shippers.ShipperName IN (1, 2)
+-- ORDER BY `Nome de contato`, `Empresa que fez o envio`, `Data do pedido`;
+
+SELECT ContactName AS `Nome de contato`,
+ShipperName AS `Empresa que fez o envio`,
+OrderDate AS `Data do pedido`
+FROM w3schools.customers AS customers
+JOIN w3schools.orders AS orders
+ON customers.CustomerID = orders.CustomerID
 JOIN w3schools.shippers AS shippers
 ON shippers.ShipperID = orders.ShipperID
-JOIN w3schools.employees AS wemployees
-ON wemployees.EmployeeID = orders.EmployeeID
-WHERE shippers.ShipperName IN (1, 2)
+WHERE shippers.ShipperID IN (1, 2)
 ORDER BY `Nome de contato`, `Empresa que fez o envio`, `Data do pedido`;
