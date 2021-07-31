8050c09262b31f3280cbaa764b67023d37743230
Flavio Andrade <foa12381@gmail.com>
2021-07-31 09:56:07 -0300

    desafio2.3

--- desafio2.sql
+++ desafio2.sql
@@ -2,7 +2,7 @@ SELECT JOB_TITLE AS "Cargo",
 CASE
 WHEN MAX_SALARY between 5000 and 10000 then "Baixo"
 WHEN MAX_SALARY between 10001 and 20000 then "Médio"
-WHEN MAX_SALARY between 200001 and 30000 then "Alto"
+WHEN MAX_SALARY between 20001 and 30000 then "Alto"
 WHEN MAX_SALARY > 30000 then "Altíssimo"
 ELSE MAX_SALARY END AS "Nível" 
 FROM hr.jobs 
