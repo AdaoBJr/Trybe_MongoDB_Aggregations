f6898dd1ce9929e0ea8e4b922010a1c39940166d
Abimaelr <abimaelr.albu@gmail.com>
2021-07-31 19:47:15 -0300

    Feature 13

---
+++ desafio13.sql
@@ -0,0 +1,7 @@
+SELECT
+p.ProductName as 'Produto',
+p.Price as 'Preço'
+FROM w3schools.products as p, w3schools.order_details as od
+WHERE od.ProductID = p.ProductID AND od.Quantity > 80
+ORDER BY 'Produto'
+;
