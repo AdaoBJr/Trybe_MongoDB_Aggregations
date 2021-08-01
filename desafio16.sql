832a374d90058415abbeeb6a79a5dbc2f8c71d7f
Duda Wiltiner <eduardawiltiner@gmail.com>
2021-08-01 11:13:50 -0300

    desafios

--- desafio16.sql
+++ desafio16.sql
@@ -4,7 +4,7 @@ DELIMITER $$
 CREATE FUNCTION buscar_quantidade_de_empregos_por_funcionario(email_contact VARCHAR(200))
 RETURNS INT READS SQL DATA
 BEGIN
-	DECLARE result INT;
+DECLARE result INT;
   SELECT
     COUNT(*)
   FROM hr.job_history j
