df200733b118195f9c02b0d481629475e568e0d5
palenske <viniciuspalenske@gmail.com>
2021-08-01 20:25:44 -0300

    [fix]~refatora ordem do inner join

--- desafio6.sql
+++ desafio6.sql
@@ -1,8 +1,9 @@
-SELECT CONCAT(E.FIRST_NAME, ' ', E.LAST_NAME) AS 'Nome completo',
+SELECT CONCAT(E.FIRST_NAME, ' ', E.LAST_NAME) AS `Nome completo`,
 J.JOB_TITLE AS Cargo,
 JH.START_DATE AS 'Data de início do cargo',
 D.DEPARTMENT_NAME AS Departamento
-FROM hr.employees E INNER JOIN hr.jobs J ON E.JOB_ID = J.JOB_ID
-INNER JOIN hr.job_history JH ON E.JOB_ID = JH.JOB_ID
-LEFT JOIN hr.departments D ON JH.DEPARTMENT_ID = D.DEPARTMENT_ID
-ORDER BY CONCAT(E.FIRST_NAME, ' ', E.LAST_NAME) DESC, Cargo;
+FROM hr.employees E
+INNER JOIN hr.job_history JH ON JH.EMPLOYEE_ID = E.EMPLOYEE_ID
+INNER JOIN hr.jobs J ON J.JOB_ID = JH.JOB_ID
+INNER JOIN hr.departments D ON D.DEPARTMENT_ID = JH.DEPARTMENT_ID
+ORDER BY `Nome completo` DESC, Cargo;
