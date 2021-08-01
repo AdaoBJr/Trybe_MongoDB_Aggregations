a55c87771a141c17836e8a9599317d0380282c2d
bidaffonso <bidaffondo@gmail.com>
2021-08-01 15:58:06 -0400

    REQUISITO 11 TESTE 4

--- desafio11.sql
+++ desafio11.sql
@@ -1,11 +1,12 @@
 SELECT 
     C1.ContactName AS Nome,
     C1.Country AS País,
-    (COUNT(C2.Country) -1) AS `Número de compatriotas`        
+    (COUNT(C2.Country) - 1) AS `Número de compatriotas`
 FROM
-    w3schools.customers AS C1, w3schools.customers AS C2
-    WHERE
-	C1.Country = C2.Country
-	GROUP BY `Nome` , C1.Country      
-    HAVING `Número de compatriotas` > 0
+    w3schools.customers AS C1,
+    w3schools.customers AS C2
+WHERE
+    C1.Country = C2.Country
+GROUP BY `Nome` , C1.Country
+HAVING `Número de compatriotas` > 0
 ORDER BY Nome ASC;
