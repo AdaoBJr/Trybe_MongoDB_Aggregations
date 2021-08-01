f934bb3043d44af690b97039da2de873b192108e
Dângelo-Silva-Miranda <dangelo.silva.miranda@gmail.com>
2021-08-01 11:14:59 -0300

    refactor: :recycle: requimente 4 complete
    implements alternative solution

--- desafio4.sql
+++ desafio4.sql
@@ -1,6 +1,7 @@
 /* 4 - Exiba a média salarial e o nível de senioridade de todas as pessoas empregadas, agrupadas pelo cargo */
 
-SELECT
+/* SOLUCAO 1 */
+/* SELECT
   j.JOB_TITLE 'Cargo',
   ROUND(AVG(e.SALARY), 2) `Média salarial`,
   CASE
@@ -17,5 +18,29 @@ ON e.JOB_ID = j.JOB_ID
 GROUP BY j.JOB_TITLE
 ORDER BY
   `Média salarial`,
-  j.JOB_TITLE;
+  j.JOB_TITLE; */
 
+/* SOLUCAO 2 */
+SELECT
+  j.JOB_TITLE 'Cargo',
+  emp.`Média salarial`,
+  CASE
+    WHEN `Média salarial` BETWEEN 2000 AND 5800 THEN 'Júnior'
+    WHEN `Média salarial` <= 7500 THEN 'Pleno'
+    WHEN `Média salarial` <= 10500 THEN 'Sênior'
+    WHEN `Média salarial` > 10500 THEN 'CEO'
+  END AS 'Senioridade'
+FROM
+  (SELECT
+    e.JOB_ID,
+    ROUND(AVG(e.SALARY), 2) `Média salarial`
+  FROM
+    hr.employees e
+  GROUP BY e.JOB_ID) emp
+    INNER JOIN
+  hr.jobs j
+ON emp.JOB_ID = j.JOB_ID
+ORDER BY
+  `Média salarial`,
+  j.JOB_TITLE;
+  
