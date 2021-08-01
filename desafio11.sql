33b5c95bd46153db790c62371a0d286d3c131b72
Breno3B <brenorm@gmail.com>
2021-08-01 20:27:17 -0300

    11º requirement: Fix name column NOME

--- desafio11.sql
+++ desafio11.sql
@@ -1,13 +1,12 @@
 SELECT 
-    c.ContactName AS 'Nome de contato',
+    c.ContactName AS 'Nome',
     c.Country AS 'País',
     (SELECT 
-            COUNT(*)
+            (COUNT(*) -1)
         FROM
             w3schools.customers
         WHERE
-            Country = c.Country
-                AND ContactName != c.ContactName) AS `Número de compatriotas`
+            Country = c.Country) AS `Número de compatriotas`
 FROM
     w3schools.customers AS c
 HAVING `Número de compatriotas` != 0
