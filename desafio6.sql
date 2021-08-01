517cc4bc151aab8331c68b13a1e0ca564084b290
palenske <viniciuspalenske@gmail.com>
2021-08-01 19:04:39 -0300

    entrega desafio 6

---
+++ desafio6.sql
@@ -0,0 +1,8 @@
+SELECT CONCAT(E.FIRST_NAME, ' ', E.LAST_NAME) AS 'Nome completo',
+J.JOB_TITLE AS Cargo,
+JH.START_DATE AS 'Data de início do cargo',
+D.DEPARTMENT_NAME AS Departamento
+FROM hr.employees E INNER JOIN hr.jobs J ON J.JOB_ID = E.JOB_ID
+INNER JOIN hr.job_history JH ON JH.JOB_ID = E.JOB_ID
+INNER JOIN hr.departments D ON D.DEPARTMENT_ID = JH.DEPARTMENT_ID
+ORDER BY CONCAT(E.FIRST_NAME, ' ', E.LAST_NAME) DESC, Cargo
