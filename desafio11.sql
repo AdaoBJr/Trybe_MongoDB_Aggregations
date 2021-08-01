369e83bd7b36b4cc9ed62c577a956484c86fd077
bidaffonso <bidaffondo@gmail.com>
2021-08-01 15:27:54 -0400

    Requisito 11 Teste 2

--- desafio11.sql
+++ desafio11.sql
@@ -1,13 +1,11 @@
 SELECT
-	C1.ContactName AS Nome,
-    C1.Country AS País,
+C1.ContactName AS Nome,
+C1.Country AS País,
     (SELECT
     COUNT(*)
     FROM w3schools.customers AS C2
     WHERE 
     C1.Country = C2.Country
     AND C1.ContactName <> C2.ContactName) AS `Número de compatriotas`
-		
 FROM w3schools.customers AS C1
-
 ORDER BY Nome ASC;
