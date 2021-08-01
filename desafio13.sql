e909bb11fb881d29a8c37f5a79c12c2f9ae1f556
bidaffonso <bidaffondo@gmail.com>
2021-08-01 16:37:25 -0400

    REQUISITO 14 TESTE 1

--- desafio13.sql
+++ desafio13.sql
@@ -1,9 +1,9 @@
-SELECT
-ProductName AS Produto,
-Price AS Preço
-FROM w3schools.products
-INNER JOIN 
-w3schools.order_details ON products.ProductID = order_details.ProductID
+SELECT 
+    ProductName AS Produto, Price AS Preço
+FROM
+    w3schools.products
+        INNER JOIN
+    w3schools.order_details ON products.ProductID = order_details.ProductID
 GROUP BY order_details.ProductID
 HAVING MAX(order_details.Quantity) > 80
 ORDER BY Produto ASC;
