2af74a9f5d651a6d6cc5ac1132aeceba093fbd80
Thiago Leite <leite.tjs@gmail.com>
2021-08-01 17:59:58 -0300

    requi 16 fix

---
+++ desafio16.sql
@@ -0,0 +1,18 @@
+USE hr;
+DELIMITER $$
+
+CREATE FUNCTION buscar_quantidade_de_empregos_por_funcionario(email_employee VARCHAR(50))
+RETURNS INT READS SQL DATA
+BEGIN
+DECLARE total_de_empregos INT;
+SELECT COUNT(*)
+FROM hr.job_history AS jbh
+INNER JOIN hr.employees AS emp
+ON jbh.EMPLOYEE_ID = emp.EMPLOYEE_ID
+WHERE email = CONCAT('%', email_employee, '%')
+INTO total_de_empregos;
+RETURN total_de_empregos;	
+END
+$$
+
+DELIMITER ;
