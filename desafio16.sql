3f4b5312cdb7c34c4a0bb95da96d96c5b97f6f39
Duda Wiltiner <eduardawiltiner@gmail.com>
2021-08-01 11:12:44 -0300

    desafios

--- desafio16.sql
+++ desafio16.sql
@@ -5,13 +5,13 @@ CREATE FUNCTION buscar_quantidade_de_empregos_por_funcionario(email_contact VARC
 RETURNS INT READS SQL DATA
 BEGIN
 	DECLARE result INT;
-    SELECT
-      COUNT(*)
-    FROM hr.job_history j
-    INNER JOIN hr.employees e
-      ON e.EMPLOYEE_ID = j.EMPLOYEE_ID
-    WHERE e.EMAIL = email_contact INTO result;
-    RETURN result;
+  SELECT
+    COUNT(*)
+  FROM hr.job_history j
+  INNER JOIN hr.employees e
+    ON e.EMPLOYEE_ID = j.EMPLOYEE_ID
+  WHERE e.EMAIL = email_contact INTO result;
+  RETURN result;
 END $$
 
 DELIMITER ;
--- desafio4.sql
+++ desafio4.sql
@@ -1,18 +1,17 @@
 SELECT
   j.job_title AS 'Cargo',
   ROUND(AVG(e.SALARY), 2) AS 'Média salarial',
-  CASE
-    WHEN AVG(e.SALARY) >= 2000 AND
-      AVG(e.SALARY) <= 5800 THEN 'Júnior'
-    WHEN AVG(e.SALARY) >= 5801 AND
-      AVG(e.SALARY) <= 7500 THEN 'Pleno'
-    WHEN AVG(e.SALARY) >= 7501 AND
-      AVG(e.SALARY) <= 10500 THEN 'Sênior'
-    ELSE 'CEO'
-  END AS 'Senioridade'
+CASE
+  WHEN AVG(e.SALARY) >= 2000 AND
+    AVG(e.SALARY) <= 5800 THEN 'Júnior'
+  WHEN AVG(e.SALARY) >= 5801 AND
+    AVG(e.SALARY) <= 7500 THEN 'Pleno'
+  WHEN AVG(e.SALARY) >= 7501 AND
+    AVG(e.SALARY) <= 10500 THEN 'Sênior'
+  ELSE 'CEO'
+END AS 'Senioridade'
 FROM hr.employees e
 INNER JOIN hr.jobs j
   ON e.job_id = j.job_id
 GROUP BY j.JOB_TITLE
 ORDER BY AVG(e.SALARY);
- 
