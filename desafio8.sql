13176ff1b040097c6c7fdef7ec57d505d95c680e
Breno3B <brenorm@gmail.com>
2021-08-01 17:25:12 -0300

    8º requirement

---
+++ desafio8.sql
@@ -0,0 +1,16 @@
+SELECT 
+    c.ContactName AS 'Nome de contato',
+    s.ShipperName AS 'Empresa que fez o envio',
+    o.OrderDate AS 'Data do pedido'
+FROM
+    w3schools.customers AS c
+    RIGHT JOIN
+    w3schools.orders AS o
+    ON c.CustomerID = o.CustomerID 
+    LEFT JOIN
+    w3schools.shippers AS s
+    ON o.ShipperID = s.ShipperID
+WHERE
+   o.ShipperID IN (1 , 2)
+ORDER BY
+	1 ASC, 2 ASC, 3 ASC;
