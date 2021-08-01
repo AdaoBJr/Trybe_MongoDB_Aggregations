43b729cbfec983a1ec0537d4ca04e2af26716e7f
Thiago Leite <leite.tjs@gmail.com>
2021-08-01 11:27:08 -0300

    requi 15

---
+++ desafio15.sql
@@ -0,0 +1,14 @@
+USE HR;
+DELIMITER $$
+
+CREATE PROCEDURE buscar_media_por_cargo(IN selectedJob VARCHAR(50), OUT averageSalary DOUBLE)
+BEGIN
+	SELECT ROUND(AVG(emp.salary), 2) AS 'Média salarial'
+	FROM hr.employees AS emp
+	INNER JOIN hr.jobs AS jbs
+	ON emp.JOB_ID = jbs.JOB_ID
+	WHERE JOB_TITLE = selectedJob;
+END
+$$
+
+DELIMITER ;
