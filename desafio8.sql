787b6fe53cc049456491a9e841eb9895512c8e9b
Dângelo-Silva-Miranda <dangelo.silva.miranda@gmail.com>
2021-08-01 15:08:24 -0300

    feat: :sparkles: requirement 8 complete

---
+++ desafio8.sql
@@ -0,0 +1,17 @@
+/* 8 - Exibe todas as pessoas consumidoras cujos pedidos já foram enviados pelas empresas Speedy Express ou United Package */
+SELECT
+  c.ContactName `Nome de contato`,
+  s.ShipperName `Empresa que fez o envio`,
+  o.OrderDate `Data do pedido`
+FROM
+  (w3schools.orders o
+    INNER JOIN
+  w3schools.customers c
+  ON o.CustomerID = c.CustomerID)
+    INNER JOIN
+  w3schools.shippers s
+  ON s.ShipperID = o.ShipperID
+ORDER BY
+  `Nome de contato`,
+  `Empresa que fez o envio`,
+  `Data do pedido`;
