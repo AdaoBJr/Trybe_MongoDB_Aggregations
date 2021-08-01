91f49df0976ac62019ed0a19e72dc9ff731d9037
bidaffonso <bidaffondo@gmail.com>
2021-08-01 16:25:56 -0400

    REQUISITO 13 TESTE 1

---
+++ desafio13.sql
@@ -0,0 +1,9 @@
+SELECT
+ProductName AS Produto,
+Price AS Preço
+FROM w3schools.products
+INNER JOIN 
+w3schools.order_details ON products.ProductID = order_details.ProductID
+GROUP BY order_details.ProductID
+HAVING MAX(order_details.Quantity) > 80
+ORDER BY Produto ASC;
