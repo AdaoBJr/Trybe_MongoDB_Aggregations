08c78b1cb3db44d01db97c31143345e5a1fecbba
Lucas Ribeiro <lucasrb04@gmail.com>
2021-08-01 13:32:50 -0300

    corrigindo erro de history

--- desafio6.sql
+++ desafio6.sql
@@ -1,13 +1,13 @@
 SELECT
   CONCAT(E.FIRST_NAME, ' ', E.LAST_NAME) AS 'Nome completo',
   J.JOB_TITLE AS 'Cargo',
-  H.START_DATE AS 'Data de início do cargo',
+  E.HIRE_DATE AS 'Data de início do cargo',
   D.DEPARTMENT_NAME AS 'Departamento'
-FROM hr.employees AS E
+FROM hr.job_history AS H
 INNER JOIN hr.jobs AS J
-  ON J.JOB_ID = E.JOB_ID
+  ON J.JOB_ID = H.JOB_ID
 INNER JOIN hr.departments AS D
-  ON D.DEPARTMENT_ID = E.DEPARTMENT_ID
-INNER JOIN hr.job_history AS H
+  ON D.DEPARTMENT_ID = H.DEPARTMENT_ID
+INNER JOIN hr.employees AS E
   ON H.EMPLOYEE_ID = E.EMPLOYEE_ID
   ORDER BY CONCAT(E.FIRST_NAME, ' ', E.LAST_NAME) DESC, J.JOB_TITLE;
