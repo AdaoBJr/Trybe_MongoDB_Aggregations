fe5d0e2139dbfc64556d733a5cafa9aef4a4871a
Breno3B <brenorm@gmail.com>
2021-08-01 20:04:02 -0300

    11º requirement

---
+++ desafio11.sql
@@ -0,0 +1,13 @@
+SELECT 
+    c.ContactName AS 'Nome de contato',
+    c.Country AS 'País',
+    (SELECT 
+            (COUNT(*) -1)
+        FROM
+            w3schools.customers
+        WHERE
+            Country = c.Country) AS `Número de compatriotas`
+FROM
+    w3schools.customers AS c
+HAVING `Número de compatriotas` != 0
+ORDER BY 1 ASC;
