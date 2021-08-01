afe9d20f230b21523246c7828d01726bddd3b0d5
AndersonPedrosa35 <anderson.loiro222@gmail.com>
2021-08-01 18:27:05 -0300

    Correção de colunas

--- desafio15.sql
+++ desafio15.sql
@@ -1,11 +1,1 @@
-
-DELIMITER $$
-CREATE PROCEDURE buscar_media_por_cargo(IN cargo_pesquisado VARCHAR(40))
-BEGIN
-SELECT ROUND(SUM(SALARY) / COUNT(SALARY), 2) AS "Média salarial", j.JOB_ID, j.JOB_TITLE
-FROM hr.employees AS e INNER JOIN hr.jobs AS j ON j.JOB_ID = e.JOB_ID
-GROUP BY e.JOB_ID HAVING JOB_TITLE IN (cargo_pesquisado);
-END $$
-DELIMITER ;
-
-CALL buscar_media_por_cargo('Programmer');
+CALL buscar_media_por_cargo('Programmer')
