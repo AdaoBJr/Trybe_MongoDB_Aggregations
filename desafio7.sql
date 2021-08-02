4dcf07d71f1b6fba2b5d7ddea0980c50b66cf02c
Fioravante Chiozzi <f.ichiozzi@gmail.com>
2021-08-02 18:45:44 -0300

    Feat: Solução requisito 7

---
+++ desafio7.sql
@@ -0,0 +1,12 @@
+SELECT
+  UCASE(CONCAT(e.FIRST_NAME, ' ', e.LAST_NAME)) AS 'Nome completo',
+  jh.START_DATE AS 'Data de início',
+  e.SALARY AS 'Salário'
+FROM
+  employees AS e
+  INNER JOIN job_history AS jh ON e.EMPLOYEE_ID = e.EMPLOYEE_ID
+WHERE
+  MONTH(jh.START_DATE) IN (01, 02, 03)
+ORDER BY
+  `Nome completo`,
+  START_DATE;
