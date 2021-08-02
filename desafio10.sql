3e746675eaae3f5336efe6aad55d80585f8c7c35
MarceloPelegrino <pelegrinocorrea@gmail>
2021-08-02 15:22:46 -0300

    Desafio 10 - Corrigindo Code Climate 1

--- desafio10.sql
+++ desafio10.sql
@@ -3,10 +3,8 @@ SELECT
     MIN(orDe.Quantity) AS 'Mínima',
     MAX(orDe.Quantity) AS 'Máxima',
     ROUND(AVG(Quantity), 2) AS 'Média'
-FROM
-	w3schools.order_details AS orDe
-		INNER JOIN
-	products AS pro ON orDe.ProductID = pro.ProductID
+FROM w3schools.order_details AS orDe
+INNER JOIN products AS pro ON orDe.ProductID = pro.ProductID
 GROUP BY orDe.ProductID
-HAVING `Média` > 20.00
-ORDER BY `Média`, `Produto`;
+HAVING Média > 20.00
+ORDER BY Média , Produto;
