08df29d8125b5f439f4a8d5c270ddde72e8d7615
Dângelo-Silva-Miranda <dangelo.silva.miranda@gmail.com>
2021-08-02 20:57:26 -0300

    style: :fire: remove solution 2 test

--- desafio11.sql
+++ desafio11.sql
@@ -12,19 +12,3 @@ GROUP BY
   c1.ContactName, c1.Country
 ORDER BY
   `Nome`;
-/*SELECT
-  c1.ContactName `Nome`,
-  c1.Country `País`,
-  (SELECT
-    COUNT(c2.Country) `Número de compatriotas`
-  FROM
-    w3schools.customers c2
-  WHERE
-    c1.CustomerID <> c2.CustomerID AND 
-    c1.Country = c2.Country) `Número de compatriotas`
-FROM
-  w3schools.customers c1
-ORDER BY
-  `Nome`;
-  */
-  
