c2a59124d5498032fddae73b5eb2324d4fc60210
Caio <caioqs.info@gmail.com>
2021-08-02 11:40:28 -0300

    Requisito 10

---
+++ desafio10.sql
@@ -0,0 +1,7 @@
+SELECT PRO.ProductName AS 'Produto', MIN(O.Quantity) AS 'Mínima', MAX(O.Quantity) AS 'Máxima',
+ROUND(AVG(O.Quantity), 2) AS 'Média'
+FROM w3schools.products AS PRO
+INNER JOIN w3schools.order_details AS O
+ON PRO.ProductID = O.ProductID
+GROUP BY PRO.ProductName
+ORDER BY `Média`, `Produto`;
