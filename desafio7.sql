6897002582c746b5c563411b5a02d5f57eda16d3
Flavio Andrade <foa12381@gmail.com>
2021-08-02 12:46:58 -0300

    desafio7.1

---
+++ desafio7.sql
@@ -0,0 +1,10 @@
+select upper(concat(e.first_name, " ", e.last_name)) as "Nome completo",
+jh.START_DATE as "Data de início",
+e.SALARY as "Salário"
+from `hr`.`job_history` as jh
+INNER JOIN `hr`.`employees` as e
+on e.EMPLOYEE_ID = jh.EMPLOYEE_ID
+where month(jh.start_date) = 01
+or month(jh.start_date) = 02
+or month(jh.start_date) = 03
+order by  `Nome completo` , `Data de início`,
