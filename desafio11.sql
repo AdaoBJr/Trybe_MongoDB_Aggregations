ecf79e407e4e6a36d845a1441b3692f5aa0a69f5
palenske <viniciuspalenske@gmail.com>
2021-08-02 17:11:43 -0300

    fix style desafio 11

--- desafio11.sql
+++ desafio11.sql
@@ -1,4 +1,3 @@
 SELECT C1.ContactName AS Nome, C1.Country AS País,
-	(SELECT COUNT(*) FROM w3schools.customers C2
-		WHERE C1.Country = C2.Country AND C1.ContactName <> C2.ContactName) `Número de compatriotas`
+(SELECT COUNT(*) FROM w3schools.customers C2 WHERE C1.Country = C2.Country AND C1.ContactName <> C2.ContactName) `Número de compatriotas`
 FROM w3schools.customers C1 ORDER BY Nome;
