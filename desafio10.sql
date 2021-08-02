848e6652622c58c948e22b0f8b2b657b0dab8496
Lucas Ribeiro <lucasrb04@gmail.com>
2021-08-01 22:34:48 -0300

    req 10

---
+++ desafio10.sql
@@ -0,0 +1,13 @@
+SELECT
+  P.ProductName,
+  MIN(OD.Quantity) AS 'Mínima',
+  MAX(OD.Quantity) AS 'Máxima',
+  ROUND((MIN(OD.Quantity) + MAX(OD.Quantity)) / 2, 2) AS 'Média'
+FROM
+  w3schools.order_details AS OD
+INNER JOIN
+  w3schools.products AS P
+ON
+  P.ProductID = OD.ProductID
+GROUP BY OD.ProductID
+ORDER BY (MIN(OD.Quantity) + MAX(OD.Quantity)) / 2, P.ProductName;
