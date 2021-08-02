2c754718909ef9e3f7b1088b9a8871aef3069ff4
Lucas Ribeiro <lucasrb04@gmail.com>
2021-08-02 00:49:15 -0300

    limpando desafio 16

--- desafio16.sql
+++
@@ -1,8 +0,0 @@
-SELECT 
-  ROUND(AVG(E.SALARY),2) AS 'salario',
-  J.JOB_TITLE AS 'cargo'
- FROM hr.employees AS E
- INNER JOIN hr.jobs AS J 
- ON J.JOB_ID = E.JOB_ID
- WHERE J.JOB_TITLE = 'Programmer'
- GROUP BY J.JOB_TITLE;
