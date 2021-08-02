8fcb220f713d4e71198afb3632321c807deedc3c
Matheus Monteiro <monteiromsh@gmail.com>
2021-08-02 02:56:39 -0300

    desafio 11 corrigido

--- desafio11.sql
+++ desafio11.sql
@@ -1,1 +1,1 @@
-SELECT c.contactName as 'Nome', c.country as 'País', COUNT(c.country = c1.country) - 1 as 'Número de compatriotas' FROM w3schools.customers as c, w3schools.customers as c1 WHERE c.country = c1.country GROUP BY c.customerID ORDER BY c.contactName;
+SELECT c.contactName as 'Nome', c.country as 'País', COUNT(c.country = c1.country) - 1 as 'Número de compatriotas' FROM w3schools.customers as c, w3schools.customers as c1 WHERE (c.country = c1.country) GROUP BY c.customerID HAVING COUNT(c.country = c1.country) > 1 ORDER BY c.contactName;
