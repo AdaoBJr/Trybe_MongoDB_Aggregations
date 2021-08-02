205809929f634598dcdc4ba9a23778621ea64cb4
Flavio Andrade <foa12381@gmail.com>
2021-08-02 19:17:10 -0300

    desafio9.1

---
+++ desafio9.sql
@@ -0,0 +1,7 @@
+select concat(e.firstName, " ", e,lastName) as "Nome completo",
+count(*) as "Total de pedidos" 
+FROM `w3schools`.`orders` as o
+inner join `w3schools`.`employees` as e
+on e.EmployeeID = o.EmployeeID
+group by o.EmployeeID
+order by `Total de pedidos`;
