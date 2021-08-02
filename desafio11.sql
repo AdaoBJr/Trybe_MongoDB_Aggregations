9cab96e8131d58e45522f47ca879ca7c6636f41d
Dângelo-Silva-Miranda <dangelo.silva.miranda@gmail.com>
2021-08-02 18:56:11 -0300

    feat: :sparkles: requirement 11 complete  - solution 1 test

---
+++ desafio11.sql
@@ -0,0 +1,15 @@
+/* 11 - Exibe todas as pessoas clientes que possuem compatriotas, mostrando a quantidade de compatriotas para cada pessoa cliente */
+SELECT
+  c1.ContactName `Nome`,
+  c1.Country `País`,
+  COUNT(c1.Country) `Número de compatriotas`
+FROM
+  w3schools.customers c1,
+  w3schools.customers c2
+WHERE
+  c1.CustomerID <> c2.CustomerID AND c1.Country = c2.Country
+GROUP BY
+  c1.Country, c1.ContactName
+ORDER BY
+  `Nome`;
+
