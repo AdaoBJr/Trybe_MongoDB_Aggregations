036f0ba7951591ad4f4f2498914b98cdfa8c84f3
Nícolas Tozato <ntozato9@gmail.com>
2021-08-02 17:05:54 -0300

    requisito 15

---
+++ desafio15.sql
@@ -0,0 +1,12 @@
+DELIMITER $$
+
+CREATE PROCEDURE buscar_media_por_cargo(IN cargo VARCHAR(200))
+BEGIN
+	SELECT ROUND(AVG(e.SALARY), 2)
+FROM hr.employees AS e
+INNER JOIN hr.jobs AS j
+ON e.JOB_ID = j.JOB_ID
+WHERE j.JOB_TITLE = cargo;
+END $$
+
+DELIMITER ;
