1c78dca96a73ab08b8a0971eec087d6dcb56cbb0
Daniel <dsouzaborini@gmail.com>
2021-08-02 03:40:56 -0300

    fix req 8 and 11

---
+++ desafio11.sql
@@ -0,0 +1,8 @@
+SELECT
+  c1.ContactName 'Nome',
+    c1.Country 'País',
+    (SELECT COUNT(*) FROM w3schools.customers c2
+    WHERE c1.Country = c2.Country AND c1.ContactName <> c2.ContactName) 'Número de compatriotas'
+FROM w3schools.customers c1
+HAVING `Número de compatriotas` <> 0
+ORDER BY c1.ContactName;
