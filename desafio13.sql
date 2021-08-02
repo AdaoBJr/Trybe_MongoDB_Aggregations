463cb823aafffce40e228f54337a66a614117b74
Aladino Borges <74330016+AladinoBorges@users.noreply.github.com>
2021-08-02 20:04:26 -0300

    Requisito 13: 100%.

---
+++ desafio13.sql
@@ -0,0 +1,10 @@
+SELECT 
+    ProductName AS Produto, Price AS Preço
+FROM
+    w3schools.products AS product
+        INNER JOIN
+    w3schools.order_details AS details ON product.ProductID = details.ProductID
+WHERE
+    details.Quantity > 80
+GROUP BY Produto
+ORDER BY Produto ASC;
