8d61e91c64b1c214e744e70e5239db71f270642e
Adão Benites <adaobjr@gmail.com>
2021-08-02 11:09:23 -0400

    fix: :art: additional tabs

--- desafio16.sql
+++ desafio16.sql
@@ -3,12 +3,12 @@ DELIMITER $$
 CREATE FUNCTION buscar_quantidade_de_empregos_por_funcionario(selectedEmail VARCHAR(100))
 RETURNS INT READS SQL DATA
 BEGIN
-	DECLARE employees_count INT;
+DECLARE employees_count INT;
 SELECT
-	COUNT(*)
+COUNT(*)
 FROM
     hr.employees AS e
-	INNER JOIN hr.job_history AS jh ON e.employee_id = jh.employee_id
+INNER JOIN hr.job_history AS jh ON e.employee_id = jh.employee_id
     WHERE e.email = selectedEmail INTO employees_count;
     RETURN employees_count;
 END $$
