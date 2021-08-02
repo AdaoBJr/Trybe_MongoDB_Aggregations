92a5b9bad99544fba44e9d7283fc062b28b35400
palenske <viniciuspalenske@gmail.com>
2021-08-02 17:27:56 -0300

    [fix]~Usando HAVING para comparar o alias `Número de compatriotas`

--- desafio11.sql
+++ desafio11.sql
@@ -1,3 +1,4 @@
 SELECT C1.ContactName AS Nome, C1.Country AS País,
 (SELECT COUNT(*) FROM w3schools.customers C2 WHERE C1.Country = C2.Country AND C1.ContactName <> C2.ContactName) `Número de compatriotas`
-FROM w3schools.customers C1 ORDER BY Nome;
+FROM w3schools.customers C1
+HAVING `Número de compatriotas` <> 0 ORDER BY Nome;
