48514ce46201b02ab9cb41e581e4c31f75a8030d
Daniel <dsouzaborini@gmail.com>
2021-08-02 04:03:17 -0300

    add req 13

---
+++ desafio13.sql
@@ -0,0 +1,9 @@
+SELECT 
+	p.ProductName 'Produto',
+    p.Price 'Preço'
+FROM w3schools.order_details o
+INNER JOIN w3schools.products p ON o.ProductID = p.ProductID
+GROUP BY o.ProductID
+HAVING MAX(o.Quantity) > 80
+ORDER BY
+	p.ProductName;
