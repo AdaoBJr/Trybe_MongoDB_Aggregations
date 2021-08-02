bc5a673f4753f71accc5c4afe818af01791c682f
Aladino Borges <74330016+AladinoBorges@users.noreply.github.com>
2021-08-02 14:21:30 -0300

    Requisito 6: 101%.

--- desafio6.sql
+++ desafio6.sql
@@ -3,10 +3,13 @@ SELECT
             ' ',
             employees_table.LAST_NAME) AS 'Nome completo',
     jobs_table.JOB_TITLE AS Cargo,
-    HIRE_DATE AS 'Data de início do cargo',
+    history_table.START_DATE AS 'Data de início do cargo',
     departments_table.DEPARTMENT_NAME AS 'Departamento'
 FROM
-    hr.employees AS employees_table
+	hr.employees AS employees_table
+INNER JOIN
+	hr.job_history AS history_table
+    ON employees_table.EMPLOYEE_ID = history_table.EMPLOYEE_ID
 INNER JOIN
     hr.jobs AS jobs_table
     ON employees_table.JOB_ID = jobs_table.JOB_ID
