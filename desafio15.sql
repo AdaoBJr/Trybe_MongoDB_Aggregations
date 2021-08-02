53e7fe4c8e0226d835a9cdbd10f8b7dd3ae04c0c
Nícolas Tozato <ntozato9@gmail.com>
2021-08-02 17:08:42 -0300

    codeclimate

--- desafio15.sql
+++ desafio15.sql
@@ -2,7 +2,7 @@ DELIMITER $$
 
 CREATE PROCEDURE buscar_media_por_cargo(IN cargo VARCHAR(200))
 BEGIN
-	SELECT ROUND(AVG(e.SALARY), 2)
+SELECT ROUND(AVG(e.SALARY), 2)
 FROM hr.employees AS e
 INNER JOIN hr.jobs AS j
 ON e.JOB_ID = j.JOB_ID
