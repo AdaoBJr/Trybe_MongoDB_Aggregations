829d6d6a518b60aa78fda32b0516d1d9374a0bb1
palenske <viniciuspalenske@gmail.com>
2021-08-01 21:45:20 -0300

    entrega desafio 9

---
+++ desafio9.sql
@@ -0,0 +1,5 @@
+SELECT CONCAT(E.FirstName, ' ', E.LastName) AS `Nome completo`,
+COUNT(O.EmployeeID) AS `Total de pedidos`
+FROM w3schools.employees AS E
+RIGHT JOIN w3schools.orders AS O ON O.EmployeeID = E.EmployeeID
+GROUP BY O.EmployeeID ORDER BY `Total de pedidos`;
