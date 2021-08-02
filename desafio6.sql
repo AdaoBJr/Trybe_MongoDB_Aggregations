303dd09c2123fd996de7200944a4212797b8147c
Mariana Savoldi <marianasavodi123@gmail.com>
2021-08-01 23:33:18 -0300

    Desafio 6 test correção (ordenação errada)

--- desafio6.sql
+++ desafio6.sql
@@ -6,4 +6,4 @@ FROM hr.job_history AS jobh INNER JOIN jobs AS j
 ON jobh.JOB_ID = j.JOB_ID INNER JOIN employees AS e
 ON e.EMPLOYEE_ID = jobh.EMPLOYEE_ID INNER JOIN departments AS d
 ON d.DEPARTMENT_ID = jobh.DEPARTMENT_ID
-ORDER BY 1, 2;
+ORDER BY 1 DESC, 2;
