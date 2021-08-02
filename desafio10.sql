68e54c3c3aa0499c236b587a65d4b251708f569a
Aladino Borges <74330016+AladinoBorges@users.noreply.github.com>
2021-08-02 17:09:20 -0300

    Requisito 10: refatoração. Beta não funciona.

--- desafio10.sql
+++ desafio10.sql
@@ -1,13 +1,12 @@
-USE w3schools;
 SELECT 
     products_table.ProductName AS Produto,
     MIN(details_table.Quantity) AS Mínima,
     MAX(details_table.Quantity) AS Máxima,
     ROUND(AVG(details_table.Quantity), 2) AS Média
 FROM
-    order_details AS details_table
+    w3schools.order_details AS details_table
         INNER JOIN
-    products AS products_table ON details_table.ProductID = products_table.ProductID
+    w3schools.products AS products_table ON details_table.ProductID = products_table.ProductID
 GROUP BY Produto
 HAVING Média > 20.00
 ORDER BY Média ASC , Produto ASC;
