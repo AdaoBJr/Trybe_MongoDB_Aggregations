0f974bc3aa4bacc5fb5ac3fd2e9e447b180ac4ed
Joao Vitor Fidanza <vitor.fidanza@gmail.com>
2021-08-02 19:14:18 -0300

    desafio 1 vs3

--- desafio1.sql
+++ desafio1.sql
@@ -1,2 +1,1 @@
-select COUNTRY_NAME as 'País', IF(REGION_ID = 1,'incluído', 'não incluído') as 'Status Inclusão' from countries
-order by País;
+select COUNTRY_NAME as 'País', IF(REGION_ID = 1,'incluído', 'não incluído') as 'Status Inclusão' from countries order by País;
