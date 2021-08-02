a3c3049b549ca41d1c8ae5c684e516d72b4f1d42
CamilaCode7 <CamilaCode7>
2021-08-02 11:22:47 -0300

    atualizando desafio 15

--- desafio15.sql
+++ desafio15.sql
@@ -1,16 +1,14 @@
 -- use hr;
 delimiter $$
 create procedure buscar_media_por_cargo(
-  in cargo varchar(100),
-  OUT media_salarial double
+  in cargo varchar(100)
 )
 begin
   select round(avg(E.SALARY), 2)
-  as `Média salarial` into media_salarial
+  as `Média salarial`
   from hr.employees as E
   inner join hr.jobs as J on E.JOB_ID = J.JOB_ID
   where cargo = J.JOB_TITLE;
 end $$
 delimiter ;
-CALL buscar_media_por_cargo('Programmer', @media_salarial);
--- select @media_salarial;
+CALL buscar_media_por_cargo('Programmer');
