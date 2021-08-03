18fe515f40dc0e25c157d587790e80255cdee405
Holistic <paulofreire.si@outlook.com>
2021-08-03 18:50:22 -0300

    ççsdfl

--- desafio4.sql
+++ desafio4.sql
@@ -1,3 +1,12 @@
-select 
-j.job_title as 'Cargo', 
-round(avg(e.salary), 2) as 'Média salarial', case when avg(e.salary) between 2000 and 5800 then 'Júnior' when avg(e.salary) between 5801 and 7500 then 'Pleno' when avg(e.salary) between 7501 and 10500 then 'Sênior' when avg(e.salary) > 10500 then 'CEO' end as 'Senioridade' from jobs j inner join employees e on j.JOB_ID = e.JOB_ID group by 'Cargo' order by 'Média Salarial', 'Cargo';
+select j.JOB_TITLE as 'Cargo', ROUND(AVG(employees.SALARY), 2) as 'Média salarial',
+case
+when ROUND(AVG(e.SALARY), 2) between 2000 and 5800 then 'Júnior'
+when ROUND(AVG(e.SALARY), 2) between 5801 and 7500 then 'Pleno'
+when ROUND(AVG(e.SALARY), 2) between 7501 and 10500 then 'Sênior'
+else 'CEO'
+end as 'Senioridade'
+from hr.jobs as j
+inner join hr.employees as e
+on employees.JOB_ID = j.JOB_ID
+group by JOB_TITLE
+order by ROUND(AVG(e.SALARY), 2) asc, JOB_TITLE asc;
