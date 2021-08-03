0e26178b689560433303ecb769f15ac35801d7f7
AndersonPedrosa35 <anderson.loiro222@gmail.com>
2021-08-03 17:56:30 -0300

    Correção nome da tabela

--- desafio11.sql
+++ desafio11.sql
@@ -1,4 +1,5 @@
-SELECT c1.ContactName, c1.Country, COUNT(c1.Country) 
+SELECT c1.ContactName, c1.Country, COUNT(c1.Country) AS "Número de compatriotas"
 FROM w3schools.customers AS c1, w3schools.customers AS c2
-WHERE c1.Country = c2.Country AND c1.ContactName <> c2.ContactName GROUP BY Country, ContactName
+WHERE c1.Country = c2.Country AND c1.ContactName <> c2.ContactName 
+GROUP BY Country, ContactName
 ORDER BY c1.ContactName;
