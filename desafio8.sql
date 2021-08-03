efad1e62b73a358f9b436fc2e9ba2ccb8173a165
Yago Rocha de Souza Lima <yagorochasouza@gmail.com>
2021-08-03 17:58:53 -0300

    req8 implementation

---
+++ desafio8.sql
@@ -0,0 +1,10 @@
+SELECT customers.ContactName AS 'Nome de contato',
+shippers.ShipperName AS 'Empresa que fez o envio',
+orders.OrderDate AS 'Data do pedido'
+FROM w3schools.customers AS customers
+INNER JOIN w3schools.orders AS orders
+ON customers.CustomerID = orders.CustomerID
+INNER JOIN w3schools.shippers AS shippers
+ON shippers.ShipperID = orders.ShipperID
+WHERE ShipperName IN (1, 2)
+ORDER BY `Nome de contato`, `Empresa que fez o envio`, `Data do pedido`;
