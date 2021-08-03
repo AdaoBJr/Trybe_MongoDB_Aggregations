8ad0315aa9b550f771236f5cde6d99c295aa00e5
Joao Vanelli <vanellijoao@gmail.com>
2021-08-03 15:26:29 -0300

    Req11

--- desafio11.sql
+++ desafio11.sql
@@ -1,15 +1,4 @@
-SELECT 
-    c.ContactName AS 'Nome',
-    c.Country AS 'País',
-    (SELECT 
-            COUNT(*) - 1
-        FROM
-            customers c
-        WHERE
-            c.Country = País) AS 'Número de compartriotas'
-FROM
-    customers c
-WHERE
-    c.Country = c.Country
-GROUP BY c.ContactName , c.Country
-ORDER BY Nome;
+SELECT c1.ContactName 'Nome', c1.Country 'País', (SELECT COUNT(*) FROM w3schools.customers c2 WHERE c1.Country = c2.Country AND c1.ContactName <> c2.ContactName) `Número de compatriotas`
+FROM w3schools.customers c1
+HAVING `Número de compatriotas` <> 0
+ORDER BY c1.ContactName;
