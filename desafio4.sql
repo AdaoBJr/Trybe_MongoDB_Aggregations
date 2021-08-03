5ef385a543128c64a1110b2c5ad54c4604f66180
Holistic <paulofreire.si@outlook.com>
2021-08-03 18:43:19 -0300

    sdççlkkf

--- desafio4.sql
+++ desafio4.sql
@@ -1,3 +1,3 @@
-select
-j.job_title as 'Cargo',
-round(avg(e.salary), 2) as 'Média salarial', case when avg(e.salary) between 2000 and 5800 then 'Júnior' when avg(e.salary) between 5801 and 7500 then 'Pleno' when avg(e.salary) between 7501 and 10500 then 'Sênior' when avg(e.salary) > 10500 then 'CEO' end as 'Senioridade' from jobs j inner join employees e on j.JOB_ID = e.JOB_ID group by `Cargo` order by `Média Salarial`, `Cargo`;
+select 
+j.job_title as 'Cargo', 
+round(avg(e.salary), 2) as 'Média salarial', case when avg(e.salary) between 2000 and 5800 then 'Júnior' when avg(e.salary) between 5801 and 7500 then 'Pleno' when avg(e.salary) between 7501 and 10500 then 'Sênior' when avg(e.salary) > 10500 then 'CEO' end as 'Senioridade' from jobs j inner join employees e on j.JOB_ID = e.JOB_ID group by 'Cargo' order by 'Média Salarial', 'Cargo';
