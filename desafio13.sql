1eb5b63b017d91e108aa31168b755195db4aa795
palenske <viniciuspalenske@gmail.com>
2021-08-03 18:43:11 -0300

    [fix]~atribui alias para as colunas

--- desafio13.sql
+++ desafio13.sql
@@ -1,3 +1,3 @@
-SELECT ProductName, Price FROM w3schools.products
+SELECT ProductName AS Produto, Price AS Preço FROM w3schools.products
 WHERE ProductID in (SELECT ProductID FROM w3schools.order_details WHERE Quantity > 80)
-ORDER BY ProductName;
+ORDER BY Produto;
