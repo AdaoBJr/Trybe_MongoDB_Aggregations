7a3d17aeba57718070ab45ba3d7cf0b74bbec1ca
Miguel Dantas <miiguelalb@gmail.com>
2021-08-03 16:57:41 -0300

    Refatorando alguns requisitos

--- desafio2.sql
+++ desafio2.sql
@@ -1,8 +1,19 @@
 SELECT 
-    COUNTRY_NAME AS 'País',
-    IF(REGION_ID = 1,
-        'incluído',
-        'não incluído') AS 'Status Inclusão'
+    JOB_TITLE AS 'Cargo',
+    CASE
+        WHEN (MAX_SALARY > 4999 AND MAX_SALARY < 10001) THEN 'Baixo'
+        WHEN
+            (MAX_SALARY > 10000
+                AND MAX_SALARY < 20001)
+        THEN
+            'Médio'
+        WHEN
+            (MAX_SALARY > 20000
+                AND MAX_SALARY < 30001)
+        THEN
+            'Alto'
+        ELSE 'Altíssimo'
+    END AS 'Nível'
 FROM
-    hr.countries
-ORDER BY COUNTRY_NAME ASC;
+    hr.jobs
+ORDER BY JOB_TITLE;
