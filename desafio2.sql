532783e10879700983c848302c0b77a2175fb713
Pedro Toselli <pedrotrybe@gmail.com>
2021-08-03 20:18:53 -0300

    Requisito 2 - Refatorado

--- desafio2.sql
+++ desafio2.sql
@@ -16,6 +16,7 @@ SELECT job_title AS 'Cargo',
     WHEN max_salary BETWEEN 5000 AND 10000 THEN 'Baixo'
     WHEN max_salary BETWEEN 10001 AND 20000 THEN 'Médio'
     WHEN max_salary BETWEEN 20001 AND 30000 THEN 'Alto'
-    ELSE 'Altíssimo'
+    WHEN max_salary > 30000 THEN 'Altíssimo'
+    ELSE 'Sem categoria'
   END AS 'Nível'
 FROM jobs ORDER BY job_title;
