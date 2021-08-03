0f5f059ffc211f67a3b3ee8a5be9a082fb21fcbe
arthur-menezes202 <arthurmcorrea282@gmail.com>
2021-08-03 09:42:28 -0300

    desafios 18 ao 20

---
+++ desafio18.sql
@@ -0,0 +1,12 @@
+USE hr;
+DELIMITER $$
+CREATE FUNCTION exibir_quantidade_pessoas_contratadas_por_mes_e_ano(current_month VARCHAR(2), current_year VARCHAR(4))
+RETURNS INT READS SQL DATA
+BEGIN
+DECLARE hiredByMonthAndYear INT;
+SELECT COUNT(*) AS 'pessoas_contratadas'
+FROM employees
+WHERE month(hire_date) = current_month and year(hire_date) = current_year
+INTO hiredByMonthAndYear;
+RETURN hiredByMonthAndYear;
+END $$ DELIMITER ;
---
+++ desafio20.sql
@@ -0,0 +1,18 @@
+USE hr;
+DELIMITER $$
+CREATE PROCEDURE exibir_historico_completo_por_funcionario(IN email VARCHAR(20))
+BEGIN
+  SELECT CONCAT(e.First_Name, ' ', e.Last_Name) AS 'Nome completo',
+  d.DEPARTMENT_NAME AS 'Departamento',
+  j.JOB_TITLE AS 'Cargo'
+  FROM employees AS e
+  INNER JOIN job_history AS h
+  ON e.EMPLOYEE_ID = h.EMPLOYEE_ID
+  INNER JOIN jobs AS j
+  ON j.JOB_ID = h.JOB_ID
+  INNER JOIN departments AS d
+  ON d.DEPARTMENT_ID = h.DEPARTMENT_ID
+  WHERE e.EMAIL LIKE email
+  ORDER BY `Departamento`, `Cargo`;
+END $$
+DELIMITER ;
