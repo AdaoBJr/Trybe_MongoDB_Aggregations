ba41eae3406dd16b33586e93d4a6aba4a4c41e65
Aderson Santos <assaderson@gmail.com>
2021-08-03 17:03:54 -0300

    Requisito 10

---
+++ desafio10.sql
@@ -0,0 +1,1 @@
+SELECT products_table.ProductName Produto, MIN(details_table.Quantity) Mínima, MAX(details_table.Quantity) Máxima, ROUND(AVG(details_table.Quantity), 2) Média FROM    w3schools.order_details AS details_table INNER JOIN w3schools.products AS products_table ON details_table.ProductID = products_table.ProductID GROUP BY Produto HAVING Média > 20.00 ORDER BY Média ASC , Produto ASC;
