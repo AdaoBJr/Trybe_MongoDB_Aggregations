2e2a2719382d27316fa54493d96c19c2aa684a5b
Jonnes Bezerra <jonnes@pop-os.localdomain>
2021-08-03 00:54:23 -0300

    Fix: month 1 to 3

--- desafio7.sql
+++ desafio7.sql
@@ -1,8 +1,8 @@
 SELECT UCASE(CONCAT(em.FIRST_NAME, ' ', em.LAST_NAME)) 'Nome completo',
-START_DATE 'Data de início do cargo',
-SALARY 'Salário'
+jh.START_DATE 'Data de início do cargo',
+em.SALARY 'Salário'
 FROM hr.job_history jh
-INNER JOIN hr.employees em
-ON jh.EMPLOYEE_ID = em.EMPLOYEE_ID
+INNER JOIN hr.employees em ON jh.EMPLOYEE_ID = em.EMPLOYEE_ID
+WHERE MONTH(jh.START_DATE) BETWEEN 1 AND 3
 ORDER BY UCASE(CONCAT(em.FIRST_NAME, ' ', em.LAST_NAME)),
 START_DATE;
