a93c012d0b7912f42743a0ea2ca910ea100262a4
arthur-menezes202 <arthurmcorrea282@gmail.com>
2021-08-03 09:36:56 -0300

    desafios de 13 ao 16

---
+++ desafio16.sql
@@ -0,0 +1,14 @@
+DELIMITER $$
+CREATE FUNCTION buscar_quantidade_de_empregos_por_funcionario(email varchar(50))
+RETURNS INT READS SQL DATA
+BEGIN
+DECLARE jobs_total INT;
+select
+count(*)
+from job_history jh 
+inner join employees e on jh.EMPLOYEE_ID = e.EMPLOYEE_ID
+group by e.EMAIL
+having e.EMAIL = email INTO jobs_total;
+RETURN jobs_total;
+END $$
+DELIMITER ;
