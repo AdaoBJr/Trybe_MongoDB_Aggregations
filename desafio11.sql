c3d9b86e5fca511493a49c4b6996276dbd2c3063
AndersonPedrosa35 <anderson.loiro222@gmail.com>
2021-08-03 15:31:51 -0300

    Correção do code climate

--- desafio11.sql
+++ desafio11.sql
@@ -1,3 +1,3 @@
 SELECT c1.ContactName AS Nome, c1.Country AS País,
   ((SELECT Count(c2.Country) FROM w3schools.customers AS c2 GROUP BY c2.Country Having c1.Country = c2.Country) - 1) AS "Número de compatriotas" 
- FROM w3schools.customers AS c1 ORDER BY Nome;
+  FROM w3schools.customers AS c1 ORDER BY Nome;
