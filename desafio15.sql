5329a32e3007724873802d533193ee448ebdb517
MarceloPelegrino <pelegrinocorrea@gmail>
2021-08-03 14:53:48 -0300

    Desafio 15 --- Desafio 14 em andamento

---
+++ desafio15.sql
@@ -0,0 +1,11 @@
+DELIMITER $$
+CREATE PROCEDURE buscar_media_por_cargo(IN job VARCHAR(20))
+BEGIN
+	SELECT ROUND(AVG(SALARY), 2) AS 'Média salarial'
+	FROM hr.employees AS em
+	INNER JOIN jobs AS jo ON jo.JOB_ID = em.JOB_ID
+	WHERE JOB_TITLE=job;
+END $$
+DELIMITER ;
+
+CALL buscar_media_por_cargo('Programmer');
