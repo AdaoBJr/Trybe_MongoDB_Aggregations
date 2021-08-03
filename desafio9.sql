8567dc0a7a8bc1ec008796574b7ebe9cb97c27cb
Yago Rocha de Souza Lima <yagorochasouza@gmail.com>
2021-08-03 18:15:59 -0300

    req9 implementaion

---
+++ desafio9.sql
@@ -0,0 +1,7 @@
+SELECT CONCAT(employees.FirstName, ' ', employees.LastName) AS 'Nome completo',
+COUNT(*) AS 'Total de pedidos'
+FROM w3schools.employees AS employees
+INNER JOIN w3schools.orders AS orders
+ON employees.EmployeeID = orders.EmployeeID
+GROUP BY `Nome Completo`
+ORDER BY `Total de pedidos`;
