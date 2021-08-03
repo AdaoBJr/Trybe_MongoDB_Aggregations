b57930c87b8b4cdf2ac03e16b469c8f0e4ba5c8f
Mariana Savoldi <marianasavodi123@gmail.com>
2021-08-03 17:22:02 -0300

    Desafio 14 correção

--- desafio14.sql
+++ desafio14.sql
@@ -1,4 +1,4 @@
-(SELECT Country AS 'País' FROM w3schools.customers LIMIT 5)
+(SELECT Country AS 'País' FROM w3schools.customers)
 UNION
-(SELECT Country AS 'País' FROM w3schools.suppliers LIMIT 5)
+(SELECT Country AS 'País' FROM w3schools.suppliers)
 ORDER BY `País` LIMIT 5;
