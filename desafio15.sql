9d1f7d802b69e7933965bf5cb8baf824450ab4e4
Matheus Monteiro <monteiromsh@gmail.com>
2021-08-03 17:01:18 -0300

    code climate updates

--- desafio15.sql
+++ desafio15.sql
@@ -3,9 +3,10 @@ DELIMITER $$;
 
 CREATE PROCEDURE buscar_media_por_cargo(IN nomecargo VARCHAR(50))
 BEGIN
-	SELECT ROUND(AVG(e.salary), 2) as 'Média Salarial' FROM hr.employees as e INNER JOIN hr.jobs as j ON j.job_id = e.job_id WHERE job_title = nomecargo GROUP BY job_title;
+SELECT ROUND(AVG(e.salary), 2) as 'Média Salarial' FROM hr.employees as e INNER JOIN hr.jobs as j ON j.job_id = e.job_id WHERE job_title = nomecargo GROUP BY job_title;
 END $$;
 
 DELIMITER ;
 
 CALL buscar_media_por_cargo('Programmer')
+
