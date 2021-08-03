fd35d4e2d15173c1a46477d01a9de725dbf033d7
Jonnes Bezerra <jonnes@pop-os.localdomain>
2021-08-03 13:09:18 -0300

    Feat: requirement 11 DONE!

---
+++ desafio11.sql
@@ -0,0 +1,10 @@
+SELECT c1.ContactName "Nome",
+c1.Country "País",
+(SELECT COUNT(*)
+	FROM w3schools.customers c2
+    WHERE c1.Country = c2.Country AND c1.ContactName <> c2.ContactName) `Número de compatriotas`
+FROM w3schools.customers c1
+HAVING `Número de compatriotas` <> 0
+ORDER BY c1.ContactName;
+
+-- I get help from Mari Mohr's PR: https://github.com/tryber/sd-010-b-mysql-vocabulary-booster/blob/mariana-mohr-mysql-vocabulary-booster/desafio11.sql
