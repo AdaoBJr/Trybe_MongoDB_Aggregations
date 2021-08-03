d7042957b61625cefd95efb74b5fbe3c07b11fd7
AndersonPedrosa35 <anderson.loiro222@gmail.com>
2021-08-03 17:42:04 -0300

    Feito rquisito 11

--- desafio11.sql
+++ desafio11.sql
@@ -1,3 +1,4 @@
-SELECT c1.ContactName AS Nome, c1.Country AS País,
-  ((SELECT Count(c2.Country) FROM w3schools.customers AS c2 GROUP BY c2.Country Having c1.Country = c2.Country) - 1) AS "Número de compatriotas" 
-  FROM w3schools.customers AS c1 ORDER BY Nome;
+SELECT c1.ContactName, c1.Country, COUNT(c1.Country) 
+FROM w3schools.customers AS c1, w3schools.customers AS c2
+WHERE c1.Country = c2.Country AND c1.ContactName <> c2.ContactName GROUP BY Country, ContactName
+ORDER BY c1.ContactName;
