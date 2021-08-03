69b6e4fb4b86af0deb9104ab307a4ebad595ab30
Miguel Dantas <miiguelalb@gmail.com>
2021-08-03 17:23:58 -0300

    Requisitos 5 a 10 completos

---
+++ desafio10.sql
@@ -0,0 +1,12 @@
+SELECT 
+    product.ProductName AS 'Produto',
+    MIN(order_d.Quantity) AS 'Mínima',
+    MAX(order_d.Quantity) AS 'Máxima',
+    ROUND(AVG(order_d.Quantity), 2) AS 'Média'
+FROM
+    w3schools.order_details AS order_d
+        INNER JOIN
+    w3schools.products AS product ON order_d.ProductID = product.ProductID
+GROUP BY order_d.ProductID
+HAVING AVG(order_d.Quantity) > 20
+ORDER BY `Média` ASC , `Produto` ASC;
---
+++ desafio7.sql
@@ -0,0 +1,11 @@
+SELECT 
+    UCASE(CONCAT(e.FIRST_NAME, ' ', e.LAST_NAME)) AS 'Nome completo',
+    jh.START_DATE AS 'Data de início',
+    e.SALARY AS 'Salário'
+FROM
+    hr.employees AS e
+        INNER JOIN
+    hr.job_history AS jh ON e.EMPLOYEE_ID = jh.EMPLOYEE_ID
+WHERE
+    MONTH(jh.START_DATE) BETWEEN 1 AND 3
+ORDER BY `Nome completo` ASC , jh.START_DATE ASC;
