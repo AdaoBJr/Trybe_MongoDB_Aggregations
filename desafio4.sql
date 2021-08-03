4740c35d6b598ef6bf8bd3303d57b9d0d1a162b9
Luiz Reina <luizreeina@gmail.com>
2021-08-03 13:33:16 -0300

    requirements 3 - 9

---
+++ desafio4.sql
@@ -0,0 +1,9 @@
+SELECT jobs.job_title AS `Cargo`, ROUND(AVG(employ.salary), 2) AS `Média salarial`,
+CASE
+WHEN ROUND(AVG(employ.salary), 2) >= 2000 AND ROUND(AVG(employ.salary), 2) <= 5800 THEN ('Júnior')
+WHEN ROUND(AVG(employ.salary), 2) > 5800 AND ROUND(AVG(employ.salary), 2) <= 7501 THEN ('Pleno')
+WHEN ROUND(AVG(employ.salary), 2) > 7501 AND ROUND(AVG(employ.salary), 2) <= 10500 THEN ('Sênior')
+WHEN ROUND(AVG(employ.salary), 2) > 10500 THEN ('CEO')
+END AS `Senioridade` FROM hr.jobs as jobs
+INNER JOIN hr.employees as employ ON jobs.job_id = employ.job_id
+GROUP BY jobs.job_title ORDER BY `Média salarial`, Cargo;
---
+++ desafio6.sql
@@ -0,0 +1,6 @@
+SELECT CONCAT(employ.first_name, ' ', employ.last_name) AS `Nome completo`, jobs.job_title AS `Cargo`,
+jhistory.start_date AS `Data de início do cargo`, depart.DEPARTMENT_NAME AS `Departamento` FROM hr.employees employ
+INNER JOIN hr.job_history jhistory ON employ.employee_id = jhistory.employee_id
+INNER JOIN hr.jobs jobs ON jhistory.job_id = jobs.job_id
+INNER JOIN hr.departments depart ON jhistory.department_id = depart.department_id
+ORDER BY `Nome completo` DESC, `Cargo`;
