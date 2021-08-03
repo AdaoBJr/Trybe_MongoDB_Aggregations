45013dd59d04643a4414f1533c4120dec21742c0
Mariana Savoldi <marianasavodi123@gmail.com>
2021-08-02 23:26:25 -0300

    Desafio 7 testando outra coluna em comum

--- desafio7.sql
+++ desafio7.sql
@@ -2,6 +2,6 @@ SELECT UCASE(CONCAT(e.first_name, ' ', e.last_name)) AS 'Nome completo',
 jobh.START_DATE AS 'Data de início',
 e.SALARY AS 'Salário'
 FROM hr.job_history AS jobh INNER JOIN employees AS e
-ON e.JOB_ID = jobh.JOB_ID
+ON e.EMPLOYEE_ID = jobh.EMPLOYEE_ID
 WHERE MONTH(jobh.START_DATE) BETWEEN 1 AND 3
 ORDER BY 1, 2;
