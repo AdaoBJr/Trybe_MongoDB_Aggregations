bae302cb8a82d85893579c121c79cd7f5d04b377
Holistic <paulofreire.si@outlook.com>
2021-08-03 19:06:49 -0300

    baby come back, any kind of fool could see...

--- desafio5.sql
+++ desafio5.sql
@@ -1,1 +1,1 @@
-select job_title as 'Cargo' (max_salary = min_salary) as 'Variação Salarial' round(min_salary / 12, 2) as 'Média mínima mensal' round(max_salary / 12, 2) as 'Média máxima mensal' from hr.jobs order by (max_salary = min_salary), job_title;
+select JOB_TITLE 'Cargo', (MAX_SALARY - MIN_SALARY) 'Variação Salarial', round(MIN_SALARY/12, 2) 'Média mínima mensal', round(MAX_SALARY/12, 2) 'Média máxima mensal' from hr.jobs group by JOB_ID order by (MAX_SALARY - MIN_SALARY), JOB_TITLE;
