c716971359251a9471956f55c5948ad35569afc0
MarceloPelegrino <pelegrinocorrea@gmail>
2021-08-03 16:52:23 -0300

    Desafio 11

--- desafio11.sql
+++ desafio11.sql
@@ -1,20 +1,8 @@
 SELECT 
     w1.ContactName AS 'Nome',
-    Country AS 'País',
+    w1.Country AS 'País',
     count(w2.Country) As 'Número de compatriotas'
 FROM w3schools.customers AS w1, w3schools.customers AS w2
-GROUP BY `Nome`
-HAVING `Número de compatriotas` > 0
+WHERE w1.Country = w2.Country AND w1.ContactName <> w2.ContactName
+GROUP BY `Nome`, `País`
 ORDER BY `Nome`;
--- Error Code: 1052. Column 'ContactName' in field list is ambiguous
-
-
--- SELECT 
---     ContactName AS 'Nome',
---     Country AS 'País',
---     count(Country) As 'Número de compatriotas'
--- FROM w3schools.customers
--- GROUP BY `Nome`
--- HAVING `Número de compatriotas` > 0
--- ORDER BY `Nome`;
--- Error Code: 1055. Expression #2 of SELECT list is not in GROUP BY clause and contains nonaggregated column 'w3schools.customers.Country' which is not functionally dependent on columns in GROUP BY clause; this is incompatible with sql_mode=only_full_group_by
