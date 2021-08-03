8f3ba95384c882a4d725b3ff0f9a52f446d739a4
AndersonPedrosa35 <anderson.loiro222@gmail.com>
2021-08-03 18:03:54 -0300

    Correção de nomes de colunas

--- desafio11.sql
+++ desafio11.sql
@@ -1,5 +1,5 @@
-SELECT c1.ContactName, c1.Country, COUNT(c1.Country) AS "Número de compatriotas"
+SELECT c1.ContactName AS Nome, c1.Country AS País, COUNT(c1.Country) AS "Número de compatriotas"
 FROM w3schools.customers AS c1, w3schools.customers AS c2
 WHERE c1.Country = c2.Country AND c1.ContactName <> c2.ContactName 
-GROUP BY Country, ContactName
+GROUP BY c1.Country, c1.ContactName
 ORDER BY c1.ContactName;
