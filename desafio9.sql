5794a67d8fc86ed08363b9302a49bdfe9775f7ab
brunamfguimaraes <brunamfguimaraes@gmail.com>
2021-08-03 10:21:07 -0300

    Adicionando requisito 8 e 9

---
+++ desafio9.sql
@@ -0,0 +1,9 @@
+SELECT 
+    CONCAT(LastName, ' ', FirstName) AS 'Nome completo',
+    COUNT(*) 'Total de pedidos'
+FROM
+    w3schools.employees AS e
+        INNER JOIN
+    w3schools.orders AS o ON e.EmployeeID = o.EmployeeID
+GROUP BY o.EmployeeID
+ORDER BY COUNT(*) ASC;
