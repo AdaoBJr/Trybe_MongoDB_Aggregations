9279088c69c92913a4bc2ecfeb502ecf8dd44464
MarceloPelegrino <pelegrinocorrea@gmail>
2021-08-03 20:10:26 -0300

    Desafio 14

--- desafio14.sql
+++ desafio14.sql
@@ -1,12 +1,10 @@
 SELECT 
     Country AS 'País'
 FROM
-    w3schools.customers AS cust
-INNER JOIN orders AS ord ON cust.CustomerID = ord.CustomerID
-INNER JOIN order_details AS orDe ON ord.OrderID = orDe.OrderID
-INNER JOIN products AS pro ON orDe.ProductID = pro.ProductID
-INNER JOIN suppliers AS supp ON pro.SupplierID = supp.SupplierID
-WHERE cust.Country <> supp.Country
-ORDER BY `País`
+    w3schools.customers 
+UNION SELECT 
+    Country AS 'País'
+FROM
+    w3schools.suppliers
+ORDER BY País
 LIMIT 5;
--- Error Code: 1052. Column 'Country' in field list is ambiguous
