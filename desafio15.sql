cac3aa2d3cb17a7c18c4bc9db7a207308d7b6889
Matheus Monteiro <monteiromsh@gmail.com>
2021-08-03 17:11:35 -0300

    update desafio 15

--- desafio15.sql
+++ desafio15.sql
@@ -3,7 +3,7 @@ DELIMITER $$;
 
 CREATE PROCEDURE buscar_media_por_cargo(IN nomecargo VARCHAR(50))
 BEGIN
-SELECT ROUND(AVG(e.salary), 2) as 'Média Salarial' FROM hr.employees as e INNER JOIN hr.jobs as j ON j.job_id = e.job_id WHERE job_title = nomecargo GROUP BY job_title;
+SELECT ROUND(AVG(e.salary), 2) as 'Média salarial' FROM hr.employees as e INNER JOIN hr.jobs as j ON j.job_id = e.job_id WHERE job_title = nomecargo GROUP BY job_title;
 END $$;
 
 DELIMITER ;
