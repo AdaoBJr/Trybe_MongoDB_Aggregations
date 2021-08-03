816831d89690b4eaadb3a566a3edd5428ffbe925
arthur-menezes202 <arthurmcorrea282@gmail.com>
2021-08-03 09:10:43 -0300

    desafio 8

---
+++ desafio8.sql
@@ -0,0 +1,12 @@
+SELECT
+  customers.ContactName AS 'Nome de contato',
+  shippers.ShipperName AS 'Empresa que fez o envio',
+  orders.OrderDate AS 'Data do pedido'
+FROM w3schools.customers AS customers
+INNER JOIN w3schools.orders AS orders
+  ON orders.CustomerID = customers.CustomerID
+INNER JOIN w3schools.shippers AS shippers
+  ON shippers.ShipperID = orders.ShipperID
+WHERE shippers.ShipperName = 'Speedy Express'
+OR shippers.ShipperName = 'United Package'
+ORDER BY customers.ContactName ASC, shippers.ShipperName ASC, orders.OrderDate ASC;
