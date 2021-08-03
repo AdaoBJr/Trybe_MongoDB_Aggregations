13c3c3fc15f8c08ff656a626cd917ffaa5e3ae77
AndersonPedrosa35 <anderson.loiro222@gmail.com>
2021-08-03 15:37:55 -0300

    Feito requisito 14

--- desafio14.sql
+++ desafio14.sql
@@ -1,1 +1,3 @@
-SELECT DISTINCT Country AS País FROM w3schools.customers ORDER BY Country LIMIT 5;
+SELECT Country as 'País' FROM 
+(SELECT Country FROM w3schools.customers UNION SELECT Country FROM w3schools.suppliers) AS Países 
+ORDER BY `País` LIMIT 5;
