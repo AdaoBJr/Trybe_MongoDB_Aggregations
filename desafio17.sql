bd2e232c5a9527ab9f2de69146efeb5eeff6ab4a
Matheus Monteiro <monteiromsh@gmail.com>
2021-08-04 11:21:41 -0300

    update code climate em desafio 17

--- desafio17.sql
+++ desafio17.sql
@@ -2,8 +2,8 @@ USE w3schools;
 
 DELIMITER $$
 CREATE TRIGGER data_atual_automatica
-	BEFORE INSERT ON w3schools.orders
-    FOR EACH ROW
+BEFORE INSERT ON w3schools.orders
+FOR EACH ROW
 BEGIN 
 	SET NEW.orderdate = current_date();
 END $$
