216c08ef6f7166b665af97982ab7ddfcf20d2a41
palenske <viniciuspalenske@gmail.com>
2021-08-04 12:25:49 -0300

    fix style desafio 15

--- desafio15.sql
+++ desafio15.sql
@@ -3,9 +3,8 @@ DELIMITER $$
 
 CREATE PROCEDURE buscar_media_por_cargo(IN jobName VARCHAR(35))
 BEGIN
-	SELECT
-		ROUND(AVG(SALARY), 2) `Média salarial` FROM hr.employees
-        WHERE JOB_ID IN (SELECT JOB_ID FROM hr.jobs WHERE JOB_TITLE = jobName)
-        GROUP BY JOB_ID;
+SELECT ROUND(AVG(SALARY), 2) `Média salarial` FROM hr.employees
+WHERE JOB_ID IN (SELECT JOB_ID FROM hr.jobs WHERE JOB_TITLE = jobName)
+GROUP BY JOB_ID;
 END $$
 DELIMITER ;
