447d01258783f465f4f2f6d95eb1cdabb327cebb
Alexandre Damasceno <ale.damasceno.silva@gmail.com>
2021-08-03 22:45:32 -0300

    Challenge 4

--- desafio4.sql
+++ desafio4.sql
@@ -1,6 +1,7 @@
+-- Aprendi a arredondar nesse caso específico  nesse link: https://www.devmedia.com.br/forum/formatar-decimal-dentro-da-consulta-sql/158148
 SELECT 
     j.JOB_TITLE AS 'Cargo',
-    AVG(e.salary) AS 'Média salarial',
+    round(AVG(e.salary), 2) AS 'Média salarial',
     CASE
         WHEN AVG(e.salary) BETWEEN 2000 AND 5800 THEN 'Júnior'
         WHEN AVG(e.salary) BETWEEN 5801 AND 7500 THEN 'Pleno'
