2573620f947f432a294ff4532a9261141ad08d84
Holistic <paulofreire.si@outlook.com>
2021-08-04 12:45:17 -0300

    req elf

---
+++ desafio11.sql
@@ -0,0 +1,1 @@
+select ContactName as 'Nome', Country as 'País', (select count(*) - 1 from customers where Country = c.Country) as 'Número de compatriotas' from customers c where (select count(*) - 1 from customers where Country = c.Country) > 0 order by ContactName;
