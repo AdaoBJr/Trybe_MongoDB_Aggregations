48e57327e4087387d48b0ba9a217e5f757b91d61
Alexandre Damasceno <ale.damasceno.silva@gmail.com>
2021-08-03 23:57:04 -0300

    Challenge 6

--- desafio6.sql
+++ desafio6.sql
@@ -2,17 +2,16 @@
 -- select * from hr.employees;
 -- select * from hr.job_history;
 -- select * from hr.departments;
-SELECT 
-    CONCAT(e.first_name, ' ', e.last_name) AS 'Nome completo',
-    j.job_title AS 'Cargo',
-    jh.START_DATE AS 'Data de início do cargo',
-    d.department_name AS 'Departamento'
-FROM
-    hr.employees AS e
-        INNER JOIN
-    hr.jobs AS j ON e.JOB_ID = j.JOB_ID
-        INNER JOIN
-    hr.job_history AS jh ON e.EMPLOYEE_ID = jh.EMPLOYEE_ID
-        INNER JOIN
-    hr.departments AS d ON jh.DEPARTMENT_ID = d.DEPARTMENT_ID
-ORDER BY CONCAT(e.first_name, ' ', e.last_name) DESC , j.JOB_TITLE;
+SELECT
+  CONCAT(e.first_name, ' ', e.last_name) AS 'Nome completo',
+  j.job_title AS 'Cargo',
+  jh.START_DATE AS 'Data de início do cargo',
+  d.department_name AS 'Departamento'
+FROM hr.employees AS e
+INNER JOIN hr.jobs AS j
+  ON e.JOB_ID = j.JOB_ID
+INNER JOIN hr.job_history AS jh
+  ON e.EMPLOYEE_ID = jh.EMPLOYEE_ID
+INNER JOIN hr.departments AS d
+  ON jh.DEPARTMENT_ID = d.DEPARTMENT_ID
+ORDER BY CONCAT(e.first_name, ' ', e.last_name) DESC, j.JOB_TITLE;
