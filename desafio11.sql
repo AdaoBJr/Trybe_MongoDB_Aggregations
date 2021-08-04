d67ba06054093acdcccff2d2b800127b06778b21
Gustavo Cerqueira <gmcerqueira93@gmail.com>
2021-08-04 00:25:18 -0300

    desafio 11 correction

--- desafio11.sql
+++ desafio11.sql
@@ -1,7 +1,7 @@
 SELECT
-  t1.ContactName,
-  t1.Country,
-  COUNT(t2.ContactName)
+  t1.ContactName AS Nome,
+  t1.Country AS País,
+  COUNT(t2.ContactName) AS 'Número de compatriotas'
 FROM
   w3schools.customers AS t1,
   w3schools.customers AS t2
@@ -9,7 +9,7 @@ WHERE
   t1.Country = t2.Country
   AND t1.ContactName <> t2.ContactName
 GROUP BY
-  t1.ContactName,
-  t1.Country
+  Nome,
+  País
 ORDER BY
-  t1.ContactName;
+  Nome;
