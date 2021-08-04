0c57733c76feccba510520d7f806bbcdc0793f72
Alexandre Damasceno <ale.damasceno.silva@gmail.com>
2021-08-03 22:39:30 -0300

    Challenge 4

--- desafio4.sql
+++ desafio4.sql
@@ -1,14 +1,15 @@
-select * from hr.employees;
-select * from hr.jobs;
-SELECT j.JOB_TITLE as 'Cargo', avg(e.salary) as 'Média salarial',
-case
-	when avg(e.salary) BETWEEN 2000 AND 5800 THEN 'Júnior'
-    when avg(e.salary) BETWEEN 5801 AND 7500 THEN 'Pleno'
-	when avg(e.salary) BETWEEN 7501 AND 10500 THEN 'Sênior'
-    ELSE 'CEO'
-end as 'Senioridade'
-from  hr.employees as e
-inner join hr.jobs as j
-on e.JOB_ID = j.JOB_ID
-group by e.JOB_ID
-order by avg(e.salary), j.JOB_TITLE;
+SELECT 
+    j.JOB_TITLE AS 'Cargo',
+    AVG(e.salary) AS 'Média salarial',
+    CASE
+        WHEN AVG(e.salary) BETWEEN 2000 AND 5800 THEN 'Júnior'
+        WHEN AVG(e.salary) BETWEEN 5801 AND 7500 THEN 'Pleno'
+        WHEN AVG(e.salary) BETWEEN 7501 AND 10500 THEN 'Sênior'
+        ELSE 'CEO'
+    END AS 'Senioridade'
+FROM
+    hr.employees AS e
+        INNER JOIN
+    hr.jobs AS j ON e.JOB_ID = j.JOB_ID
+GROUP BY e.JOB_ID
+ORDER BY AVG(e.salary) , j.JOB_TITLE;
