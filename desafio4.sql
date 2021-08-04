70efa2c5132d54958ac58f076187b494edc8ad2d
Michael Mascarenhas <michaelmascarenhastrybe@gmail.com>
2021-08-04 10:55:12 -0300

    desafio 4

---
+++ desafio4.sql
@@ -0,0 +1,16 @@
+SELECT 
+    j.JOB_TITLE AS 'Cargo',
+    ROUND(AVG(e.salary), 2) AS 'Média salarial',
+    CASE
+        WHEN AVG(e.salary) BETWEEN 2000 AND 5800 THEN 'Júnior'
+        WHEN AVG(e.salary) BETWEEN 5801 AND 7500 THEN 'Pleno'
+        WHEN AVG(e.salary) BETWEEN 7501 AND 10500 THEN 'Sênior'
+        ELSE 'CEO'
+    END AS 'Senioridade'
+FROM
+    hr.employees AS e
+        INNER JOIN
+    hr.jobs AS j ON e.JOB_ID = j.JOB_ID
+GROUP BY e.JOB_ID
+ORDER BY AVG(e.salary) , j.JOB_TITLE;
+    
