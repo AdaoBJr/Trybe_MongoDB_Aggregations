0edc3051f2aee185e13bf9c2272e48cfc509c42c
Alexandre Damasceno <ale.damasceno.silva@gmail.com>
2021-08-04 01:49:29 -0300

    Challenge 8

---
+++ desafio8.sql
@@ -0,0 +1,18 @@
+-- select * from w3schools.customers;
+-- select * from w3schools.shippers;
+-- select * from w3schools.order_details;
+-- select * from w3schools.orders;
+SELECT 
+    c.ContactName AS 'Nome de contato',
+    s.ShipperName AS 'Empresa que fez o envio',
+    o.OrderDate AS 'Data do pedido'
+FROM
+    w3schools.customers AS c
+        INNER JOIN
+    w3schools.orders AS o ON c.CustomerID = o.CustomerID
+        INNER JOIN
+    w3schools.shippers AS s ON o.ShipperID = s.ShipperID
+WHERE
+    s.ShipperName = 'Speedy Express'
+        OR s.ShipperName = 'United Package'
+ORDER BY ContactName , ShipperName , OrderDate;
