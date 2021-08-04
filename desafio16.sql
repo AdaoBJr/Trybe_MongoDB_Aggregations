1bcb50bfd955a08f22a80e4d7318892a03c7a5a8
Flavio Andrade <foa12381@gmail.com>
2021-08-04 10:25:11 -0300

    16.1

--- desafio16.sql
+++ desafio16.sql
@@ -1,1 +1,16 @@
-use `hr`;
+use `hr`
+DELIMITER $$
+
+CREATE FUNCTION buscar_quantidade_de_empregos_por_funcionario(email VARCHAR(50))
+RETURNS INT READS SQL DATA
+BEGIN
+DECLARE MENSAGEM INT;
+SELECT COUNT(jh.JOB_ID)
+FROM `hr`.`job_history`  as jh
+INNER JOIN `hr`.`employees` as e
+on e.email = email
+where e.employees_id = jh.employees_id
+INTO MENSAGEM;
+RETURN MENSAGEM;
+END $$
+DELIMITER ;
