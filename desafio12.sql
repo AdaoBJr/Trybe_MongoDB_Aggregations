05b40dacd850972398740f50279eb28378371478
Gustavo Cerqueira <gmcerqueira93@gmail.com>
2021-08-04 00:45:20 -0300

    desafio 12 correction

--- desafio12.sql
+++ desafio12.sql
@@ -4,7 +4,7 @@ SELECT
   t1.PHONE_NUMBER AS 'Telefone funcionário 1',
   CONCAT(t2.FIRST_NAME, ' ', t2.LAST_NAME) AS 'Nome completo funcionário 2',
   t2.SALARY AS 'Salário funcionário 2',
-  t1.PHONE_NUMBER AS 'Telefone funcionário 2'
+  t2.PHONE_NUMBER AS 'Telefone funcionário 2'
 FROM
   hr.employees AS t1,
   hr.employees AS t2
