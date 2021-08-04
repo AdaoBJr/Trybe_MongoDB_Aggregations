1af1c04814eeaeca0cd01da705e0e8f381a56d03
Alexandre Damasceno <ale.damasceno.silva@gmail.com>
2021-08-04 00:14:14 -0300

    Challenge 6 again

--- desafio6.sql
+++ desafio6.sql
@@ -5,10 +5,10 @@ SELECT
   d.department_name AS 'Departamento'
 FROM 
 	hr.employees AS e
-		INNER JOIN
-	hr.jobs AS j ON e.JOB_ID = j.JOB_ID
-		INNER JOIN
-	hr.job_history AS jh ON e.EMPLOYEE_ID = jh.EMPLOYEE_ID
-		INNER JOIN
-	hr.departments AS d ON jh.DEPARTMENT_ID = d.DEPARTMENT_ID
-ORDER BY CONCAT(e.first_name, ' ', e.last_name) DESC, j.JOB_TITLE;
+INNER JOIN hr.jobs AS j
+  ON e.JOB_ID = j.JOB_ID
+INNER JOIN hr.job_history AS jh
+  ON e.EMPLOYEE_ID = jh.EMPLOYEE_ID
+INNER JOIN hr.departments AS d
+  ON jh.DEPARTMENT_ID = d.DEPARTMENT_ID
+ORDER BY `Nome completo` DESC, j.JOB_TITLE;
