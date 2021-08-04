aee5215a85ca41e706f777266220fade4672be2e
Holistic <paulofreire.si@outlook.com>
2021-08-04 11:53:25 -0300

    req ninee

---
+++ desafio9.sql
@@ -0,0 +1,1 @@
+select concat(e.FirstName, ' ', e.LastName) 'Nome completo', count(*) 'Total de pedidos' from w3schools.employees e inner join w3schools.orders o on e.EmployeeID = o.EmployeeID group by o.EmployeeID order by count(*);
