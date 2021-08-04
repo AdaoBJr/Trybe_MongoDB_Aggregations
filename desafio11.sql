4f1fa5612d48c22a93baa3ffa2b8cd7628b98bcc
João Herculano de Souza Neto <joaohsneto@gmail.com>
2021-08-04 04:22:02 -0300

    Resolvido o desafio 11

---
+++ desafio11.sql
@@ -0,0 +1,1 @@
+SELECT a.ContactName AS 'Nome', a.Country AS 'País', COUNT(b.Country) - 1 AS 'Número de compatriotas' FROM w3schools.customers AS a, w3schools.customers AS b WHERE a.Country = b.Country GROUP BY a.ContactName , a.Country ORDER BY a.ContactName;
