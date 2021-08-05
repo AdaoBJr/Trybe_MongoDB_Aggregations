6be8a6c672ed9341b108d1064b0ad7e409eab04c
Alexandre Damasceno <ale.damasceno.silva@gmail.com>
2021-08-05 17:01:36 -0300

    Challenge 10

---
+++ desafio10.sql
@@ -0,0 +1,19 @@
+-- select * from w3schools.products;
+-- show tables from w3schools;
+-- select * from w3schools.order_details;
+SELECT ProductName AS 'Produto', (
+	SELECT min(quantity)
+    FROM w3schools.order_details
+    WHERE productID = w3schools.products.productID
+) AS 'Mínima', (
+	SELECT max(quantity)
+    FROM w3schools.order_details
+    WHERE productID = w3schools.products.productID 
+) AS 'Máxima', (
+	SELECT round(AVG(quantity), 2)
+    FROM w3schools.order_details
+    WHERE productID = w3schools.products.productID
+) AS 'Média'
+FROM w3schools.products
+HAVING `Média` > 20.00 -- Primeiro eu use o WHERE aqui, só que não deu certo, depois testei o having e deu certo, porém não entendi o motivo disso já que o having é pra ser usado em agrupamentos, sendo que eu não usei o GROUP BY. ???
+ORDER BY `Média`, `Produto`;
