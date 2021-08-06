dc3374f1c6d62ee32f08e9e7d64f0dba78490341
Alexandre Damasceno <ale.damasceno.silva@gmail.com>
2021-08-06 01:43:34 -0300

    Challenge 18

--- desafio18.sql
+++ desafio18.sql
@@ -5,10 +5,9 @@ SELECT
     CONCAT(e.First_name, ' ', e.last_name) AS 'Nome completo',
     DATE_FORMAT(jh.start_date, '%d/%m/%Y') AS 'Data de início',
     DATE_FORMAT(jh.end_date, '%d/%m/%Y') AS 'Data de recisão',
-    ROUND((DATEDIFF(jh.end_date, jh.start_date) / 365),
-            2) AS 'Anos trabalhados'
+    ROUND((DATEDIFF(jh.end_date, jh.start_date) / 365), 2) AS 'Anos trabalhados'
 FROM
     hr.employees AS e
         INNER JOIN
     hr.job_history AS jh ON e.EMPLOYEE_ID = jh.EMPLOYEE_ID
-ORDER BY `Nome completo` , `Anos trabalhados`;
+ORDER BY CONCAT(e.First_name, ' ', e.last_name), ROUND((DATEDIFF(jh.end_date, jh.start_date) / 365), 2);
