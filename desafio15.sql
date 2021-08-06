4c7779ec24fe428a90200f66458f72201761de83
Alexandre Damasceno <ale.damasceno.silva@gmail.com>
2021-08-05 22:27:16 -0300

    Challenge 15

---
+++ desafio15.sql
@@ -0,0 +1,17 @@
+DELIMITER $$
+
+CREATE PROCEDURE buscar_media_por_cargo(IN mediaJob VARCHAR(50))
+BEGIN
+  SELECT 
+    ROUND(AVG(e.salary), 2)
+FROM
+    hr.jobs AS j
+        INNER JOIN
+    hr.employees AS e ON e.job_id = j.job_id
+WHERE
+    j.job_title = mediaJob;
+END $$
+
+DELIMITER ;
+
+CALL buscar_media_por_cargo('Programmer');
