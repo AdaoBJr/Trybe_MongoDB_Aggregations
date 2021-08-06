f9a4e713b3f0bcb7762dc502e26462a9968bfb0e
Alexandre Damasceno <ale.damasceno.silva@gmail.com>
2021-08-05 21:08:06 -0300

    Challenge 13

---
+++ desafio13.sql
@@ -0,0 +1,12 @@
+-- select * from w3schools.products;
+-- select sum(Quantity) from w3schools.order_details where productID = 1;
+-- select * from w3schools.order_details where productID = 1;
+SELECT 
+    p.ProductName, p.Price AS 'Preço'
+FROM
+    w3schools.products AS p
+        INNER JOIN
+    w3schools.order_details AS d ON p.productID = d.productID
+WHERE
+    d.quantity > 80
+ORDER BY ProductName;
