a868c089e7af397d52906c933db4249bcb13bf54
Alexandre Damasceno <ale.damasceno.silva@gmail.com>
2021-08-06 01:34:09 -0300

    Challenge 18

---
+++ desafio18.sql
@@ -0,0 +1,15 @@
+-- select * from hr.employees;
+-- select * from hr.job_history;
+
+-- Busquei aprender a usar o date_format nesse link: https://www.gigasystems.com.br/artigo/75/formatando-datas-no-mysql-com-date_format.
+
+SELECT 
+    CONCAT(e.First_name, ' ', e.last_name) AS 'Nome completo',
+    DATE_FORMAT(jh.start_date, '%d/%m/%Y') AS 'Data de início',
+    DATE_FORMAT(jh.end_date, '%d/%m/%Y') AS 'Data de recisão',
+    ROUND((DATEDIFF(jh.end_date, jh.start_date) / 365), 2) AS 'Anos trabalhados'
+FROM
+    hr.employees AS e
+        INNER JOIN
+    hr.job_history AS jh ON e.EMPLOYEE_ID = jh.EMPLOYEE_ID
+ORDER BY `Nome completo` , `Anos trabalhados`;
