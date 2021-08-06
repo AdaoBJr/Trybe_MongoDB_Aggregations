45b452b4e215531da082ac781c2da21f30f2dc1f
ricardo-rals <ricardo.rals.dev@gmail.com>
2021-08-06 10:19:16 -0300

    Desafio 3 - fix codeclimate Desafio 1

--- desafio1.sql
+++ desafio1.sql
@@ -1,5 +1,5 @@
 SELECT country_name AS País,
-         IF(region_id = 1,
-         'incluído', 'não incluído') AS 'Status Inclusão'
+        IF(region_id = 1,
+        'incluído', 'não incluído') AS 'Status Inclusão'
 FROM hr.countries
-ORDER BY  country_name ASC; 
+ORDER BY  country_name ASC;
