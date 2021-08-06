d510fd81ebf6f596bfb007b524d7c3b65ba44286
Alexandre Damasceno <ale.damasceno.silva@gmail.com>
2021-08-06 02:43:11 -0300

    Challenge 12

--- desafio12.sql
+++ desafio12.sql
@@ -1,10 +1,10 @@
 -- select * from hr.employees;
 SELECT 
     CONCAT(e1.first_name, ' ', e1.last_name) AS 'Nome completo funcionário 1',
+	CONCAT(e2.first_name, ' ', e2.last_name) AS 'Nome completo funcionário 2',
     e1.salary AS 'Salário funcionário 1',
+	e2.salary AS 'Salário funcionário 2',
     e1.phone_number AS 'Telefone funcionário 1',
-    CONCAT(e2.first_name, ' ', e2.last_name) AS 'Nome completo funcionário 2',
-    e2.salary AS 'Salário funcionário 2',
     e2.phone_number AS 'Telefone funcionário 2'
 FROM
     hr.employees AS e1,
