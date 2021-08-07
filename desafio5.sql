97b3b7dc8e7a824202114c057a77423a276ac9b1
Bruna Campos <92brunacampos@gmail.com>
2021-08-07 03:24:38 -0300

    codeclimate

--- desafio5.sql
+++ desafio5.sql
@@ -2,7 +2,7 @@ select
   job_title as 'Cargo',
   max_salary - jobs.min_salary as 'Variação Salarial',
   ROUND(min_salary, 2) as 'Média mínima',
-	ROUND(max_salary, 2) as 'Média máxima mensal'
+  ROUND(max_salary, 2) as 'Média máxima mensal'
 from
   hr.jobs
 ORDER BY
