40f20dfa7495d69319cea2c3a000c796959e2ef4
Luiz Paulo Lima <limaluizpaulo@live.com>
2021-08-08 04:17:00 -0300

    requisito 19 refeito

--- desafio19.sql
+++ desafio19.sql
@@ -1,11 +1,11 @@
 USE hr;
-DELIMITER $ $ CREATE FUNCTION exibir_quantidade_pessoas_contratadas_por_m_e_a(m INT, a INT) RETURNS INT READS SQL DATA BEGIN DECLARE total_hired INT;
+DELIMITER $ $ CREATE FUNCTION exibir_quantidade_pessoas_contratadas_por_mes_e_ano(mes INT, ano INT) RETURNS INT READS SQL DATA BEGIN DECLARE total_hired INT;
 SELECT
   COUNT(*)
 FROM
   hr.employees
 WHERE
-  MONTH(HIRE_DATE) = m
-  AND YEAR(HIRE_DATE) = a INTO total_hired;
+  MONTH(HIRE_DATE) = mes
+  AND YEAR(HIRE_DATE) = ano INTO total_hired;
 RETURN total_hired;
 END $ $ DELIMITER;
