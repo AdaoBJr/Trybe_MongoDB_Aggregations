22c0ed551cbe25ee18f8c141b43e3bf375b6cf8d
Bruna Campos <92brunacampos@gmail.com>
2021-08-09 10:48:53 -0300

    code climate

--- desafio10.sql
+++ desafio10.sql
@@ -1,14 +1,15 @@
-
-SELECT 
-    products.ProductName AS 'Produto',
-    MIN(order_details.Quantity) as 'Mínima',
-    MAX(order_details.Quantity) as 'Máxima',
-    ROUND(AVG(order_details.Quantity), 2) as 'Média'
+SELECT
+  products.ProductName AS 'Produto',
+  MIN(order_details.Quantity) as 'Mínima',
+  MAX(order_details.Quantity) as 'Máxima',
+  ROUND(AVG(order_details.Quantity), 2) as 'Média'
 FROM
-	w3schools.order_details as order_details
-        INNER JOIN
-         w3schools.products AS products
-    on order_details.ProductID = products.ProductID
-GROUP BY 1
-HAVING Média > 20
-ORDER BY 4, 1;
+  w3schools.order_details as order_details
+  INNER JOIN w3schools.products AS products on order_details.ProductID = products.ProductID
+GROUP BY
+  1
+HAVING
+  M é dia > 20
+ORDER BY
+  4,
+  1;
