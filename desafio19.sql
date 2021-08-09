06f3b286f1d5dc60398271cb03ef2ac2cbf3bbd5
Alexandre Damasceno <ale.damasceno.silva@gmail.com>
2021-08-09 14:00:42 -0300

    Challenge 19

---
+++ desafio19.sql
@@ -0,0 +1,18 @@
+DELIMITER $$
+
+CREATE FUNCTION exibir_quantidade_pessoas_contratadas_por_mes_e_ano(mes INT, ano INT)
+RETURNS INT READS SQL DATA
+BEGIN
+	DECLARE total INT;
+SELECT 
+    COUNT(*)
+FROM
+    hr.employees
+WHERE
+    HIRE_DATE LIKE CONCAT(ano, '-', '%', mes, '-', '%') INTO total;
+    RETURN total;
+END $$
+
+DELIMITER ;
+
+SELECT EXIBIR_QUANTIDADE_PESSOAS_CONTRATADAS_POR_MES_E_ANO(6, 1987);
