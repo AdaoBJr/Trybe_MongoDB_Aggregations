0b9d1e2dc18414a6b16b1788ae6f70824cf117d5
Matheus H. Gois <matheus.hengo@gmail.com>
2021-08-08 23:23:03 -0300

    11

---
+++ desafio11.sql
@@ -0,0 +1,11 @@
+SELECT 
+    custA.ContactName AS 'Nome',
+    custA.Country AS 'País',
+    (
+		SELECT COUNT(*) FROM customers AS custB
+		WHERE custB.Country = custA.Country 
+        AND custB.ContactName != custA.ContactName
+	) AS `Número de compatriotas`
+FROM customers AS custA
+HAVING `Número de compatriotas` != 0
+ORDER BY custA.ContactName;
