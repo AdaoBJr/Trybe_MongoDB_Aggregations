12e1c47ce5bddb89a22762601a3ba1d5908ea029
Matheus H. Gois <matheus.hengo@gmail.com>
2021-08-08 23:38:45 -0300

    12

--- desafio11.sql
+++ desafio11.sql
@@ -2,10 +2,10 @@ SELECT
     custA.ContactName AS 'Nome',
     custA.Country AS 'País',
     (
-		SELECT COUNT(*) FROM customers AS custB
-		WHERE custB.Country = custA.Country 
+		    SELECT COUNT(*) FROM customers AS custB
+		    WHERE custB.Country = custA.Country 
         AND custB.ContactName != custA.ContactName
-	) AS `Número de compatriotas`
+    ) AS `Número de compatriotas`
 FROM customers AS custA
 HAVING `Número de compatriotas` != 0
 ORDER BY custA.ContactName;
---
+++ desafio12.sql
@@ -0,0 +1,10 @@
+SELECT
+	CONCAT(emploA.FIRST_NAME, ' ', emploA.LAST_NAME) AS 'Nome completo funcionário 1',
+    emploA.SALARY AS "Salário funcionário 1",
+    emploA.PHONE_NUMBER AS "Telefone funcionário 1",
+    CONCAT(emploB.FIRST_NAME, ' ', emploB.LAST_NAME) AS 'Nome completo funcionário 2',
+    emploB.SALARY AS "Salário funcionário 2",
+    emploB.PHONE_NUMBER AS "Telefone funcionário 2"
+FROM employees AS emploA, employees AS emploB
+WHERE emploA.EMPLOYEE_ID != emploB.EMPLOYEE_ID AND emploA.JOB_ID = emploB.JOB_ID
+ORDER BY emploA.FIRST_NAME, emploB.FIRST_NAME;
