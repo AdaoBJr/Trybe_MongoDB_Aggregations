4bce275ea7f01057d9724af08d089fdd76abcff0
ricardo-rals <ricardo.rals.dev@gmail.com>
2021-08-09 12:00:47 -0300

    Fix Desafio 8 - fix Desafio 6

--- desafio6.sql
+++ desafio6.sql
@@ -4,10 +4,10 @@ SELECT
     DATE(job_history.start_date) AS `Data de início do cargo`,
     departments.department_name AS `Departamento`
 FROM hr.employees AS employees
-INNER JOIN hr.jobs AS jobs
-    ON jobs.job_id = employees.job_id
 INNER JOIN hr.job_history AS job_history
     ON job_history.employee_id = employees.employee_id
+INNER JOIN hr.jobs AS jobs
+    ON jobs.job_id = job_history.job_id
 INNER JOIN hr.departments AS departments
     ON departments.department_id = job_history.department_id
 ORDER BY  `Nome completo` DESC, `Cargo` ASC;
--- desafio8.sql
+++ desafio8.sql
@@ -1,5 +1,5 @@
 SELECT 
-    customers.CustomerName AS `Nome de contato`,
+    customers.ContactName AS `Nome de contato`,
     shippers.ShipperName AS `Empresa que fez o envio`,
     orders.OrderDate AS `Data do pedido`
 FROM w3schools.customers AS customers
