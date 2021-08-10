f4c33b2c98c26f804af535b6421518f1e69b065c
ricardo-rals <ricardo.rals.dev@gmail.com>
2021-08-10 11:25:37 -0300

    fix Desafio 11 - second try

--- desafio11.sql
+++ desafio11.sql
@@ -1,7 +1,7 @@
 SELECT 
     customer1.ContactName AS `Nome`,
     customer1.Country AS `País`,
-    COUNT(customer1.ContactName) -1 AS `Número de compatriotas`
+    COUNT(customer1.ContactName) AS `Número de compatriotas`
 FROM 
     w3schools.customers AS customer1,
     w3schools.customers AS customer2
