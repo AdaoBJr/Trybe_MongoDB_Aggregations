7a5c171568be067a8849124936ab88eb11f21099
Bruna Campos <92brunacampos@gmail.com>
2021-08-10 00:41:52 -0300

    code climate

--- desafio17.sql
+++ desafio17.sql
@@ -1,10 +1,9 @@
 USE w3schools;
 DELIMITER $$
 CREATE TRIGGER trigger_date
-	BEFORE INSERT ON orders
-	FOR EACH ROW 
+BEFORE INSERT ON orders
+FOR EACH ROW 
 BEGIN
-	SET
-  NEW.OrderDate = NOW();
+SET NEW.OrderDate = NOW();
 END $$
 DELIMITER ;
