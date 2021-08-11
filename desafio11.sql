3e245d6ca8c826993b35f317559b07fb5731cff7
renanpbraga <renanpbraga@gmail.com>
2021-08-11 20:17:36 -0300

    corrige desafio 11

--- desafio11.sql
+++ desafio11.sql
@@ -1,13 +1,14 @@
 SELECT 
-  b.ContactName AS 'Nome',
-  b.Country AS 'País',
-  (SELECT 
-    COUNT(*)
-      FROM
-        w3schools.customers AS a
-      WHERE
-        a.Country = c.Country
-      AND a.ContactName != b.ContactName) AS `Número de compatriotas`
-FROM w3schools.customers AS b
+    c.ContactName AS 'Nome',
+    c.Country AS 'País',
+    (SELECT 
+            COUNT(*)
+        FROM
+            w3schools.customers AS cu
+        WHERE
+            cu.Country = c.Country
+                AND cu.ContactName != c.ContactName) AS `Número de compatriotas`
+FROM
+    w3schools.customers AS c
 HAVING `Número de compatriotas` != 0
-ORDER BY b.ContactName;
+ORDER BY c.ContactName;
