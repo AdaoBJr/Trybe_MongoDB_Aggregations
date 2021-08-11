dc3cef58108592203a32156c5269e5ff15773df6
renanpbraga <renanpbraga@gmail.com>
2021-08-11 20:13:47 -0300

    corrige desafio 11

--- desafio11.sql
+++ desafio11.sql
@@ -1,14 +1,13 @@
-USE w3schools;
-SELECT
-  a.ContactName AS 'Nome',
-  a.Country AS 'País',
-  COUNT(b.Country) - 1 AS 'Número de compatriotas'
-FROM
-      customers AS a,
-      customers AS b
-WHERE a.Country = b.Country
-AND a.CustomerID <> b.CustomerID
-GROUP BY
-          a.ContactName,
-          a.Country
-ORDER BY a.ContactName;
+SELECT 
+  b.ContactName AS 'Nome',
+  b.Country AS 'País',
+  (SELECT 
+    COUNT(*)
+      FROM
+        w3schools.customers AS a
+      WHERE
+        a.Country = c.Country
+      AND a.ContactName != b.ContactName) AS `Número de compatriotas`
+FROM w3schools.customers AS b
+HAVING `Número de compatriotas` != 0
+ORDER BY b.ContactName;
