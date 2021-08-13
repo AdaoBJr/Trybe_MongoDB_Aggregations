3fbb2464001e6234e37b6dbf48b12f40604249fc
Junior030 <juniorhenrique030@gmail.com>
2021-08-13 11:09:24 -0300

    REQUISITO 10

---
+++ desafio10.sql
@@ -0,0 +1,9 @@
+SELECT PRO.ProductName AS "Produto",
+MIN(ORD_D.Quantity) AS "Mínima",
+MAX(ORD_D.Quantity) AS "Máxima",
+AVG(ORD_D.Quantity) AS "Média"
+FROM w3schools.products AS PRO
+INNER JOIN w3schools.order_details AS ORD_D
+ON ORD_D.ProductID = PRO.ProductID
+GROUP BY PRO.ProductName
+ORDER BY AVG(ORD_D.Quantity) ASC, PRO.ProductName ASC;
--- desafio9.sql
+++ desafio9.sql
@@ -1,6 +1,7 @@
 SELECT CONCAT(EMP.FirstName, ' ', EMP.LastName) AS "Nome de contato",
-COUNT(ORD.EmployeeID) AS "Total de pedidos"
+COUNT(*) AS "Total de pedidos"
 FROM w3schools.employees AS EMP
 INNER JOIN w3schools.orders AS ORD
 ON ORD.EmployeeID = EMP.EmployeeID
-GROUP BY ORD.EmployeeID;
+GROUP BY ORD.EmployeeID
+ORDER BY COUNT(*);
