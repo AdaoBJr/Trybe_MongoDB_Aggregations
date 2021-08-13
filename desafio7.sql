94f6af4fa28c78e03b3a4fd2f1e3f7e433d04bf0
Junior030 <juniorhenrique030@gmail.com>
2021-08-12 22:14:05 -0300

    Requesito 07

---
+++ desafio7.sql
@@ -0,0 +1,8 @@
+SELECT
+UCASE(CONCAT(EMP.FIRST_NAME, ' ', EMP.LAST_NAME)) AS "Nome completo",
+DATE(JOB_H.START_DATE) AS "Data de início",
+EMP.SALARY AS "Salário"
+FROM employees AS EMP
+INNER JOIN hr.job_history AS JOB_H
+ON JOB_H.EMPLOYEE_ID = EMP.EMPLOYEE_ID
+ORDER BY "Nome completo" ASC, "Data de início" ASC;
