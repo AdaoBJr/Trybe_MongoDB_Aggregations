29d167bf999012db96bb2bc834e536419b4f2704
Junior030 <juniorhenrique030@gmail.com>
2021-08-13 15:52:27 -0300

    Requisito 15

--- desafio14.sql
+++ desafio14.sql
@@ -2,4 +2,4 @@ SELECT CUS.Country  AS "País"
 FROM w3schools.customers AS CUS
 INNER JOIN w3schools.suppliers AS SUP
 ON SUP.Country = CUS.Country
-GROUP BY CUS.Country LIMIT 5; 
+GROUP BY CUS.Country LIMIT 5;
