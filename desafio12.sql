81a86e3550ca615bc098dd09fcaa701be5ae613c
Junior030 <juniorhenrique030@gmail.com>
2021-08-13 17:14:21 -0300

    Requisito 12 V2.0

--- desafio12.sql
+++ desafio12.sql
@@ -6,7 +6,8 @@ CONCAT(EMP2.FIRST_NAME, ' ', EMP2.LAST_NAME) AS "Nome completo funcionário 2",
 EMP2.SALARY AS "Salário funcionário 2",
 EMP2.PHONE_NUMBER AS "Telefone funcionário 2"
 FROM
-hr.employees AS EMP,
-hr.employees AS EMP2
-WHERE EMP.JOB_ID = EMP2.JOB_ID AND EMP.EMPLOYEE_ID != EMP2.EMPLOYEE_ID
-ORDER BY "Nome completo funcionário 1", "Nome completo funcionário 1";
+hr.employees AS EMP
+INNER JOIN hr.employees AS EMP2
+ON EMP.JOB_ID = EMP2.JOB_ID
+WHERE CONCAT(EMP.FIRST_NAME, ' ', EMP.LAST_NAME) <> CONCAT(EMP2.FIRST_NAME, ' ', EMP2.LAST_NAME)
+ORDER BY 1, 4;
