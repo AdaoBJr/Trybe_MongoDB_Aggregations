d9966d329758a1c71b3ce3d6f471e9dc1da89ce4
Junior030 <juniorhenrique030@gmail.com>
2021-08-12 22:16:51 -0300

    Code Climat

--- desafio6.sql
+++ desafio6.sql
@@ -1,21 +1,13 @@
 SELECT
 CONCAT(EMP.FIRST_NAME, ' ', EMP.LAST_NAME) AS "Nome completo", 
-	
 JOB.JOB_TITLE AS "Cargo",
-
 DATE(JOB_H.START_DATE) AS "Data de início do cargo",
-
 DEP.DEPARTMENT_NAME AS "Departamento"
-
 FROM employees AS EMP
-
 INNER JOIN hr.jobs AS JOB
 ON EMP.JOB_ID = JOB.JOB_ID
-
 INNER JOIN hr.job_history AS JOB_H
 ON EMP.EMPLOYEE_ID = JOB_H.EMPLOYEE_ID
-
 INNER JOIN hr.departments AS DEP
 ON EMP.DEPARTMENT_ID = JOB_H.DEPARTMENT_ID
-
 ORDER BY "Nome completo" DESC, "Cargo";
--- desafio7.sql
+++ desafio7.sql
@@ -1,5 +1,4 @@
-SELECT
-UCASE(CONCAT(EMP.FIRST_NAME, ' ', EMP.LAST_NAME)) AS "Nome completo",
+SELECT UCASE(CONCAT(EMP.FIRST_NAME, ' ', EMP.LAST_NAME)) AS "Nome completo",
 DATE(JOB_H.START_DATE) AS "Data de início",
 EMP.SALARY AS "Salário"
 FROM employees AS EMP
