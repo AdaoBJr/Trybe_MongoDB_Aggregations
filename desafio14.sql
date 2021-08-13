862973b8a39945efd60a3853cfe7116b0062cbcc
Junior030 <juniorhenrique030@gmail.com>
2021-08-13 16:21:41 -0300

    Requisito 15 v4.0

--- desafio14.sql
+++ desafio14.sql
@@ -1,9 +1,5 @@
-(SELECT CUS.Country AS "País"
-FROM w3schools.customers AS CUS)
-
-UNION
-
-(SELECT SUP.Country
-FROM w3schools.suppliers AS SUP)
-
-ORDER BY "País" LIMIT 5;
+(SELECT DISTINCT Country AS "País"
+FROM w3schools.customers)
+UNION(SELECT DISTINCT Country
+FROM w3schools.suppliers)
+ORDER BY `País` LIMIT 5;
