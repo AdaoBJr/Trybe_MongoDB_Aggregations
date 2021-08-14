d7f3d9a9256a8627efe41f992fd8d748841303df
Guilherme Ribeiro <guilhermedasilvar@gmail.com>
2021-08-13 23:49:52 -0300

    Requisito 09

---
+++ desafio9.sql
@@ -0,0 +1,13 @@
+SELECT
+CONCAT(FirstName,' ',LastName) AS `Nome completo`,
+COUNT(*) AS `Total de pedidos`
+FROM
+w3schools.orders AS o
+INNER JOIN
+w3schools.employees AS e
+ON
+o.EmployeeID = e.EmployeeID
+GROUP BY
+`Nome completo`
+ORDER BY
+`Total de pedidos`;
