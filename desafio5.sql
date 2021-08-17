36209154057f9365489ba65e28f3ce7fddb20854
Daniel Alves de Araujo <araujodaniel321@gmail.com>
2021-08-17 18:13:18 -0300

    another change on exercise 5

--- desafio5.sql
+++ desafio5.sql
@@ -1,5 +1,5 @@
 SELECT JOB_TITLE AS 'Cargo',
-(MAX_SALARY - MIN_SALARY) as `Variação Salarial`,
+(MAX_SALARY - MIN_SALARY) as 'Variação Salarial',
 ROUND (MIN_SALARY/12,2) AS 'Média minima mensal',
 ROUND (MAX_SALARY/12,2) AS 'Média máxima mensal'
 FROM hr.jobs 
