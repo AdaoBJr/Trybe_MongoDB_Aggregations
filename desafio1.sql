39afcaebcde1ad47c3236557f6f8da256da37bae
João Victor Pistorio <joao.pistorio@gmail.com>
2021-08-17 13:59:04 -0300

    desafio 1

---
+++ desafio1.sql
@@ -0,0 +1,9 @@
+SELECT country.COUNTRY_NAME AS País,
+CASE
+WHEN region.REGION_NAME = 'Europe' THEN 'incluído'
+ELSE 'não incluído'
+END AS 'Status Inclusão'
+FROM hr.countries AS country
+INNER JOIN hr.regions AS region
+ON country.REGION_ID = region.REGION_ID
+ORDER BY COUNTRY_NAME;
