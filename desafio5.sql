74d0110053b0dd6ef61356be0ad6c3f2d25fef53
João Victor Pistorio <joao.pistorio@gmail.com>
2021-08-17 16:29:18 -0300

    desafio 3, 4, 5

---
+++ desafio5.sql
@@ -0,0 +1,10 @@
+SELECT
+jobs.JOB_TITLE AS 'Cargo',
+(jobs.MAX_SALARY - jobs.MIN_SALARY) AS 'Variação Salarial',
+ROUND((jobs.MIN_SALARY/12), 2) AS 'Média mínima mensal',
+ROUND((jobs.MAX_SALARY/12), 2) AS 'Média máxima mensal'
+FROM hr.jobs AS jobs
+INNER JOIN hr.employees AS employees
+ON jobs.JOB_ID = employees.JOB_ID
+GROUP BY `Cargo`, `Variação Salarial`, `Média mínima Mensal`, `Média máxima mensal`
+ORDER BY `Variação Salarial` ASC, `Cargo` ASC;
