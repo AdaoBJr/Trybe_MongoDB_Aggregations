9163c583e342b9bb565642b1e899975db2046f40
Daniel Alves de Araujo <araujodaniel321@gmail.com>
2021-08-17 18:00:54 -0300

    changed the order of the lines

--- desafio5.sql
+++ desafio5.sql
@@ -1,6 +1,6 @@
 SELECT JOB_TITLE AS 'Cargo',
 (MAX_SALARY - MIN_SALARY) as `Variação Salarial`,
-ROUND (MIN_SALARY/12,2) AS 'Média minima mensal', 
-ROUND (MAX_SALARY/12,2) AS 'Média máxima mensal' 
+ROUND (MAX_SALARY/12,2) AS 'Média máxima mensal',
+ROUND (MIN_SALARY/12,2) AS 'Média minima mensal'
 FROM hr.jobs 
 ORDER BY `Variação Salarial`, 'Cargo';
