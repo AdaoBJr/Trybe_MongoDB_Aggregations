2f0b788eadbb39386d5260c7e13869b6e7f9a9a1
richardfazeredo <richardcompletetour@gmail.com>
2021-08-17 08:30:34 -0300

    Desafio 15

---
+++ desafio15.sql
@@ -0,0 +1,15 @@
+USE hr;
+
+DELIMITER $$
+
+CREATE PROCEDURE buscar_media_por_cargo(IN cargo VARCHAR(30))
+BEGIN
+	SELECT
+		ROUND(AVG(SALARY), 2) AS `Média salarial`
+	FROM employees AS e
+    INNER JOIN jobs AS j ON e.JOB_ID = j.JOB_ID AND j.JOB_TITLE = cargo;
+END $$
+
+DELIMITER $$;
+
+CALL buscar_media_por_cargo("Programmer");
