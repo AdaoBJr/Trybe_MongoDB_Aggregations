8407fcc386a7fbc9489538e683ce250f347e7536
João Victor Pistorio <joao.pistorio@gmail.com>
2021-08-17 17:09:10 -0300

    desafio 14

---
+++ desafio14.sql
@@ -0,0 +1,14 @@
+SELECT
+c.Country as 'País'
+FROM w3schools.customers c
+LEFT JOIN w3schools.suppliers s
+ON c.Country = s.Country
+UNION
+SELECT
+s.Country
+FROM w3schools.customers c
+RIGHT JOIN w3schools.suppliers s
+ON c.Country = s.Country
+GROUP BY 1
+ORDER BY 1 ASC
+LIMIT 5;
