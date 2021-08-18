5dcbe9ee2175e86ae0c1d08cf18a10dafc413ee6
Paulo Henrique Lima <limapaulobsb@gmail.com>
2021-08-18 09:50:47 -0300

    Fix requisito 1

--- desafio1.sql
+++ desafio1.sql
@@ -1,5 +1,5 @@
 SELECT
 COUNTRY_NAME AS País,
-IF(REGION_ID = 1, "incluido", "não incluido") AS "Status inclusão"
+IF(REGION_ID = 1, "incluído", "não incluído") AS "Status inclusão"
 FROM hr.countries
 ORDER BY COUNTRY_NAME;
