e576bc8dd6f5c1b5ab7c4799a95919949cade8b5
Daniel Alves de Araujo <araujodaniel321@gmail.com>
2021-08-18 18:44:43 -0300

    challenge 10 done

---
+++ desafio10.sql
@@ -0,0 +1,9 @@
+SELECT products.ProductName AS 'Produto',
+MIN(orders.Quantity) AS 'Mínima',
+MAX(orders.Quantity) AS 'Máxima',
+ROUND(AVG(orders.Quantity), 2) AS 'Média'
+FROM w3schools.products AS products
+INNER JOIN w3schools.order_details AS orders
+ON products.ProductID = orders.ProductID
+GROUP BY products.ProductID HAVING `Média` > 20
+ORDER BY `Média`, `Produto`;
