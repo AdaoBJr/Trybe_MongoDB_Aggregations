6d7d7d132d36e2476d3af649bc2fa8edb84738bf
Matheus H. Gois <matheus.hengo@gmail.com>
2021-08-22 03:37:12 -0300

    Desafio 17

---
+++ desafio17.sql
@@ -0,0 +1,10 @@
+USE w3schools;
+DELIMITER $$
+CREATE TRIGGER gatilho_inserir_data_atual_orderdate
+BEFORE INSERT ON w3schools.orders
+FOR EACH ROW
+BEGIN
+	SET new.OrderDate = now();
+END $$
+DELIMITER;
+
