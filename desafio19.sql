a574c064cdab7ac29459325b61d98ab19b17406a
Matheus H. Gois <matheus.hengo@gmail.com>
2021-08-22 04:10:24 -0300

    Fix 19

--- desafio19.sql
+++ desafio19.sql
@@ -2,9 +2,9 @@ USE hr;
 DELIMITER $$
 
 CREATE FUNCTION exibir_quantidade_pessoas_contratadas_por_mes_e_ano(mes int, ano int )
-RETURN INT READS SQL DATA
+RETURNS INT READS SQL DATA
 BEGIN
-DECLARE contratados INT;
+  DECLARE contratados INT;
   SELECT COUNT(*)
   FROM hr.employees
   WHERE MONTH(hire_date) = mes AND YEAR(hire_date) = ano INTO contratados;
