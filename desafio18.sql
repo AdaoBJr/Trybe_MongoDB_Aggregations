2d7eb7d3c1b02723be24f3a2209579d449da0e31
Paulo Henrique Lima <limapaulobsb@gmail.com>
2021-08-22 08:21:12 -0300

    Teste requisito 18

---
+++ desafio18.sql
@@ -0,0 +1,8 @@
+SELECT
+CONCAT(e.FIRST_NAME, " ", e.LAST_NAME) "Nome completo",
+CONCAT(DAY(START_DATE), "/", MONTH(START_DATE), "/", YEAR(START_DATE)) "Data de início",
+CONCAT(DAY(END_DATE), "/", MONTH(END_DATE), "/", YEAR(END_DATE)) "Data de rescisão",
+ROUND(DATEDIFF(END_DATE, START_DATE) / 365, 2) "Anos trabalhados"
+FROM hr.job_history h
+INNER JOIN employees e ON h.EMPLOYEE_ID = e.EMPLOYEE_ID
+ORDER BY `Nome Completo`;
