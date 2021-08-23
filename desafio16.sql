bfcfe44765239ff01d086ab0784f3117f34df712
aythan azevedo <aythan16@gmail.com>
2021-08-23 13:01:41 -0300

    desafio16

---
+++ desafio16.sql
@@ -0,0 +1,11 @@
+USE hr;
+DELIMITER $$
+CRIAR  FUNÇÃO  buscar_quantidade_de_empregos_por_funcionario (param varchar ( 100 ))
+RETORNA INT LÊ DADOS SQL
+COMEÇAR
+DECLARAR quantidade INT ;
+SELECT  COUNT ( jh . EMPLOYEE_ID ) FROM job_history jh INNER JOIN funcionários e ON  jh . EMPLOYEE_ID  =  e . EMPLOYEE_ID  WHERE EMAIL = param
+INTO quantidade;
+RETORNAR quantidade;
+END $$
+DELIMITER;
