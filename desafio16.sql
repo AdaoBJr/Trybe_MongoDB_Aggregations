4004f36216152740b0ac52c9f4902258cd4d4f9b
MarceloPelegrino <pelegrinocorrea@gmail>
2021-08-26 12:01:25 -0300

    Desafio 16 - Corrigido

--- desafio16.sql
+++ desafio16.sql
@@ -3,10 +3,10 @@ CREATE FUNCTION buscar_quantidade_de_empregos_por_funcionario(email VARCHAR(50))
 RETURNS DOUBLE READS SQL DATA
 BEGIN
 DECLARE quantidadeEmpregos DOUBLE;
-SELECT COUNT(joHi.EMPLOYEE_ID) AS 'total_empregos'
+SELECT COUNT(joHi.JOB_ID) AS 'total_empregos'
 FROM job_history AS joHi
 INNER JOIN hr.employees AS em ON joHi.EMPLOYEE_ID = em.EMPLOYEE_ID
-WHERE EMAIL=email
+WHERE em.EMAIL=email
 INTO quantidadeEmpregos;
 RETURN quantidadeEmpregos;
 END $$
